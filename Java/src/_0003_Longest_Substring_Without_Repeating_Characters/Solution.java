73d07ed9db021c4e672d8245e98b11c93418b56c
i-love-linux <1137247975@qq.com>
2020-01-01 15:35:31 +0800

    LeetCode 3

---
+++ Java/src/_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,26 @@
+package _0003_Longest_Substring_Without_Repeating_Characters;
+
+public class Solution {
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    public int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[256];
+        int l = 0, r = -1;  // 滑动窗口[l...r]
+        int res = 0;
+
+        while (l < s.length()){
+            if (r + 1 < s.length() && freq[s.charAt(r+1)] == 0)
+                freq[s.charAt(++r)]++;
+            else
+                freq[s.charAt(l++)]--;
+
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).lengthOfLongestSubstring("abcabcbb"));
+    }
+}
