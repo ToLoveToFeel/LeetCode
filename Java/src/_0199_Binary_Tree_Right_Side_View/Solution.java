5742f734c94ac0fa2464e8c490b2da9e216df6c7
i-love-linux <1137247975@qq.com>
2020-01-09 19:13:39 +0800

    LeetCode 199 二叉树的层序遍历

---
+++ Java/src/_0199_Binary_Tree_Right_Side_View/Solution.java
@@ -0,0 +1,37 @@
+package _0199_Binary_Tree_Right_Side_View;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (null == root)
+            return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int levelNum = 1;  // 当前层节点数
+        while (!queue.isEmpty()){
+            int newLevelNum = 0;
+            for (int i = 0; i < levelNum; i++){  // 遍历当前层所有节点
+                TreeNode node = queue.remove();
+
+                if (null != node.left){
+                    queue.add(node.left);
+                    newLevelNum++;
+                }
+                if (null != node.right){
+                    queue.add(node.right);
+                    newLevelNum++;
+                }
+                if (levelNum - 1 == i)  // 遍历到最后一个节点
+                    res.add(node.val);
+            }
+            levelNum = newLevelNum;
+        }
+        return res;
+    }
+}
---
+++ Java/src/_0199_Binary_Tree_Right_Side_View/TreeNode.java
@@ -0,0 +1,10 @@
+package _0199_Binary_Tree_Right_Side_View;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
