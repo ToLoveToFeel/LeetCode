1f4ade59632577d1b911c50b39bede31a7ff4aac
i-love-linux <1137247975@qq.com>
2020-01-13 17:27:54 +0800

    LeetCode 46 全排列问题

---
+++ Java/src/_0046_Permutations/Solution.java
@@ -0,0 +1,55 @@
+package _0046_Permutations;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    // 时间复杂度：O(n^n)
+    // 空间复杂度：O(n)
+    private ArrayList<List<Integer>> res;
+    private boolean[] used;
+
+    private void generatePermutation(int[] nums, int index, LinkedList<Integer> p){
+        if (index == nums.length){
+            res.add((LinkedList<Integer>)p.clone());
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++)
+            if (!used[i]){
+                p.addLast(nums[i]);
+                used[i] = true;
+                generatePermutation(nums, index + 1, p);
+                p.removeLast();
+                used[i] = false;
+            }
+        return;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<List<Integer>>();
+
+        if (nums == null || nums.length == 0)
+            return res;
+
+        used = new boolean[nums.length];
+        LinkedList<Integer> p = new LinkedList<>();
+        generatePermutation(nums, 0, p);
+
+        return res;
+    }
+
+    private static void printList(List<Integer> list){
+        for(Integer e: list)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> res = (new Solution()).permute(nums);
+        for (List<Integer> list:res)
+            printList(list);
+    }
+}
---
+++ Java/src/_0046_Permutations/Solution2.java
@@ -0,0 +1,62 @@
+package _0046_Permutations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Solution2 {
+    // 时间复杂度：O(n!)
+    // 空间复杂度：O(n)
+    private ArrayList<List<Integer>> res;
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0)
+            return res;
+
+        generatePermutation(nums, 0);
+
+        return res;
+    }
+
+    private void generatePermutation(int[] nums, int index){
+
+        if(index == nums.length){
+            List<Integer> list = new ArrayList<Integer>();
+            for(int i : nums)
+                list.add(i);
+            res.add(list);
+            return;
+        }
+
+        for(int i = index ; i < nums.length ; i ++){
+            swap(nums, i, index);
+            generatePermutation(nums, index + 1);
+            swap(nums, i, index);
+        }
+
+        return;
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    private static void printList(List<Integer> list){
+        for(Integer e: list)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> res = (new Solution2()).permute(nums);
+        for(List<Integer> list: res)
+            printList(list);
+    }
+}
