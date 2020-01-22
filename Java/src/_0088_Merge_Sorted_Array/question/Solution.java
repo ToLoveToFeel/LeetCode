3ea06166509a5dd3a33e3ee7d1084f91da3768b6
i-love-linux <1137247975@qq.com>
2020-01-22 12:07:24 +0800

    LeetCode 88 数组相关问题 merge

---
+++ Java/src/_0088_Merge_Sorted_Array/question/Solution.java
@@ -0,0 +1,38 @@
+package _0088_Merge_Sorted_Array.question;
+
+public class Solution {
+    // 时间复杂度：O(m+n)
+    // 空间复杂度：O(m+n)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int length = m + n;
+        if(length == 0)
+            return;
+        int[] aux = new int[length];
+        int i = 0;
+        int j = 0;
+        int k = 0;  // [0...k]已经排好序
+        while (k < length){
+            if (i >= m)
+                aux[k++] = nums2[j++];
+            else if (j >= n)
+                aux[k++] = nums1[i++];
+            else if (nums1[i] > nums2[j])
+                aux[k++] = nums2[j++];
+            else
+                aux[k++] = nums1[i++];
+        }
+        for (int ii = 0; ii < length; ii++)
+            nums1[ii] = aux[ii];
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        (new Solution()).merge(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++)
+            System.out.println(nums1[i]);
+    }
+}
---
+++ Java/src/_0088_Merge_Sorted_Array/question/Solution2.java
@@ -0,0 +1,38 @@
+package _0088_Merge_Sorted_Array.question;
+
+public class Solution2 {
+    // 时间复杂度：O(m+n)
+    // 空间复杂度：O(1)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int length = m + n;
+
+        for (int i = n + m - 1; i >= n; i--)  // nums1元素整体向后移动n个元素
+            nums1[i] = nums1[i - n];
+
+        int i = n;  // pointer for nums1 [n, n+m)
+        int j = 0;  // pointer for nums2 [0, n)
+        int k = 0;  // pointer merged nums1 [0, n+m)
+
+        while (k < length){
+            if (i >= length)
+                nums1[k++] = nums2[j++];
+            else if (j >= n)
+                nums1[k++] = nums1[i++];
+            else if (nums1[i] > nums2[j])
+                nums1[k++] = nums2[j++];
+            else
+                nums1[k++] = nums1[i++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        (new Solution2()).merge(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++)
+            System.out.println(nums1[i]);
+    }
+}
