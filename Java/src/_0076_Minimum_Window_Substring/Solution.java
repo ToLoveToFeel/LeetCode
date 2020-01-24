11da3c55ea3ba1346642ac9cacb96443e6e26c96
i-love-linux <1137247975@qq.com>
2020-01-24 16:09:35 +0800

    LeetCode 76 数组相关问题 滑动窗口 最小窗口子串

---
+++ Java/src/_0076_Minimum_Window_Substring/Solution.java
@@ -0,0 +1,58 @@
+package _0076_Minimum_Window_Substring;
+
+public class Solution {
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    // 滑动窗口
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length())
+            return "";
+
+        int[] tFreq = new int[256];  // 初始化默认为0
+        // 统计t中字符个数
+        for (int i = 0; i < t.length(); i++)
+            tFreq[t.charAt(i)] ++;
+
+        int[] sFreq = new int[256];
+        int sCount = 0;  // 统计s中出现t中字符的个数
+
+        int minLength = s.length() + 1;  // 统计最短的满足条件的子串
+        int startIndex = -1;  // 子串起始位置
+
+        int l = 0, r = -1;  // [l...r]滑动窗口
+        int sLength = s.length();
+        int tLength = t.length();
+        // l + t - 1 <= s - 1  ==>  l + t - 1 < s  ==> l < s - t + 1
+        while (l < sLength - tLength + 1){
+            if (r + 1 < sLength && sCount < tLength){
+                sFreq[s.charAt(r+1)]++;
+                if (sFreq[s.charAt(r+1)] <= tFreq[s.charAt(r+1)])  // s[r+1]是t中的未统计的字符
+                    sCount++;
+                r++;
+            }
+            else {
+                assert sCount <= tLength;
+                if (sCount == tLength && r - l + 1 < minLength){
+                    minLength = r - l + 1;  // 更新最小长度
+                    startIndex = l;
+                }
+                sFreq[s.charAt(l)]--;
+                if (sFreq[s.charAt(l)] < tFreq[s.charAt(l)])   // s[l]是t中的未统计的字符
+                    sCount--;
+                l++;
+            }
+        }
+
+        if (startIndex != -1)
+            return s.substring(startIndex, startIndex + minLength);
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).minWindow("ADOBECODEBANC", "ABC"));
+        System.out.println((new Solution()).minWindow("a", "aa"));
+        System.out.println((new Solution()).minWindow("aa", "aa"));
+        System.out.println((new Solution()).minWindow("bba", "ab"));
+    }
+}
---
+++ Java/src/_0076_Minimum_Window_Substring/Solution2.java
@@ -0,0 +1,69 @@
+package _0076_Minimum_Window_Substring;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Solution2 {
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    // 使用一个字符串filtered_s，其中只包含s中那些t中含有的字符
+    // 当t的长度远远小于s的长度时，效果比较好
+    public String minWindow(String s, String t) {
+        HashSet<Character> t_set = new HashSet<>();
+        int[] tFreq = new int[256];
+        for (int i = 0; i < t.length(); i++){
+            t_set.add(t.charAt(i));
+            tFreq[t.charAt(i)]++;
+        }
+
+        String filtered_s = "";
+        ArrayList<Integer> pos = new ArrayList<>();  // 记录filtered_s中各字符在s中的位置
+        for (int i = 0; i < s.length(); i++)
+            if (t_set.contains(s.charAt(i))){
+                filtered_s += s.charAt(i);
+                pos.add(i);
+            }
+
+        int[] sFreq = new int[256];
+        int sCount = 0;  // 统计s中出现t中字符的个数
+
+        int minLength = s.length() + 1;  // 统计最短的满足条件的子串
+        int startIndex = -1;  // 子串起始位置
+
+        int l = 0, r = -1;  // [l...r]滑动窗口
+        int sLength = s.length();
+        int tLength = t.length();
+        while (l < filtered_s.length()){
+            if (r + 1 < filtered_s.length() && sCount < tLength){
+                sFreq[filtered_s.charAt(r+1)]++;
+                if (sFreq[filtered_s.charAt(r+1)] <= tFreq[filtered_s.charAt(r+1)])  // s[r+1]是t中的未统计的字符
+                    sCount++;
+                r++;
+            }
+            else {
+                assert sCount <= tLength;
+                if (sCount == tLength && pos.get(r) - pos.get(l)  + 1 < minLength){
+                    minLength = pos.get(r) - pos.get(l)  + 1;  // 更新最小长度
+                    startIndex = pos.get(l);
+                }
+                sFreq[filtered_s.charAt(l)]--;
+                if (sFreq[filtered_s.charAt(l)] < tFreq[filtered_s.charAt(l)])   // s[l]是t中的未统计的字符
+                    sCount--;
+                l++;
+            }
+        }
+
+        if (startIndex != -1)
+            return s.substring(startIndex, startIndex + minLength);
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution2()).minWindow("ADOBECODEBANC", "ABC"));
+        System.out.println((new Solution2()).minWindow("a", "aa"));
+        System.out.println((new Solution2()).minWindow("aa", "aa"));
+        System.out.println((new Solution2()).minWindow("bba", "ab"));
+    }
+}
