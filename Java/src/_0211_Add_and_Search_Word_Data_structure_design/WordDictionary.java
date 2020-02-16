cf0f76ca2bb6cfdad47187a8516758e85f2cb46f
i-love-linux <1137247975@qq.com>
2020-02-16 11:33:15 +0800

    LeetCode211 字典树 模式匹配

---
+++ Java/src/_0211_Add_and_Search_Word_Data_structure_design/WordDictionary.java
@@ -0,0 +1,65 @@
+package _0211_Add_and_Search_Word_Data_structure_design;
+
+import java.util.TreeMap;
+
+public class WordDictionary {
+
+    private class Node{
+
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node(){
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    // 在以node为根节点的字典树中查找是否存在单词word，index表示匹配到第几个字符
+    private boolean match(Node node, String word, int index){
+        if (index == word.length())
+            return node.isWord;
+
+        char c = word.charAt(index);
+        if (c != '.'){
+            if (node.next.get(c) == null)
+                return false;
+            return match(node.next.get(c), word, index + 1);
+        }
+        else{  // c == '.'
+            for (char nextChar : node.next.keySet())
+                if (match(node.next.get(nextChar), word, index + 1))
+                    return true;
+            return false;
+        }
+
+    }
+}
