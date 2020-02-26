183ac3e66ac517d109551ca62d1c87ed17a9cbf6
i-love-linux <1137247975@qq.com>
2020-02-26 19:17:10 +0800

    LeetCode 50 求x的n次方

---
+++ Java/src/_0050_Pow_x_n/Solution.java
@@ -0,0 +1,32 @@
+package _0050_Pow_x_n;
+
+public class Solution {
+    public double myPow(double x, int n){
+        return pow(x, n);
+    }
+
+    private double pow(double x, long n) {
+        if (n < 0){
+            x = 1.0 / x;
+            n *= -1;
+        }
+        else if (n == 0)
+            return 1;
+        else if (n == 1)
+            return x;
+
+        if (n % 2 == 0){
+            double half = pow(x, n / 2);
+            return half * half;
+        }
+        else // n % 2 == 1
+            return x * pow(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).myPow(2.0, 10));
+        System.out.println((new Solution()).myPow(2.1, 3));
+        System.out.println((new Solution()).myPow(2.0, -2));
+        System.out.println((new Solution()).myPow(2.0, -2147483648));
+    }
+}
---
+++ Java/src/_0050_Pow_x_n/Solution2.java
@@ -0,0 +1,35 @@
+package _0050_Pow_x_n;
+
+public class Solution2 {
+    public double myPow(double x, int n) {
+        if (n < 0){
+            x = 1.0 / x;
+            if (n != Integer.MIN_VALUE)  // 对int最小值做特殊处理
+                n *= -1;
+        }
+        else if (n == 0)
+            return 1;
+        else if (n == 1)
+            return x;
+
+        if (n == Integer.MIN_VALUE){  // 对int最小值做特殊处理
+            n++;
+            n *= -1;
+            return x * myPow(x, n);
+        }
+
+        if (n % 2 == 0){
+            double half = myPow(x, n / 2);
+            return half * half;
+        }
+        else // n % 2 == 1
+            return x * myPow(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution2()).myPow(2.0, 10));
+        System.out.println((new Solution2()).myPow(2.1, 3));
+        System.out.println((new Solution2()).myPow(2.0, -2));
+        System.out.println((new Solution2()).myPow(2.0, -2147483648));
+    }
+}
