d3dfcaa4b5170e0f2cc14620d792f5d9216714b7
wxx <1137247975@qq.com>
2020-08-24 14:44:59 +0800

    LeetCode 136 Single Number

---
+++ Java/src/_0136_Single_Number/Solution.java
@@ -0,0 +1,20 @@
+package _0136_Single_Number;
+
+/**
+ * Date: 2020/8/24 14:20
+ * Content:
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println((new Solution()).singleNumber(nums));
+    }
+}
