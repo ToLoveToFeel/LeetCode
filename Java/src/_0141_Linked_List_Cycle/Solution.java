839bcefc5a31bb27969f6978c2e1af28b0ae070b
wxx <1137247975@qq.com>
2020-09-09 20:40:14 +0800

    Leetcode 0141 å 环形链表

---
+++ Java/src/_0141_Linked_List_Cycle/Solution.java
@@ -0,0 +1,30 @@
+package _0141_Linked_List_Cycle;
+
+import _0000_classicalProblems._datastructure.set.Set;
+
+import java.util.HashSet;
+
+/**
+ * Date: 2020/9/9 20:17
+ * Content:
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode curNode = head;
+        while (curNode != null) {
+            if (set.contains(curNode))
+                return true;
+            else
+                set.add(curNode);
+            curNode = curNode.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = (new MyLinkedList(new int[]{1, 2})).getHead();
+        head.next.next = head;
+        System.out.println((new Solution()).hasCycle(head));
+    }
+}
---
+++ Java/src/_0141_Linked_List_Cycle/Solution2.java
@@ -0,0 +1,33 @@
+package _0141_Linked_List_Cycle;
+
+import java.util.HashSet;
+
+/**
+ * Date: 2020/9/9 20:17
+ * Content:
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+public class Solution2 {
+    // 双指针（一快一慢）
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode slowNode = head;
+        ListNode fastNode = head.next;
+        while (slowNode != fastNode) {
+            if (fastNode == null || fastNode.next == null)
+                return false;
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = (new MyLinkedList(new int[]{1, 2})).getHead();
+        head.next.next = head;
+        System.out.println((new Solution2()).hasCycle(head));
+    }
+}
