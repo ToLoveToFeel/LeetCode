dafbb942c1c63884a44abf1154876abc4c92409f
wxx <1137247975@qq.com>
2020-09-15 11:18:13 +0800

    Leetcode 0022 括号生成

---
+++ Java/src/_0022_Generate_Parentheses/Solution.java
@@ -0,0 +1,47 @@
+package _0022_Generate_Parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date: 2020/9/15 10:44
+ * Content:
+ * 回溯算法
+ */
+public class Solution {
+    ArrayList<String> res;
+
+    // sb：待生成的括号；leftNum、rightNum：左右括号可用的个数；n：括号对数
+    private void generateParenthesis(StringBuilder sb, int leftNum, int rightNum, int n) {
+        if (leftNum == n && rightNum == n) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (leftNum < n) {
+            sb.append('(');
+            generateParenthesis(sb, leftNum + 1, rightNum, n);
+            sb.deleteCharAt(sb.length() - 1);  // 回溯
+        }
+        if (leftNum > rightNum) {
+            sb.append(')');
+            generateParenthesis(sb, leftNum, rightNum + 1, n);
+            sb.deleteCharAt(sb.length() - 1);  // 回溯
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        res = new ArrayList<>();
+        if (n <= 0)
+            return res;
+
+        StringBuilder sb = new StringBuilder();
+        generateParenthesis(sb, 0, 0, n);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println((new Solution()).generateParenthesis(n));
+    }
+}
---
+++ Java/src/_0022_Generate_Parentheses/Solution2.java
@@ -0,0 +1,42 @@
+package _0022_Generate_Parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date: 2020/9/15 10:44
+ * Content:
+ * 动态规划
+ * 状态：dp[i]：使用i对括号能够生成的组合的集合
+ * 状态转移：dp[i] = "(" + dp[j] + ")" + dp[(i-1) - j], 其中 j = 0,1,...,n-1
+ */
+public class Solution2 {
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0)
+            return new ArrayList<>();
+
+        List<List<String>> dp = new ArrayList<>();  // dp[i] = dp.get(i)
+        List<String> dp0 = new ArrayList<String>() {{
+            add("");
+        }};
+        dp.add(dp0);
+
+        for (int i = 1; i <= n; i++) {
+            List<String> cur = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = dp.get(j);
+                List<String> str2 = dp.get(i - 1 - j);
+                for (String s1 : str1)
+                    for (String s2 : str2)
+                        cur.add("(" + s1 + ")" + s2);
+            }
+            dp.add(cur);
+        }
+        return dp.get(n);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println((new Solution2()).generateParenthesis(n));
+    }
+}
