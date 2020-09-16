7b482b1c6f1f777d52545cfffcfd058b8d4e0e06
wxx <1137247975@qq.com>
2020-09-16 20:28:40 +0800

    Leetcode 0014 最长公共前缀 二分法 深入理解

--- Java/src/_0014_Longest_Common_Prefix/Solution5.java
+++ Java/src/_0014_Longest_Common_Prefix/Solution5.java
@@ -4,11 +4,11 @@ package _0014_Longest_Common_Prefix;
  * Date: 2020/8/29 9:23
  * Content:
  */
-class Solution4 {
+class Solution5 {
     private Boolean isCcommonPrefix(String[] strs, int length) {
-        String str0 = strs[0].substring(0, length + 1);
+        String str0 = strs[0].substring(0, length);
         for (int i = 1; i < strs.length; i++) {
-            for (int j = 0; j <= length; j++) {
+            for (int j = 0; j < length; j++) {
                 if (str0.charAt(j) != strs[i].charAt(j))
                     return false;
             }
@@ -16,8 +16,7 @@ class Solution4 {
         return true;
     }
 
-    // 二分查找，这种二分查找法比较好，比Solution5容易理解
-    // 因为和Solution5 的 isCcommonPrefix 实现有区别，因此 longestCommonPrefix 中实现由区别
+    // 二分查找
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0)
             return "";
@@ -30,11 +29,15 @@ class Solution4 {
         }
         // 保证 [0...low) 是最长前缀，在 [low...high] 中寻找结束点
         // 如果结束点是 high, substring(0, high) 会将数据全部截取下来
-        int low = 0, high = minLength - 1;
-        while (low <= high) {  // 在 low == high 的情况下，还要考虑 low 这个位置是不是公共前缀
-            int mid = (high - low) / 2 + low;
-            if (isCcommonPrefix(strs, mid)) {
-                low = mid + 1;
+        int low = 0, high = minLength;
+        while (low < high) {  // 如果 low==high ,此时公共前缀已经确定了
+            // int mid = (high - low) / 2 + low;
+            // high - low 为偶数时，有没有+1对 mid 的值没有影响  9 8  -->  (2-0)/2=1  (2-0+1)/2=1
+            // high - low 为奇数时，+1对 mid 的值有影响         9   -->  (1-0)/2=0  (1-0+1)/2=1
+            // 因为 isCcommonPrefix 判断的是 [0...mid) 范围的前缀，不+1的话如果 high - low = 1 的情况下，mid = 0,会陷入死循环
+            int mid = (high - low + 1) / 2 + low;  // 加一的目的是防止死循环
+            if (isCcommonPrefix(strs, mid)){
+                low = mid;
             } else {
                 high = mid - 1;
             }
@@ -43,9 +46,9 @@ class Solution4 {
     }
 
     public static void main(String[] args) {
-        String[] strs = {"flower", "flow", "flight"};
+//        String[] strs = {"flower", "flow", "flight"};
 //        String[] strs = {"dog", "racecar", "car"};
-//        String[] strs = {"a"};
-        System.out.println((new Solution4()).longestCommonPrefix(strs));
+        String[] strs = {"a"};
+        System.out.println((new Solution5()).longestCommonPrefix(strs));
     }
 }
