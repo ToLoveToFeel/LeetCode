850a0742ab837d8c9ec3d83dba9c92234fcd3a60
wxx <1137247975@qq.com>
2020-09-16 09:04:49 +0800

    Leetcode 0036 有效数独

---
+++ Java/src/_0036_Valid_Sudoku/Solution2.java
@@ -0,0 +1,57 @@
+package _0036_Valid_Sudoku;
+
+import java.util.HashSet;
+
+/**
+ * Date: 2020/9/15 17:14
+ * Content:
+ */
+public class Solution2 {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][10];
+        boolean[][] col = new boolean[9][10];
+        boolean[][] box = new boolean[9][10];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+                int curNumber = board[i][j] - '0';
+                if (row[i][curNumber] || col[j][curNumber] || box[i / 3 * 3 + j / 3][curNumber])
+                    return false;
+
+                row[i][curNumber] = true;
+                col[j][curNumber] = true;
+                box[i / 3 * 3 + j / 3][curNumber] = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        char[][] board = {
+//                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+//                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+//                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+//                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+//                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+//                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+//                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+//                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+//                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+//        };
+
+        char[][] board = {
+                {'.', '9', '.', '.', '4', '.', '.', '.', '.'},
+                {'1', '.', '.', '.', '.', '.', '6', '.', '.'},
+                {'.', '.', '3', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
+                {'3', '.', '.', '.', '5', '.', '.', '.', '.'},
+                {'.', '.', '7', '.', '.', '4', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '7', '.', '.', '.', '.'}
+        };
+        System.out.println((new Solution2()).isValidSudoku(board));
+    }
+}
