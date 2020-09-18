ed69767bff3749c495dd2aae0a262f9a7203fe64
wxx <1137247975@qq.com>
2020-09-18 15:34:23 +0800

    Leetcode 0035 搜索插入位置

---
+++ Java/src/_0035_Search_Insert_Position/Solution.java
@@ -0,0 +1,31 @@
+package _0035_Search_Insert_Position;
+
+/**
+ * Date: 2020/9/18 15:15
+ * Content:
+ * 二分查找
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (target == nums[mid])
+                return mid;
+
+            if (target > nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 0;
+        System.out.println((new Solution()).searchInsert(nums, target));
+    }
+}
