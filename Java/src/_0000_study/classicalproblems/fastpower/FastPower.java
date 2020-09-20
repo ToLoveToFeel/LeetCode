067c8067297cc7258f5e366220c647659c361536
wxx <1137247975@qq.com>
2020-09-20 11:15:01 +0800

    快速幂算法 经典算法

---
+++ Java/src/_0000_study/classicalproblems/fastpower/FastPower.java
@@ -0,0 +1,24 @@
+package _0000_study.classicalproblems.fastpower;
+
+/**
+ * Date: 2020/9/20 11:10
+ * Content:
+ */
+public class FastPower {
+    // a ^ b
+    public int fastPower(int a, int b) {
+        int res = 1;
+        while (b != 0) {
+            if ((b & 1) == 1) {
+                res *= a;
+            }
+            b /= 2;
+            a *= a;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new FastPower()).fastPower(2, 20));
+    }
+}
---
+++ Java/src/_0000_study/classicalproblems/fastpowermod/FastPowerMod.java
@@ -0,0 +1,12 @@
+package _0000_study.classicalproblems.fastpowermod;
+
+/**
+ * Date: 2020/9/20 10:15
+ * Content:
+ */
+public class FastPowerMod {
+    // (a ^ b) % c
+    public int fastPowerMod(int a, int b, int c) {
+        return 0;
+    }
+}
