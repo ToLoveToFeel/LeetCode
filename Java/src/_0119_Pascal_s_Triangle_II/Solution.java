d0c4ae1afc25842256af91788939415ef0f1f893
wxx <1137247975@qq.com>
2020-09-21 19:00:57 +0800

    Leetcode 0119 杨辉三角II

---
+++ Java/src/_0119_Pascal_s_Triangle_II/Solution.java
@@ -0,0 +1,28 @@
+package _0119_Pascal_s_Triangle_II;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date: 2020/9/21 18:48
+ * Content:
+ */
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        if (rowIndex <= 0)
+            return res;
+
+        res.add(1);
+        for (int i = 1; i < rowIndex; i++) {
+            // rowIndex - 1 - i + 1 = rowIndex - i
+            long temp = ((long) res.get(i - 1)) * (rowIndex - i) / i;
+            res.add((int) temp);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).getRow(33));
+    }
+}
