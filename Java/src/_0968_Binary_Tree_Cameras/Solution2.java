4ddf38ab2f51749504a219c713f45757149e3045
wxx <1137247975@qq.com>
2020-09-23 10:49:25 +0800

    Leetcode 0968 规范化

--- Java/src/_0968_Binary_Tree_Cameras/Solution2.java
+++ Java/src/_0968_Binary_Tree_Cameras/Solution2.java
@@ -6,30 +6,36 @@ package _0968_Binary_Tree_Cameras;
  */
 public class Solution2 {
     int res = 0;
-
     /*
     对于某个节点，有三种状态：
         State 0: 这个节点待覆盖
         State 1: 这个节点已经覆盖
-        State 2: 给这个节点安装相机
+        State 2: 这个节点安装了相机
      */
+    private static final int NEED_COVER = 0;
+    private static final int HAS_COVER = 1;
+    private static final int NEED_CAMERA = 2;
+
     private int dfs(TreeNode node) {
         if (node == null)  // 空节点代表已经覆盖
-            return 1;
+            return HAS_COVER;
 
         int left = dfs(node.left);
         int right = dfs(node.right);
-        if (left == 0 || right == 0) {  // 左右节点至少有一个待覆盖
+        if (left == NEED_COVER || right == NEED_COVER) {  // 左右节点至少有一个待覆盖
             res++;
-            return 2;
+            return NEED_CAMERA;
         }
-        if (left == 1 && right == 1)  // 左右节点都已经被覆盖
-            return 0;
-        return 1;  // left + right >= 3, 说明左右子节点至少有一个安装了相机
+        if (left == HAS_COVER && right == HAS_COVER)  // 左右节点都已经被覆盖
+            return NEED_COVER;
+        // left == HAS_COVER && right == NEED_CAMERA
+        // left == NEED_CAMERA && right == HAS_COVER
+        // left == NEED_CAMERA && right == NEED_CAMERA
+        return HAS_COVER;  // left + right >= 3, 说明左右子节点至少有一个安装了相机
     }
 
     public int minCameraCover(TreeNode root) {
-        if (dfs(root) == 0)
+        if (dfs(root) == NEED_COVER)
             res++;
         return res;
     }
