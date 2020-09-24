e3819f583649459f48d4b03638202c2fd84cce29
wxx <1137247975@qq.com>
2020-09-24 19:56:33 +0800

    Leetcode 0155 最小栈

---
+++ Java/src/_0155_Min_Stack/Solution.java
@@ -0,0 +1,51 @@
+package _0155_Min_Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Date: 2020/9/24 19:47
+ * Content:
+ */
+public class Solution {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());  // return -3
+        minStack.pop();
+        System.out.println(minStack.top());  // return 0
+        System.out.println(minStack.getMin());  // return -2
+    }
+}
+
+class MinStack {
+    Deque<Integer> stack;  // 存储对应的元素
+    Deque<Integer> minStack;  // 存储当前stack对应元素中的最小值
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+        minStack.push(Integer.MAX_VALUE);  // 加入这个元素是为了：在第一次向栈中添加元素，方便比较，相当于哨兵
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
