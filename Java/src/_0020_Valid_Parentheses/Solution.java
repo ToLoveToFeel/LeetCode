79711e1134baba0a04a42e5b5d3059d645d5d541
wxx <1137247975@qq.com>
2020-09-25 14:15:21 +0800

    Leetcode 0020 有效的括号

--- Java/src/_0020_Valid_Parentheses/Solution.java
+++ Java/src/_0020_Valid_Parentheses/Solution.java
@@ -1,15 +1,17 @@
 package _0020_Valid_Parentheses;
-import java.util.Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i=0; i < s.length(); i++) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if('(' == c || '[' == c || '{' == c) {
+            if ('(' == c || '[' == c || '{' == c) {
                 stack.push(c);
             } else {
-                if(stack.isEmpty()){
+                if (stack.isEmpty()) {
                     return false;
                 }
                 char top = stack.pop();
