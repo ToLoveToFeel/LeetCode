c41af40c43cfae95c05c9e2cabf2521d59cf5e0f
wxx <1137247975@qq.com>
2020-09-27 10:56:26 +0800

    Leetcode 0236 二叉树的最近公共祖先

---
+++ Java/src/_0236_Lowest_Common_Ancestor_of_a_Binary_Tree/TreeNode.java
@@ -0,0 +1,10 @@
+package _0236_Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
