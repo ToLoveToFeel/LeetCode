db98ebfb315cd778c3a9b9c4aa0645877a70b693
wxx <1137247975@qq.com>
2020-10-02 19:44:27 +0800

    classicalproblems multithread

---
+++ Java/src/_0000_study/classicalproblems/multithread/print_odd_even_numbers_alternately/Solution.java
@@ -0,0 +1,42 @@
+package _0000_study.classicalproblems.multithread.print_odd_even_numbers_alternately;
+
+/**
+ * Date: 2020/10/2 19:23
+ * Content:
+ */
+public class Solution {
+    private static int count = 0;  // 打印的数字
+    private static final Object lock = new Object();  // 锁
+
+    public static final int MAX_NUM = 10;  // 打印数据范围
+
+    public static void main(String[] args) {
+        // 创建 Runnable 实例
+        Runnable r1 = () -> {  // 打印偶数的线程
+            while (count <= MAX_NUM) {
+                synchronized (lock) {
+                    if ((count & 1) == 0)  // 如果 count 是偶数
+                        System.out.println(Thread.currentThread().getName() + ": " + count++);
+                }
+            }
+        };
+        Runnable r2 = () -> {  // 打印奇数的线程
+            // 注意因为 MAX_NUM 为偶数，如果是 <= 的话，假设某一刻 count = MAX_NUM, 两个线程都在等锁
+            // 偶数线程会打印出 count, 并将 count 加一，之后奇数线程会将加一后的结果打印出来，导致错误!!!
+            while (count < MAX_NUM) {
+                synchronized (lock) {
+                    if ((count & 1) == 1)  // 如果 count 是奇数数
+                        System.out.println(Thread.currentThread().getName() + ": " + count++);
+                }
+            }
+        };
+
+        // 创建线程
+        Thread thread1 = new Thread(r1, "even");
+        Thread thread2 = new Thread(r2, "odd");
+
+        // 启动线程
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ Java/src/_0000_study/classicalproblems/multithread/print_odd_even_numbers_alternately/Solution2.java
@@ -0,0 +1,50 @@
+package _0000_study.classicalproblems.multithread.print_odd_even_numbers_alternately;
+
+/**
+ * Date: 2020/10/2 19:23
+ * Content:
+ */
+public class Solution2 {
+    private static int count = 0;  // 打印的数字
+    private static final Object lock = new Object();  // 锁
+
+    public static final int MAX_NUM = 10;  // 打印数据范围
+
+    public static void main(String[] args) {
+        // 创建 Runnable
+        MyRunnable r1 = new MyRunnable();
+        MyRunnable r2 = new MyRunnable();
+
+        // 创建线程
+        Thread thread1 = new Thread(r1, "even");
+        Thread thread2 = new Thread(r2, "odd");
+
+        // 启动线程
+        thread1.start();
+        thread2.start();
+    }
+
+    // 1.拿到锁，就打印
+    // 2.打印完，唤醒其他线程，自己就休眠
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (count <= MAX_NUM) {
+                synchronized (lock) {
+                    // 拿到锁就打印
+                    System.out.println(Thread.currentThread().getName() + ": " + count++);
+                    // 唤醒另一个线程
+                    lock.notify();
+                    // 任务没有结束，则当前运行线程进入睡眠
+                    if (count <= MAX_NUM) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
