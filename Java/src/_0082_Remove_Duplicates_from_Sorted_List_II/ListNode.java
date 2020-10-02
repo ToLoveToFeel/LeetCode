cb10938f231e54cec604112878e7231735b15c79
wxx <1137247975@qq.com>
2020-10-02 11:00:12 +0800

    Leetcode 0082 删除排序链表中的重复元素II

---
+++ Java/src/_0082_Remove_Duplicates_from_Sorted_List_II/ListNode.java
@@ -0,0 +1,14 @@
+package _0082_Remove_Duplicates_from_Sorted_List_II;
+
+/**
+ * Date: 2020/9/4 18:05
+ * Content:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ Java/src/_0082_Remove_Duplicates_from_Sorted_List_II/Solution.java
@@ -0,0 +1,45 @@
+package _0082_Remove_Duplicates_from_Sorted_List_II;
+
+/**
+ * Date: 2020/10/2 10:40
+ * Content:
+ * 画个图就明白了
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return head;
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        // 创建三个游标
+        ListNode preNode = dummyHead;
+        ListNode curNode = head;
+        ListNode nextNode = curNode.next;
+
+        while (nextNode != null) {
+            while (nextNode != null && curNode.val == nextNode.val) {
+                nextNode = nextNode.next;
+            }
+            if (curNode.next != nextNode)  // 说明存在重复
+                preNode.next = nextNode;
+            else  // 说明不存在重复
+                preNode = curNode;
+            curNode = nextNode;
+            if (nextNode != null)
+                nextNode = nextNode.next;
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+//        ListNode head = (new MyLinkedList(new int[]{1, 1, 2, 3, 3, 4})).getHead();
+//        ListNode head = (new MyLinkedList(new int[]{1, 1, 1})).getHead();
+//        ListNode head = (new MyLinkedList(new int[]{1, 1, 1, 2, 3})).getHead();
+//        ListNode head = (new MyLinkedList(new int[]{1, 2, 3, 4, 5})).getHead();
+        ListNode head = (new MyLinkedList(new int[]{1, 2, 2, 2, 5})).getHead();
+        head = (new Solution()).deleteDuplicates(head);
+        MyLinkedList.printLinkedList(head);
+    }
+}
