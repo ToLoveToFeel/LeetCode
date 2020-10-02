e0371ed7e8eb36e05810fa21738d70716c7c520a
wxx <1137247975@qq.com>
2020-10-02 10:37:02 +0800

    Leetcode 0083 删除排序链表中的重复元素

---
+++ Java/src/_0083_Remove_Duplicates_from_Sorted_List/Solution.java
@@ -0,0 +1,32 @@
+package _0083_Remove_Duplicates_from_Sorted_List;
+
+/**
+ * Date: 2020/10/2 10:20
+ * Content:
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return head;
+
+        ListNode curNode = head;
+        ListNode nextNode = head.next;
+
+        while (nextNode != null) {
+            if (curNode.val == nextNode.val) {
+                curNode.next = nextNode.next;
+            } else {
+                curNode = nextNode;
+            }
+            nextNode = nextNode.next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = (new MyLinkedList(new int[]{1, 1, 2, 3, 3})).getHead();
+        head = (new Solution()).deleteDuplicates(head);
+        MyLinkedList.printLinkedList(head);
+    }
+}
