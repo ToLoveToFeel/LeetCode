087a394a6ee68434bdd42300758946296598b426
wxx <1137247975@qq.com>
2020-10-08 10:42:50 +0800

    Leetcode 0371 两整数之和

---
+++ Java/src/_0371_Sum_of_Two_Integers/Solution.java
@@ -0,0 +1,21 @@
+package _0371_Sum_of_Two_Integers;
+
+/**
+ * Date: 2020/10/8 10:19
+ * Content:
+ */
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int x = a, y = b;
+            a = (x ^ y);
+            b = ((x & y) << 1);
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).getSum(13, 11));
+    }
+}
