af7e68b6997ea885c8617be277a0390742392979
wxx <1137247975@qq.com>
2020-10-12 08:43:34 +0800

    Leetcode 0530 二叉搜索树的最小绝对差

---
+++ Java/src/_0530_Minimum_Absolute_Difference_in_BST/TreeNode.java
@@ -0,0 +1,10 @@
+package _0530_Minimum_Absolute_Difference_in_BST;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
