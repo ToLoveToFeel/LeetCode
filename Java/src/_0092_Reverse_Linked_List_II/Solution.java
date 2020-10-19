0b17832063e54550e5ed9347261257416d2a131d
wxx <1137247975@qq.com>
2020-10-19 16:44:08 +0800

    Leetcode 0092 反转链表II

--- Java/src/_0092_Reverse_Linked_List_II/Solution.java
+++ Java/src/_0092_Reverse_Linked_List_II/Solution.java
@@ -3,23 +3,18 @@ package _0092_Reverse_Linked_List_II;
 public class Solution {
     // 时间复杂度：O(n)
     // 空间复杂度：O(1)
-    // Definition for singly-linked list.
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-    public ListNode left = new ListNode(0);  // 第n+1个结点
+    public ListNode left = new ListNode(0);  // 第 n+1 个结点
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
 
         ListNode pre = dummyHead;
-        for (int i = 0; i < m - 1; i++, pre = pre.next);
+        for (int i = 0; i < m - 1; i++, pre = pre.next) ;
 
         ListNode tail = pre.next;  // 翻转后链表的结尾
 
+        // 将反转后的链表接入原本链表中
         pre.next = reverse(pre.next, n - m);
         tail.next = left;
 
@@ -27,55 +22,27 @@ public class Solution {
     }
 
     // 返回反转后链表头指针
-    private ListNode reverse(ListNode head, int index){
-        if (0 == index){
+    private ListNode reverse(ListNode head, int index) {
+        if (index == 0) {
             left = head.next;
             return head;
         }
 
-        ListNode tail = head.next;
         ListNode ret = reverse(head.next, index - 1);
-        tail.next = head;
+        head.next.next = head;
+
         return ret;
-    }
-
-    // 创建链表
-    public ListNode createLinkedList(int[] arr){
-        if (0 == arr.length)
-            return null;
-
-        ListNode head = new ListNode(arr[0]);
-
-        ListNode curNode = head;
-        for (int i = 1; i < arr.length; i++){
-            curNode.next = new ListNode(arr[i]);
-            curNode = curNode.next;
-        }
-
-        return head;
-    }
-
-    // 打印链表
-    public void printLinkedList(ListNode head){
-        ListNode curNode = head;
-        while (null != curNode){
-            System.out.print(curNode.val);
-            System.out.print("->");
-            curNode = curNode.next;
-        }
-        System.out.println("NULL");
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         Solution solution = new Solution();
 
-        // 创建链表
-        ListNode head = solution.createLinkedList(arr);
-        solution.printLinkedList(head);
+        // 创建链表，获取链表头结点
+        ListNode head = (new MyLinkedList(new int[]{1, 2, 3, 4, 5})).getHead();
         // 翻转链表
         head = solution.reverseBetween(head, 2, 4);
         // 输出链表
-        solution.printLinkedList(head);
+        MyLinkedList.printLinkedList(head);
     }
 }
