40e7dcf3a6586fd398116adb88c5a75301e43d5b
wxx <1137247975@qq.com>
2020-10-19 16:31:15 +0800

    Leetcode 0445 两数相加II

---
+++ Java/src/_0445_Add_Two_Numbers_II/ListNode.java
@@ -0,0 +1,14 @@
+package _0445_Add_Two_Numbers_II;
+
+/**
+ * Date: 2020/9/4 18:05
+ * Content:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
