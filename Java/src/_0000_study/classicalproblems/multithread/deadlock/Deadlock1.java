9cfe1c9e80fa718edd05bb3f617b221fd0383d9a
wxx <1137247975@qq.com>
2020-10-23 20:18:01 +0800

    classicalproblems multithread 死锁

---
+++ Java/src/_0000_study/classicalproblems/multithread/deadlock/Deadlock1.java
@@ -0,0 +1,56 @@
+package _0000_study.classicalproblems.multithread.deadlock;
+
+/**
+ * Date: 2020/10/23 20:00
+ * Content:
+ */
+public class Deadlock1 {
+    public static void main(String[] args) {
+        // 创建 Runnable
+        DeadlockRunnable r1 = new DeadlockRunnable();
+        DeadlockRunnable r2 = new DeadlockRunnable();
+        r1.flag = 1;
+        r2.flag = 2;
+
+        // 创建线程
+        Thread thread1 = new Thread(r1);
+        Thread thread2 = new Thread(r2);
+
+        // 启动线程
+        thread1.start();
+        thread2.start();
+    }
+
+    static class DeadlockRunnable implements Runnable {
+        int flag;
+        private static final Object lock1 = new Object();
+        private static final Object lock2 = new Object();
+
+        @Override
+        public void run() {
+            if (flag == 1) {
+                synchronized (lock1) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock2) {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+            } else if (flag == 2) {
+                synchronized (lock2) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock1) {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Java/src/_0000_study/classicalproblems/multithread/deadlock/Deadlock2.java
@@ -0,0 +1,70 @@
+package _0000_study.classicalproblems.multithread.deadlock;
+
+/**
+ * Date: 2020/10/23 20:00
+ * Content:
+ */
+public class Deadlock2 {
+    public static final String PATH = "_0000_study.classicalproblems.multithread.deadlock.";
+
+    public static void main(String[] args) {
+        // 创建 Runnable
+        MyRunnable r1 = new MyRunnable('A');
+        MyRunnable r2 = new MyRunnable('B');
+
+        // 创建线程
+        Thread thread1 = new Thread(r1);
+        Thread thread2 = new Thread(r2);
+
+        // 启动线程
+        thread1.start();
+        thread2.start();
+    }
+
+    static class MyRunnable implements Runnable{
+        private char flag;
+
+        public MyRunnable(char flag) {
+            this.flag = flag;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Class.forName(Deadlock2.PATH + "Static" + flag);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class StaticA {
+    static {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        try {
+            Class.forName(Deadlock2.PATH + "StaticB");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("StaticA init OK");
+    }
+}
+
+class StaticB {
+    static {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        try {
+            Class.forName(Deadlock2.PATH + "StaticA");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("StaticB init OK");
+    }
+}
