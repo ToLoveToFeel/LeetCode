118ba4cabbbd78aa16ae93fd049330a7a875f6d5
SimpleLifeSimpleLove <1137247975@qq.com>
2020-10-27 15:06:15 +0800

    Leetcode 0066 加一

---
+++ Java/src/_0066_Plus_One/Solution.java
@@ -0,0 +1,40 @@
+package _0066_Plus_One;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Date: 2020/10/27 14:46
+ * Content:
+ */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        List<Integer> list = new ArrayList<>();
+
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int digit = digits[i];
+            int num = (digit + carry) % 10;
+            carry = (digit + carry) / 10;
+            list.add(num);
+        }
+        if (carry == 1)
+            list.add(carry);
+
+        Collections.reverse(list);
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9};
+        int[] res = (new Solution()).plusOne(digits);
+        for (int num : res) {
+            System.out.println(num + " ");
+        }
+    }
+}
---
+++ Java/src/_0066_Plus_One/Solution2.java
@@ -0,0 +1,30 @@
+package _0066_Plus_One;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Date: 2020/10/27 14:46
+ * Content:
+ */
+public class Solution2 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9};
+        int[] res = (new Solution2()).plusOne(digits);
+        for (int num : res) {
+            System.out.println(num + " ");
+        }
+    }
+}
