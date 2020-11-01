4525d2d4b9156afc82de7b42665ef73116a88d02
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-01 10:21:55 +0800

    _redis finished

---
+++ Java/src/_0000_study/_redis/connectionpool/JedisPoolTest.java
@@ -0,0 +1,25 @@
+package _0000_study._redis.connectionpool;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Date: 2020/11/1 10:12
+ * Content:
+ */
+public class JedisPoolTest {
+
+    public static void main(String[] args) {
+        // 获取数据库连接池
+        JedisPool jedisPool = JedisPoolUtil.getInstance();
+        // 获取数据库连接池中的一个实例
+        Jedis jedis = jedisPool.getResource();
+        // 清当前数据库
+        jedis.flushDB();
+        // 向数据库中写入数据 并 获得写入的数据
+        jedis.set("key", "value");
+        System.out.println(jedis.get("key"));
+        // 将连接还回数据库连接池
+        JedisPoolUtil.release(jedisPool, jedis);
+    }
+}
