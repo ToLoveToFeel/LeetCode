30a5a9013545f75df7081898daed95bb2739196f
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-04 16:27:10 +0800

    Leetcode 0551 学生出勤记录 I

---
+++ Java/src/_0551_Student_Attendance_Record_I/Solution.java
@@ -0,0 +1,45 @@
+package _0551_Student_Attendance_Record_I;
+
+/**
+ * Date: 2020/11/4 16:04
+ * Content:
+ * 执行用时：
+ * 1 ms, 在所有 Java 提交中击败了52.84%的用户
+ * 内存消耗：36.5 MB, 在所有 Java 提交中击败了82.95%的用户
+ */
+public class Solution {
+    public static final Character ABSENT = 'A';  // 缺勤
+    public static final Character LATE = 'L';  // 迟到
+    public static final Character PRESENT = 'P';  // 到场
+
+    public boolean checkRecord(String s) {
+        if (s.length() <= 1)
+            return true;
+
+        int countAbsent = 0;  // 记录缺勤的次数
+        int countLate = 0;  // 记录连续迟到的次数
+        for (char c : s.toCharArray()) {
+            if (c == ABSENT) {
+                countAbsent++;
+                if (countAbsent >= 2)
+                    return false;
+                countLate = 0;
+            } else if (c == LATE) {
+                if (countLate >= 2)
+                    return false;
+                countLate++;
+            } else {  // c == PRESENT
+                countLate = 0;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        String s = "PPALLP";
+//        System.out.println((new Solution()).checkRecord(s));  // true
+        String s = "PPALLL";
+        System.out.println((new Solution()).checkRecord(s));  // false
+    }
+}
