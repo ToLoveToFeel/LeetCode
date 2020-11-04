74f486587f235d7a2f03c91e26d317beb31ee94e
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-04 10:35:59 +0800

    区间问题

--- Java/src/_0056_Merge_Intervals/Solution.java
+++ Java/src/_0056_Merge_Intervals/Solution.java
@@ -5,58 +5,53 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Solution {
-    // 1、首先对列表中的元素基于第一个元素排序；
-    // 2、遍历列表，如果其中的元素对的右节点比下一个元素对的左节点大，则代表上下两个元素对是重叠的，
-    //    递归的执行这个过程，找到所有的重叠部分
+
     public int[][] merge(int[][] intervals) {
         if (intervals.length <= 1)
             return intervals;
+        // 1、首先对列表中的元素基于第一个元素从小到大排序；
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
 
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                return o1[0] - o2[0];
-            }
-        });
-
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < intervals.length; i++){
+        // 2、遍历列表，如果其中的元素对的右节点比下一个元素对的左节点大，则代表上下两个元素对是重叠的，
+        //    递归的执行这个过程，找到所有的重叠部分
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
             int left = intervals[i][0];
             int right = intervals[i][1];
 
-            while (i+1 < intervals.length && intervals[i+1][0] <= right){
-                right = Math.max(right, intervals[i+1][1]);
+            while (i + 1 < intervals.length && intervals[i + 1][0] <= right) {
+                right = Math.max(right, intervals[i + 1][1]);
                 i++;
             }
-            res.add(new ArrayList<Integer>(Arrays.asList(left, right)));
+            lists.add(new ArrayList<>(Arrays.asList(left, right)));
         }
+        // 整理结果
+        int[][] res = new int[lists.size()][lists.get(0).size()];
+        for (int i = 0; i < lists.size(); i++)
+            for (int j = 0; j < lists.get(0).size(); j++)
+                res[i][j] = lists.get(i).get(j);
 
-        int[][] ret = new int[res.size()][res.get(0).size()];
-        for (int i = 0; i < res.size(); i++)
-            for (int j = 0; j < res.get(0).size(); j++)
-                ret[i][j] = res.get(i).get(j);
-
-        return ret;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[][] intervals = {
-                {1, 3},
-                {2, 6},
-                {8, 10},
-                {15, 18}
-        };
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
 
-        int[][] ret = (new Solution()).merge(intervals);
+        int[][] res = (new Solution()).merge(intervals);
 
-        for (int i = 0; i < ret.length; i++){
+        System.out.print("[");
+        for (int i = 0; i < res.length; i++) {
             System.out.print("[");
-            for (int j = 0; j < ret[0].length; j++){
-                System.out.print(ret[i][j]);
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.print(res[i][j]);
                 if (j == 0)
                     System.out.print(" ");
             }
-            System.out.println("]");
+            if (i != res.length - 1)
+                System.out.print("], ");
+            else
+                System.out.print("]");
         }
+        System.out.println("]");
     }
 }
--- Java/src/_0057_Insert_Interval/Solution2.java
+++ Java/src/_0057_Insert_Interval/Solution2.java
@@ -2,62 +2,63 @@ package _0057_Insert_Interval;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+/**
+ * 执行用时：3 ms, 在所有 Java 提交中击败了46.07%的用户
+ * 内存消耗：40.7 MB, 在所有 Java 提交中击败了86.97%的用户
+ */
 
 public class Solution2 {
     // [[1,2],[3,5],[6,7],[8,10],[12,16]]
     //         [4,         8]
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
 
-        int cur = 0;
+        int cur = 0;  // 找到第一处newInterval区间左边的值 小于或等于 intervals区间右边的值的位置
         while (cur < intervals.length && intervals[cur][1] < newInterval[0]) {
-            res.add(new ArrayList<>(Arrays.asList(intervals[cur][0], intervals[cur][1])));
+            lists.add(new ArrayList<>(Arrays.asList(intervals[cur][0], intervals[cur][1])));
             cur++;
         }
 
-        while (cur < intervals.length && intervals[cur][0] <= newInterval[1]){
+        while (cur < intervals.length && intervals[cur][0] <= newInterval[1]) {
             newInterval[0] = Math.min(intervals[cur][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[cur][1], newInterval[1]);
             cur++;
         }
-        res.add(new ArrayList<>(Arrays.asList(newInterval[0], newInterval[1])));
+        lists.add(new ArrayList<>(Arrays.asList(newInterval[0], newInterval[1])));
 
         while (cur < intervals.length) {
-            res.add(new ArrayList<>(Arrays.asList(intervals[cur][0], intervals[cur][1])));
+            lists.add(new ArrayList<>(Arrays.asList(intervals[cur][0], intervals[cur][1])));
             cur++;
         }
 
         // 整理为数组返回
-        int[][] ret = new int[res.size()][res.get(0).size()];
-        for (int i = 0; i < res.size(); i++)
-            for (int j = 0; j < res.get(0).size(); j++)
-                ret[i][j] = res.get(i).get(j);
+        int[][] res = new int[lists.size()][lists.get(0).size()];
+        for (int i = 0; i < lists.size(); i++)
+            for (int j = 0; j < lists.get(0).size(); j++)
+                res[i][j] = lists.get(i).get(j);
 
-        return ret;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[][] intervals = {
-                {1, 2},
-                {3, 5},
-                {6, 7},
-                {8, 10},
-                {12, 16}
-        };
-
+        int[][] intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
         int[] newInterval = {4, 8};
 
-        int[][] ret = (new Solution2()).insert(intervals, newInterval);
+        int[][] res = (new Solution2()).insert(intervals, newInterval);
 
-        for (int i = 0; i < ret.length; i++){
+        System.out.print("[");
+        for (int i = 0; i < res.length; i++) {
             System.out.print("[");
-            for (int j = 0; j < ret[0].length; j++){
-                System.out.print(ret[i][j]);
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.print(res[i][j]);
                 if (j == 0)
                     System.out.print(" ");
             }
-            System.out.println("]");
+            if (i != res.length - 1)
+                System.out.print("], ");
+            else
+                System.out.print("]");
         }
+        System.out.println("]");
     }
 }
