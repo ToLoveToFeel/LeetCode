9b52bbaaf9dcad50da95d1922c0042f105e4a325
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-10 10:05:17 +0800

    排列组合问题

--- Java/src/_0046_Permutations/Solution.java
+++ Java/src/_0046_Permutations/Solution.java
@@ -12,21 +12,20 @@ public class Solution {
 
     // p中保存了一个有index个元素的排列
     // 向这个排列的末尾添加第 index+1 个元素，获得一个有 index+1 个元素的排列
-    private void generatePermutation(int[] nums, int index, LinkedList<Integer> p){
-        if (index == nums.length){
-            res.add((LinkedList<Integer>)p.clone());
+    private void generatePermutation(int[] nums, int index, LinkedList<Integer> p) {
+        if (index == nums.length) {
+            res.add((LinkedList<Integer>) p.clone());
             return;
         }
 
         for (int i = 0; i < nums.length; i++)
-            if (!used[i]){
+            if (!used[i]) {
                 p.addLast(nums[i]);
                 used[i] = true;
                 generatePermutation(nums, index + 1, p);
                 p.removeLast();
                 used[i] = false;
             }
-        return;
     }
 
     public List<List<Integer>> permute(int[] nums) {
@@ -42,8 +41,8 @@ public class Solution {
         return res;
     }
 
-    private static void printList(List<Integer> list){
-        for(Integer e: list)
+    private static void printList(List<Integer> list) {
+        for (Integer e : list)
             System.out.print(e + " ");
         System.out.println();
     }
@@ -51,7 +50,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
         List<List<Integer>> res = (new Solution()).permute(nums);
-        for (List<Integer> list:res)
+        for (List<Integer> list : res)
             printList(list);
     }
 }
