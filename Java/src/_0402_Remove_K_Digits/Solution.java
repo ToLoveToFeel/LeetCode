ad8d1258318110369d7575ee30f8c9bd98e298c7
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-15 11:45:51 +0800

    Leetcode 0402 移除K位数字

---
+++ Java/src/_0402_Remove_K_Digits/Solution.java
@@ -0,0 +1,50 @@
+package _0402_Remove_K_Digits;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Date: 2020/11/15 11:27
+ * Content: 栈(单调递增栈)
+ * https://leetcode-cn.com/problems/remove-k-digits/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-5/
+ *
+ * 执行用时：12 ms, 在所有 Java 提交中击败了39.25%的用户
+ * 内存消耗：39.1 MB, 在所有 Java 提交中击败了33.83%的用户
+ */
+public class Solution {
+
+    public String removeKdigits(String num, int k) {
+        if (num.length() == k)
+            return "0";
+
+        Deque<Character> stack = new ArrayDeque<>();
+        int len = num.length() - k;  // 最终数据的长度
+        for (char c : num.toCharArray()) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > c){
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        String res = sb.reverse().substring(0, len).replaceAll("^(0+)", "");
+        return res.equals("") ? "0" : res;
+    }
+
+    public static void main(String[] args) {
+
+//        String num = "1432219";  // 1219
+//        int k = 3;
+//        System.out.println((new Solution()).removeKdigits(num, k));
+//        String num = "100200";  // 200
+//        int k = 1;
+//        System.out.println((new Solution()).removeKdigits(num, k));
+        String num = "10";  // 200
+        int k = 1;
+        System.out.println((new Solution()).removeKdigits(num, k));
+    }
+}
