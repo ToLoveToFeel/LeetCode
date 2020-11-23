7cb7a3327240d4bef487413db6af2a941e31850d
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-23 09:28:59 +0800

    Leetcode 0452 用最少数量的箭引爆气球

---
+++ Java/src/_0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution.java
@@ -0,0 +1,51 @@
+package _0452_Minimum_Number_of_Arrows_to_Burst_Balloons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Date: 2020/11/23 9:07
+ * Content:
+ * <p>
+ * 执行用时：25 ms, 在所有 Java 提交中击败了26.85%的用户
+ * 内存消耗：45.9 MB, 在所有 Java 提交中击败了86.53%的用户
+ */
+public class Solution {
+
+    public int findMinArrowShots(int[][] points) {
+
+        int n = points.length;
+        if (n == 0)
+            return 0;
+
+        // 第一步：根据point[i][0]从小到大进行排序
+        Arrays.sort(points, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+
+        // 第二步：合并区间(不同于Leetcode 0056寻找并集,这里找的区间是交集)，根据剩余区间的个数，得到结果
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < points.length; i++) {
+            int left = points[i][0];
+            int right = points[i][1];
+
+            while (i + 1 < points.length && points[i + 1][0] <= right) {
+                left = points[i + 1][0];
+                right = Math.min(right, points[i + 1][1]);
+                i++;
+            }
+            lists.add(new ArrayList<>(Arrays.asList(left, right)));
+        }
+
+        return lists.size();
+    }
+
+    public static void main(String[] args) {
+
+//        int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};  // 2
+//        int[][] points = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};  // 4
+//        int[][] points = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};  // 2
+//        int[][] points = {{1, 2}};  // 1
+//        int[][] points = {{2, 3}, {2, 3}};  // 1
+        int[][] points = {{-2147483646, -2147483645}, {2147483646, 2147483647}};  // 2
+        System.out.println((new Solution()).findMinArrowShots(points));
+    }
+}
