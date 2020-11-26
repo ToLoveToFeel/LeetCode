a07412e12adff462a93dfdcb03226abb176e4fcf
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-26 09:15:59 +0800

    classicalproblems

---
+++ Java/src/_0000_study/classicalproblems/lcs/Solution2.java
@@ -0,0 +1,44 @@
+package _0000_study.classicalproblems.lcs;
+
+/**
+ * Date: 2020/11/21 16:02
+ * Content: 动态规划
+ * 状态：dp[m+1][n+1]：text1[0...m] 和 text2[0...n] 的最长子序列的长度
+ * 状态转移：
+ * dp[m][n] = 1 + dp[m-1][n-1]   if text1[m] == text2[n]
+ * dp[m][n] = Math.max(dp[m-1][n], dp[m][n-1])   if text1[m] != text2[n]
+ * <p>
+ * 执行用时：11 ms, 在所有 Java 提交中击败了79.57%的用户
+ * 内存消耗：42.3 MB, 在所有 Java 提交中击败了61.26%的用户
+ */
+public class Solution2 {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        int len1 = text1.length();
+        int len2 = text2.length();
+
+        if (len1 == 0 || len2 == 0)
+            return 0;
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                if (text1.charAt(i) == text2.charAt(j))
+                    dp[i + 1][j + 1] = 1 + dp[i][j];
+                else
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+
+        return dp[len1][len2];
+    }
+
+    public static void main(String[] args) {
+
+        String text1 = "abcde", text2 = "ace";  // 3
+//        String text1 = "abc", text2 = "def";  // 0
+        System.out.println((new Solution2()).longestCommonSubsequence(text1, text2));
+    }
+}
---
+++ Java/src/_0000_study/classicalproblems/lis/Solution.java
@@ -0,0 +1,38 @@
+package _0000_study.classicalproblems.lis;
+
+/**
+ * Date: 2020/9/3 20:26
+ * Content:
+ * 自顶向下，无记忆化搜索，这种方式不行，因为要记录0到任意位置对应数组的最长上升子序列，最后求这里面的最大值
+ * error
+ */
+public class Solution {
+    // [0...index] 数组对应最长上升子序列
+    private int lengthOfLIS(int[] nums, int index) {
+        if (index == 0)
+            return 1;
+
+        int max = 1;
+        for (int i = 0; i < index; i++) {
+            int submax = lengthOfLIS(nums, i);
+            if (nums[index] > nums[i])
+                max = Math.max(max, 1 + submax);
+        }
+        return max;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
+        return lengthOfLIS(nums, nums.length - 1);
+    }
+
+    public static void main(String[] args) {
+
+//        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+//        int[] nums = {-2, -1};
+        int[] nums = {1, 3, 6, 7, 9, 4, 10, 5, 6};
+        System.out.println((new Solution()).lengthOfLIS(nums));
+    }
+}
