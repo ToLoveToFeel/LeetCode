76ef4cba63ad8d227cb24900e013d6f475f02ecc
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-03 16:29:58 +0800

    Leetcode 0704 二分查找

--- Java/src/_0704_Binary_Search/Solution.java
+++ Java/src/_0704_Binary_Search/Solution.java
@@ -5,9 +5,12 @@ package _0704_Binary_Search;
  * Content:
  */
 public class Solution {
+
     public int search(int[] nums, int target) {
+
         int left = 0, right = nums.length - 1;
         while (left <= right) {
+
             int mid = (right - left) / 2 + left;
             if (target == nums[mid])
                 return mid;
@@ -21,6 +24,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 9;
         System.out.println((new Solution()).search(nums, target));
