fadd30aed439f6a7e3d21580eb3b55715859b5b9
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-07 11:38:01 +0800

    Leetcode 0067 二进制加和

--- Java/src/_0067_Add_Binary/Solution2.java
+++ Java/src/_0067_Add_Binary/Solution2.java
@@ -1,36 +1,45 @@
 package _0067_Add_Binary;
 
+/**
+ * 时间复杂度：O(max(len(a), len(b)))
+ * 空间复杂度：O(1)
+ */
 public class Solution2 {
-    // 时间复杂度：O(max(len(a), len(b)))
-    // 空间复杂度：O(1)
+
     public String addBinary(String a, String b) {
+
         // 转为字符数组
         StringBuilder sb = new StringBuilder();
         char[] res = (a.length() > b.length() ? a : b).toCharArray();
         char[] adder = (a.length() > b.length() ? b : a).toCharArray();
+
         // res加上adder，有些位置可能为2，之后出处理
-        int index = res .length - 1;
-        for (int i = adder.length - 1; i >= 0; i--){
+        int index = res.length - 1;
+        for (int i = adder.length - 1; i >= 0; i--) {
             res[index] += adder[i] - '0';
             index--;
         }
+
         // 处理进位问题
-        for (int i = res.length - 1; i > 0; i--)
-            if (res[i] > '1'){
+        for (int i = res.length - 1; i > 0; i--) {
+            if (res[i] > '1') {
                 res[i - 1] += 1;  // 进位
-                res[i] = (char)('0' + (res[i] - '0') % 2);
+                res[i] = (char) ('0' + (res[i] - '0') % 2);
             }
-        if (res[0] > '1'){
-            res[0] = (char)('0' + (res[0] - '0') % 2);
+        }
+        if (res[0] > '1') {
+            res[0] = (char) ('0' + (res[0] - '0') % 2);
             sb.append('1');
         }
         sb.append(res);
+
         return sb.toString();
     }
 
     public static void main(String[] args) {
-        String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
-        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+        String a = "11";
+        String b = "1";
         System.out.println((new Solution2()).addBinary(a, b));
     }
 }
---
+++ Java/src/_0067_Add_Binary/Solution3.java
@@ -0,0 +1,41 @@
+package _0067_Add_Binary;
+
+/**
+ * Date: 2020/12/7 11:27
+ * Content: 模拟数学运算
+ * 类似于Leetcode 0002 两数相加
+ * 执行用时：2 ms, 在所有 Java 提交中击败了98.62%的用户
+ * 内存消耗：38.4 MB, 在所有 Java 提交中击败了32.11%的用户
+ */
+public class Solution3 {
+
+    public String addBinary(String a, String b) {
+
+        // 第一步：翻转字符串，方便运算
+        char[] s1 = new StringBuilder(a).reverse().toString().toCharArray();
+        char[] s2 = new StringBuilder(b).reverse().toString().toCharArray();
+
+        // 第二步：计算得到结果
+        int t = 0;  // 保存进位
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s1.length || i < s2.length; i++) {
+            int va = (i >= s1.length ? 0 : s1[i] - '0');
+            int vb = (i >= s2.length ? 0 : s2[i] - '0');
+            int sum = va + vb + t;
+            t = sum / 2;
+            sum %= 2;
+            sb.append(sum);
+        }
+        if (t != 0)
+            sb.append(t);
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        String a = "11";
+        String b = "1";
+        System.out.println((new Solution3()).addBinary(a, b));
+    }
+}
