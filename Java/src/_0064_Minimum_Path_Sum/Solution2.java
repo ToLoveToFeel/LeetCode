1795990f82b6d13cddf19d9abd54d7150d1ff06d
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-09 17:17:37 +0800

    Leetcode 0064 最小路径和

--- Java/src/_0064_Minimum_Path_Sum/Solution2.java
+++ Java/src/_0064_Minimum_Path_Sum/Solution2.java
@@ -2,17 +2,18 @@ package _0064_Minimum_Path_Sum;
 
 import java.util.Arrays;
 
+/**
+ * 记忆化搜索(在Solution的基础上)：自顶向下
+ * 1.假如我们就在最右下角的格子(也可以想象成网格只有一个格子)，那么最短路径和就是格子中的值；
+ * 2.然后假如我们在最后一行的格子中，假如是grid[grid.length-1][j]，那么从这个点出发到最右下角的
+ *  最小路径和就是本身加上它左边的格子到最右下角的最小路径和。最后一列和最后一行是同理的。
+ * 3.一个普通的位置，它到最右下角的最小路径和是多少呢，是它右边一个位置和它下面一个位置的最小
+ *  路径和中最小的那个加上它自己格子的值。
+ */
 public class Solution2 {
-    /*
-    记忆化搜索(在Solution的基础上)：自顶向下
-        1.假如我们就在最右下角的格子(也可以想象成网格只有一个格子)，那么最短路径和就是格子中的值；
-        2.然后假如我们在最后一行的格子中，假如是grid[grid.length][j]，那么从这个点出发到最右下角的
-          最小路径和就是本身加上它左边的格子到最右下角的最小路径和。最后一列和最后一行是同理的。
-        3.一个普通的位置，它到最右下角的最小路径和是多少呢，是它右边一个位置和它下面一个位置的最小
-          路径和中最小的那个加上它自己格子的值。
-     */
-    private int n;
+
     private int m;
+    private int n;
     private int[][] dp;
 
     public int minPathSum(int[][] grid) {
@@ -20,11 +21,11 @@ public class Solution2 {
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0)
             return 0;
 
-        n = grid.length;
-        m = grid[0].length;
+        m = grid.length;
+        n = grid[0].length;
 
-        dp = new int[n][m];
-        for (int i = 0; i < n; i++)
+        dp = new int[m][n];
+        for (int i = 0; i < m; i++)
             Arrays.fill(dp[i], -1);
 
         return rec(grid, 0, 0);
@@ -33,15 +34,15 @@ public class Solution2 {
     // 返回从(i,j)出发到右下格的最短距离
     private int rec(int[][] grid, int i, int j) {
 
-        if (i == n - 1 && j == m - 1)
+        if (i == m - 1 && j == n - 1)
             return grid[i][j];
         if (dp[i][j] != -1)  // 已经计算过，直接返回
             return dp[i][j];
 
         int min;
-        if (i == n - 1)  // 在最后一行
+        if (i == m - 1)  // 在最后一行
             min = grid[i][j] + rec(grid, i, j + 1);
-        else if (j == m - 1)  // 最后一列
+        else if (j == n - 1)  // 最后一列
             min = grid[i][j] + rec(grid, i + 1, j);
         else
             min = grid[i][j] + Math.min(rec(grid, i + 1, j), rec(grid, i, j + 1));
--- Java/src/_0064_Minimum_Path_Sum/Solution3.java
+++ Java/src/_0064_Minimum_Path_Sum/Solution3.java
@@ -1,23 +1,33 @@
 package _0064_Minimum_Path_Sum;
 
+/**
+ * 动态规划：自底向上
+ *
+ * 执行用时：3 ms, 在所有 Java 提交中击败了86.74%的用户
+ * 内存消耗：41.2 MB, 在所有 Java 提交中击败了75.14%的用户
+ */
 public class Solution3 {
-    /*
-    动态规划：自底向上
-     */
+
     public int minPathSum(int[][] grid) {
+
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0)
             return 0;
 
-        int r = grid.length - 1;
-        int c = grid[0].length - 1;
-        int[][] dp = new int[r + 1][c + 1];
-        dp[r][c] = grid[r][c];
-        for (int i = r - 1; i >= 0; i--)
-            dp[i][c] = dp[i + 1][c] + grid[i][c];  // 填充最后一列的
-        for (int j = c - 1; j >= 0; j--)
-            dp[r][j] = dp[r][j + 1] + grid[r][j];  // 填充最后一行的
-        for (int i = r - 1; i >= 0; i--) {  // 填充其他的
-            for (int j = c - 1; j >= 0; j--) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+
+        // 初始化
+        for (int i = m - 2; i >= 0; i--)
+            dp[i][n - 1] = dp[i + 1][n - 1] + grid[i][n - 1];  // 填充最后一列
+        for (int j = n - 2; j >= 0; j--)
+            dp[m - 1][j] = dp[m - 1][j + 1] + grid[m - 1][j];  // 填充最后一行
+
+        // 计算
+        for (int i = m - 2; i >= 0; i--) {  // 填充其他的
+            for (int j = n - 2; j >= 0; j--) {
                 dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
             }
         }
