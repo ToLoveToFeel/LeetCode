5a754eaccb636a4062c5c856d4cba8296f7c10c0
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-15 14:17:50 +0800

    Leetcode 0125 验证回文串

--- Java/src/_0125_Valid_Palindrome/Solution.java
+++ Java/src/_0125_Valid_Palindrome/Solution.java
@@ -1,23 +1,28 @@
 package _0125_Valid_Palindrome;
 
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 public class Solution {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(1)
+
     public boolean isPalindrome(String s) {
+
         s = s.toLowerCase();  // 全部转化为小写字母
         int i = 0;
         int j = s.length() - 1;
 
-        while (i < j){
+        while (i < j) {
+
             while (i < s.length() && !isValidCharacter(s.charAt(i)))
                 i++;
             if (i == s.length())  // 全部为非字母
                 return true;
 
-            while (j > 0 &&!isValidCharacter(s.charAt(j)))
+            while (j > 0 && !isValidCharacter(s.charAt(j)))
                 j--;
 
-            if(s.charAt(i) != s.charAt(j))
+            if (s.charAt(i) != s.charAt(j))
                 return false;
             i++;
             j--;
@@ -26,7 +31,8 @@ public class Solution {
         return true;
     }
 
-    private boolean isValidCharacter(Character c){
+    private boolean isValidCharacter(Character c) {
+
         return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z');
     }
 
--- Java/src/_0125_Valid_Palindrome/Solution3.java
+++ Java/src/_0125_Valid_Palindrome/Solution3.java
@@ -1,18 +1,24 @@
 package _0125_Valid_Palindrome;
 
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ */
 public class Solution3 {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(n)
+
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()) return true;
-        StringBuilder bld = new StringBuilder(s.replaceAll("[^a-zA-Z0-9]", ""));
-        return bld.toString().equalsIgnoreCase(bld.reverse().toString());
+
+        if (s.isEmpty())
+            return true;
+
+        StringBuilder sb = new StringBuilder(s.replaceAll("[^a-zA-Z0-9]", ""));
+        return sb.toString().equalsIgnoreCase(sb.reverse().toString());
     }
 
 
     public static void main(String[] args) {
-        String s = ".,";
 
+        String s = ".,";
         System.out.println((new Solution3()).isPalindrome(s));
     }
 }
