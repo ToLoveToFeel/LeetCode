f75c634e087b55f1f4eb139c157e5b6f8f0a664e
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-23 09:40:21 +0800

    Leetcode 0387 字符串中的第一个唯一字符

--- Java/src/_0387_First_Unique_Character_in_a_String/Solution.java
+++ Java/src/_0387_First_Unique_Character_in_a_String/Solution.java
@@ -1,19 +1,28 @@
 package _0387_First_Unique_Character_in_a_String;
 
-class Solution {
+/**
+ * 哈希表
+ * 执行用时：7 ms, 在所有 Java 提交中击败了82.57%的用户
+ * 内存消耗：38.9 MB, 在所有 Java 提交中击败了78.04%的用户
+ */
+public class Solution {
+
     public int firstUniqChar(String s) {
+
         int[] nums = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            nums[s.charAt(i) - 'a'] ++;
+            nums[s.charAt(i) - 'a']++;
         }
         for (int i = 0; i < s.length(); i++) {
-            if (1 == nums[s.charAt(i) - 'a'])
+            if (nums[s.charAt(i) - 'a'] == 1) {
                 return i;
+            }
         }
         return -1;
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).firstUniqChar("loveleetcode"));
     }
 }
