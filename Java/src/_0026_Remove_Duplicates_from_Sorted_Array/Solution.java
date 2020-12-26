43abbfe5b701135f66936935be7a7a50e66cbf33
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-26 22:23:07 +0800

    Leetcode 0026 删除排序数组的重复项

--- Java/src/_0026_Remove_Duplicates_from_Sorted_Array/Solution.java
+++ Java/src/_0026_Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -3,9 +3,32 @@ package _0026_Remove_Duplicates_from_Sorted_Array;
 /**
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)
+ * 执行用时：1 ms, 在所有 Java 提交中击败了83.16%的用户
+ * 内存消耗：40.2 MB, 在所有 Java 提交中击败了72.84%的用户
  */
-class Solution {
+public class Solution {
 
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++)
+            if (i == 0 || nums[i] != nums[i - 1])
+                nums[j++] = nums[i];
+        // nums[0] ~ num[j - 1] 所有a中不重复的数
+        return j;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println((new Solution()).removeDuplicates(nums));
+    }
+}
+
+/*
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0)
             return 0;
@@ -20,10 +43,4 @@ class Solution {
 
         return k;
     }
-
-    public static void main(String[] args) {
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-
-        System.out.println((new Solution()).removeDuplicates(nums));
-    }
-}
+ */
