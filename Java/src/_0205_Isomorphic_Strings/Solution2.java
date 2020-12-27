0d64320b4b1f52825abdf76b8cd9d12a92c7c138
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-27 10:10:51 +0800

    Leetcode 0205 同构字符串

--- Java/src/_0205_Isomorphic_Strings/Solution2.java
+++ Java/src/_0205_Isomorphic_Strings/Solution2.java
@@ -1,11 +1,15 @@
-// Author  :  Wang Xiaoxiao
-// Time    :  2020-01-26
 package _0205_Isomorphic_Strings;
 
+/**
+ * 时间复杂度：O(len(s))
+ * 空间复杂度：O(len of charset)
+ * 执行用时：5 ms, 在所有 Java 提交中击败了90.32%的用户
+ * 内存消耗：38.5 MB, 在所有 Java 提交中击败了62.34%的用户
+ */
 public class Solution2 {
-    // 时间复杂度：O(len(s))
-    // 空间复杂度：O(len of charset)
+
     public boolean isIsomorphic(String s, String t) {
+
         assert s.length() == t.length();
 
         if (s.length() == 0 || s.length() == 1)
@@ -19,7 +23,7 @@ public class Solution2 {
         }
 
         for (int i = 0; i < s.length(); i++)
-            if (-1 == map[s.charAt(i)]) {  // s[i]还未映射到其他字母
+            if (map[s.charAt(i)]  == -1) {  // s[i]还未映射到其他字母
                 if (mapped[t.charAt(i)])  // t[i]被别的字母所映射过了
                     return false;
                 map[s.charAt(i)] = t.charAt(i);
@@ -32,6 +36,7 @@ public class Solution2 {
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution2()).isIsomorphic("egg", "add"));
         System.out.println((new Solution2()).isIsomorphic("foo", "bar"));
         System.out.println((new Solution2()).isIsomorphic("paper", "title"));
