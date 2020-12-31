9452f5fab953af3abdf737b6fbab0d39080c8163
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-31 12:58:29 +0800

    init

---
+++ Cplusplus/_0088_Merge_Sorted_Array/_0088_main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+
+    }
+};
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
