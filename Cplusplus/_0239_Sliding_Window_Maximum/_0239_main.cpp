2c1576c28dd2c637a0790243944c4c20077b6e68
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-31 15:32:18 +0800

    Leetcode 0239 滑动窗口最大值

---
+++ Cplusplus/_0239_Sliding_Window_Maximum/_0239_main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 100010;
+int q[N];
+int hh = 0, tt = -1;
+
+/**
+ * 执行用时：516 ms, 在所有 C++ 提交中击败了35.59%的用户
+ * 内存消耗：116.3 MB, 在所有 C++ 提交中击败了22.09%的用户
+ */
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hh <= tt && i - k + 1 > q[hh]) hh++;
+            while (hh <= tt && nums[q[tt]] <= nums[i]) tt--;
+            q[++tt] = i;
+            if (i >= k - 1) res.push_back(nums[q[hh]]);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+
+//    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+//    int k = 3;
+    vector<int> nums = {1};
+    int k = 1;
+    vector<int> res = Solution().maxSlidingWindow(nums, k);
+    for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+
+    return 0;
+}
---
+++ Cplusplus/_0239_Sliding_Window_Maximum/_0239_main2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+/**
+ * 代码正确
+ * heap-buffer-overflow
+ */
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        vector<int> res;
+        deque<int> q;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!q.empty() && i - k + 1 > q.front()) q.pop_front();
+            while (!q.empty() && nums[q.back()] <= nums[i]) q.pop_back();
+            q.push_back(i);
+            if (i >= k - 1) res.push_back(nums[q.front()]);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+
+    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+//    vector<int> nums = {1};
+//    int k = 1;
+    vector<int> res = Solution().maxSlidingWindow(nums, k);
+    for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+
+    return 0;
+}
