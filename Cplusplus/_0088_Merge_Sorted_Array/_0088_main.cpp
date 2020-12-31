bca3566d8718be02b35be21e012ba6598e2a829b
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-31 15:11:59 +0800

    Leetcode 0088 合并两个有序数组

--- Cplusplus/_0088_Merge_Sorted_Array/_0088_main.cpp
+++ Cplusplus/_0088_Merge_Sorted_Array/_0088_main.cpp
@@ -3,14 +3,35 @@
 
 using namespace std;
 
+/**
+ * 执行用时：4 ms, 在所有 C++ 提交中击败了82.96%的用户
+ * 内存消耗：9.1 MB, 在所有 C++ 提交中击败了72.72%的用户
+ */
 class Solution {
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
 
+        m--, n--;
+        for (int i = nums1.size() - 1; i >= 0; i--) {
+            if (n < 0) nums1[i] = nums1[m--];
+            else if (m < 0) nums1[i] = nums2[n--];
+            else if (nums1[m] >= nums2[n]) nums1[i] = nums1[m--];
+            else nums1[i] = nums2[n--];
+        }
     }
 };
 
 int main() {
-    cout << "hello" << endl;
+
+//    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+//    vector<int> nums2 = {2, 5, 6};
+//    Solution().merge(nums1, 3, nums2, 3);
+//    for (int i = 0; i < nums1.size(); ++i) cout << nums1[i] << " ";
+
+    vector<int> nums1 = {0};
+    vector<int> nums2 = {1};
+    Solution().merge(nums1, 0, nums2, 1);
+    for (int i = 0; i < nums1.size(); ++i) cout << nums1[i] << " ";
+
     return 0;
 }
