3a1200ce8da79036c9797e1244c36082378049c9
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-03 22:12:31 +0800

    Leetcode 0002 两数之和

--- Java/src/_0002_Add_Two_Numbers/Solution.java
+++ Java/src/_0002_Add_Two_Numbers/Solution.java
@@ -3,9 +3,9 @@ package _0002_Add_Two_Numbers;
 /**
  * Date: 2020/8/27 10:08
  * Content:
- * 时间复杂度：O(max(m, n))O(max(m,n))，假设 m 和 n 分别表示 l1 和 l2 的长度，
- * 上面的算法最多重复 max(m, n)max(m,n) 次。
- * 空间复杂度：O(max(m, n))O(max(m,n))， 新列表的长度最多为 max(m,n) + max(m,n)+1。
+ * 时间复杂度：O(max(m, n))，假设 m 和 n 分别表示 l1 和 l2 的长度，
+ * 上面的算法最多重复 max(m, n) 次。
+ * 空间复杂度：O(max(m, n))， 新列表的长度最多为 max(m,n) + 1。
  */
 class Solution {
 
@@ -28,8 +28,7 @@ class Solution {
             p = p.next;
         }
         // 解决，两个链表都结束，但最高位（进位）不为0的问题
-        if (temp != 0)
-            p.next = new ListNode(temp);
+        if (temp != 0) p.next = new ListNode(temp);
 
         return dummyHead.next;
     }
