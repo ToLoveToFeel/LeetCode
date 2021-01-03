dc6f1987707634c9e52a4c3f32fdaaaf10d7024e
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-03 16:32:50 +0800

    Leetcode 0347 前K个高频元素

--- Java/src/_0347_Top_K_Frequent_Elements/Solution.java
+++ Java/src/_0347_Top_K_Frequent_Elements/Solution.java
@@ -1,64 +1,48 @@
 package _0347_Top_K_Frequent_Elements;
 
+import _0000_study._common.Output;
+
 import java.util.*;
 
+/**
+ * 执行用时：17 ms, 在所有 Java 提交中击败了41.86%的用户
+ * 内存消耗：40.9 MB, 在所有 Java 提交中击败了83.93%的用户
+ */
 class Solution {
-    private class Freq implements Comparable<Freq> {
-        public int e, freq;
-
-        public Freq(int e, int freq) {
-            this.e = e;
-            this.freq = freq;
-        }
-
-        @Override
-        public int compareTo(Freq another) {
-            if (this.freq < another.freq)
-                return -1;
-            else if (this.freq > another.freq)
-                return 1;
-            else
-                return 0;
-        }
-    }
 
     public int[] topKFrequent(int[] nums, int k) {
+
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        for (int num : nums) {
-            if (hashMap.containsKey(num))
-                hashMap.put(num, hashMap.get(num) + 1);
-            else
-                hashMap.put(num, 1);
-        }
+        // 统计词频
+        for (int num : nums)
+            if (hashMap.containsKey(num)) hashMap.put(num, hashMap.get(num) + 1);
+            else hashMap.put(num, 1);
 
-        PriorityQueue<Freq> priorityQueue = new PriorityQueue<>();
+        // 使用优先队列解决
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(
+                (a, b) -> hashMap.get(a) - hashMap.get(b)
+        );  // 最小堆
         for (int key : hashMap.keySet()) {
-            if (priorityQueue.size() < k)
-                priorityQueue.add(new Freq(key, hashMap.get(key)));
-            else if (hashMap.get(key) > priorityQueue.peek().freq) {
+            if (priorityQueue.size() < k) priorityQueue.add(key);
+            else if (hashMap.get(key) > hashMap.get(priorityQueue.peek())) {
                 priorityQueue.remove();
-                priorityQueue.add(new Freq(key, hashMap.get(key)));
+                priorityQueue.add(key);
             }
         }
 
         ArrayList<Integer> ret = new ArrayList<>();
-        while (!priorityQueue.isEmpty()) {
-            ret.add(priorityQueue.remove().e);
-        }
+        while (!priorityQueue.isEmpty()) ret.add(priorityQueue.remove());
         // 转换为数组
         int[] res = new int[ret.size()];
-        for (int i = 0; i < ret.size(); i++) {
-            res[i] = ret.get(i);
-        }
+        for (int i = 0; i < ret.size(); i++) res[i] = ret.get(i);
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 2, 1, 1, 1, 1, 3, 3, 3, 7, 7, 7, 7, 7, 7, 5, 5, 4, 6, 6, 6};
-        int[] res = (new Solution()).topKFrequent(nums, 1);
-        for (int num : res) {
-            System.out.print(num + " ");
-        }
+
+        int[] nums = new int[]{2, 2, 1, 1, 1, 1, 3, 3, 3, 7, 7, 7, 7, 7, 7, 5, 5, 4, 6, 6, 6};
+        int[] res = (new Solution()).topKFrequent(nums, 2);
+        Output.OutputBasicArray1D(res);
     }
 }
