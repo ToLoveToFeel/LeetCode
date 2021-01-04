e5e1365bc26adb4a34a67b1a9ea01a83ccbecd1e
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-04 16:21:46 +0800

    Leetcode 0006 Z字形变换

--- Java/src/_0006_ZigZag_Conversion/Solution.java
+++ Java/src/_0006_ZigZag_Conversion/Solution.java
@@ -8,9 +8,10 @@ import java.util.List;
  * Content:
  */
 public class Solution {
+
     public String convert(String s, int numRows) {
-        if (numRows == 1)
-            return s;
+
+        if (numRows == 1) return s;
 
         List<StringBuilder> list = new ArrayList<>();
         for (int i = 0; i < Math.min(numRows, s.length()); i++) {
@@ -21,7 +22,7 @@ public class Solution {
         int curRow = 0;
         for (char c : s.toCharArray()) {
             list.get(curRow).append(c);
-            if (curRow == 0 || curRow == numRows-1)
+            if (curRow == 0 || curRow == numRows - 1)
                 down = !down;
             curRow += (down ? 1 : -1);
         }
@@ -33,6 +34,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         String s = "LEETCODEISHIRING";
         int numRows = 3;
         System.out.println((new Solution()).convert(s, numRows));
