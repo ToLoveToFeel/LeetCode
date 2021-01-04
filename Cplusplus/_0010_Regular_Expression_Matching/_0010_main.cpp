fe84ad8b1c227ad19ded7c8ec53e02767deda1cb
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-04 17:58:06 +0800

    Leetcode 0010 正则表达式匹配

---
+++ Cplusplus/_0010_Regular_Expression_Matching/_0010_main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * 状态：f[i][j] : s的前i个字符是否匹配 p的前j个字符
+ * 执行用时：8 ms, 在所有 C++ 提交中击败了76.63%的用户
+ * 内存消耗：7.2 MB, 在所有 C++ 提交中击败了49.69%的用户
+ */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+
+        int n = s.size(), m = p.size();
+        s  = ' ' + s, p = ' ' + p;
+        vector<vector<bool>> f(n + 1, vector<bool>(m + 1));
+        f[0][0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (j + 1 <= m && p[j + 1] == '*') continue;
+                if (i && p[j] != '*') {
+                    f[i][j] = f[i - 1][j - 1] && (s[i] == p[j] || p[j] == '.');
+                } else if (p[j] == '*') {
+                    f[i][j] = f[i][j - 2] || (i && f[i - 1][j] && (s[i] == p[j - 1] || p[j - 1] == '.'));
+                }
+            }
+        }
+
+        return f[n][m];
+    }
+};
+
+int main() {
+
+    cout << Solution().isMatch("aa", "a") << endl;  // false
+    cout << Solution().isMatch("aa", "a*") << endl;  // true
+    cout << Solution().isMatch("ab", ".*") << endl;  // true
+    cout << Solution().isMatch("aab", "c*a*b") << endl;  // true
+
+    return 0;
+}
