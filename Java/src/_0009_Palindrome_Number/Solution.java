fd5b53dde2594718f4baeec546fb97a2d1c6fac3
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-04 17:20:35 +0800

    Leetcode 0009 回文数

--- Java/src/_0009_Palindrome_Number/Solution.java
+++ Java/src/_0009_Palindrome_Number/Solution.java
@@ -8,8 +8,7 @@ public class Solution {
 
     public boolean isPalindrome(int x) {
 
-        if (x < 0)
-            return false;
+        if (x < 0) return false;
         String s = x + "";
 
         int left = 0;
