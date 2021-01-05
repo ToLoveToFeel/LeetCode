df5aee80c2eda69bf44b5b115e4ae1a57fc1e7b8
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-05 19:01:37 +0800

    Leetcode 0033 搜索旋转排序数组

--- Java/src/_0033_Search_in_Rotated_Sorted_Array/Solution.java
+++ Java/src/_0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -8,7 +8,9 @@ package _0033_Search_in_Rotated_Sorted_Array;
  * 空间复杂度：O(1)
  */
 public class Solution {
+
     public int search(int[] nums, int target) {
+
         // 核心思想：在有序的一遍查询是否存在target，如果不存在，去无序的一遍查询target
         int left = 0, right = nums.length - 1;  // 在[left...right]中搜索target，关键要确保target在[left...right]之中
         while (left <= right) {
@@ -31,6 +33,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
         System.out.println((new Solution()).search(nums, target));
