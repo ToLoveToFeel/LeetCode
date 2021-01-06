178c097de53bae747b450b2de7ce9654484d5027
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-06 17:43:29 +0800

    Leetcode 0039 组合总和

--- Java/src/_0039_Combination_Sum/Solution.java
+++ Java/src/_0039_Combination_Sum/Solution.java
@@ -11,36 +11,36 @@ import java.util.List;
  * 0377 需要求解出结果个数(顺序不同的序列被视作不同的组合)
  */
 public class Solution {
-    private ArrayList<List<Integer>> res;
+
+    private ArrayList<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> c = new LinkedList<>();
 
     // 求解该问题，当前已经找到的组合存储在c中，需要从start开始搜索新的元素
-    private void generateCombination(int[] candidates, int target, int start, LinkedList<Integer> c) {
-        if (target < 0)
-            return;
+    private void generateCombination(int[] cs, int target, int start) {
+
+        if (target < 0) return;
         if (target == 0) {
             res.add((List<Integer>) c.clone());
             return;
         }
 
         // 从start开始，而不是从0开始，可以防止出现重复组合
-        for (int i = start; i < candidates.length; i++) {
-            c.add(candidates[i]);
+        for (int i = start; i < cs.length; i++) {
+            c.add(cs[i]);
             // 不从 i+1 开始(Leetcode 0040是从 i+1开始的)，目的是允许数字重复
-            generateCombination(candidates, target - candidates[i], i, c);
+            generateCombination(cs, target - cs[i], i);
             c.removeLast();
         }
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        res = new ArrayList<>();
-        LinkedList<Integer> c = new LinkedList<>();
-
-        generateCombination(candidates, target, 0, c);
+        generateCombination(candidates, target, 0);
         return res;
     }
 
     public static void main(String[] args) {
+
         int[] candidates = {2, 3, 5};
         int target = 8;
         List<List<Integer>> res = (new Solution()).combinationSum(candidates, target);
