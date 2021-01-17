990d0a8e418bac5b6bf4af2364aa1401a6ad8ab6
ToLoveToFeel <1137247975@qq.com>
2021-01-17 21:47:30 +0800

    Leetcode 0107 二叉树的层序遍历 II

---
+++ Cplusplus/_0107_Binary_Tree_Level_Order_Traversal_II/_0107_main.cpp
@@ -0,0 +1,63 @@
+#include "MyTree.h"
+
+/**
+ * 执行用时：12 ms, 在所有 C++ 提交中击败了20.35%的用户
+ * 内存消耗：11.3 MB, 在所有 C++ 提交中击败了97.60%的用户
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root) {
+
+        vector<vector<int>> res;
+        queue<TreeNode *> q;
+        if (root) q.push(root);
+
+        while (q.size()) {
+            vector<int> level;
+            int len = q.size();
+
+            while (len--) {
+                auto t = q.front();
+                q.pop();
+                level.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            res.push_back(level);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+void OutputBasicArray2D1(vector<vector<int>> &nums) {
+
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && i < nums.size()) cout << ' ';
+        cout << "[";
+        for (int j = 0; j < nums[i].size(); j++) {
+            cout << nums[i][j];
+            if (j != nums[i].size() - 1) cout << ", ";
+        }
+        cout << "]";
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+
+    int nu = INT_MAX;
+    vector<int> nums = {
+            3,
+            9, 20,
+            nu, nu, 15, 7
+    };
+    TreeNode *root = MyTree(nums).getRoot();
+    vector<vector<int>> res = Solution().levelOrderBottom(root);
+    OutputBasicArray2D1(res);
+
+    return 0;
+}
