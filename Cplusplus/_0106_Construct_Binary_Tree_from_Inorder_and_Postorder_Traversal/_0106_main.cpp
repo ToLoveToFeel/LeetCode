9a114fc50ae16c7bf99f8db294483fc95dad871e
ToLoveToFeel <1137247975@qq.com>
2021-01-17 21:35:15 +0800

    Leetcode 0106 从中序遍历与后续遍历序列构造二叉树

--- Cplusplus/_0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/_0106_main.cpp
+++ Cplusplus/_0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/_0106_main.cpp
@@ -2,26 +2,26 @@
 #include <unordered_map>
 
 /**
- * 执行用时：16 ms, 在所有 C++ 提交中击败了94.20%的用户
- * 内存消耗：17.1 MB, 在所有 C++ 提交中击败了92.84%的用户
+ * 执行用时：20 ms, 在所有 C++ 提交中击败了84.03%的用户
+ * 内存消耗：17.2 MB, 在所有 C++ 提交中击败了92.27%的用户
  */
 class Solution {
 public:
 
     unordered_map<int, int> pos;  // 存储中序遍历元素对应的下标值
 
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
 
         for (int i = 0; i < inorder.size(); i++) pos[inorder[i]] = i;
-        return build(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+        return build(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1);
     }
 
-    TreeNode *build(vector<int> &preorder, vector<int> &inorder, int pl, int pr, int il, int ir) {
-        if (pl > pr) return NULL;
-        auto root = new TreeNode(preorder[pl]);
+    TreeNode *build(vector<int> &inorder, vector<int> &postorder, int il, int ir, int pl, int pr) {
+        if (il > ir) return NULL;
+        auto root = new TreeNode(postorder[pr]);
         int k = pos[root->val];
-        root->left = build(preorder, inorder, pl + 1, pl + 1 + k - 1 - il, il, k - 1);
-        root->right = build(preorder, inorder, pl + 1 + k - 1 - il + 1, pr, k + 1, ir);
+        root->left = build(inorder, postorder, il, k - 1, pl, pl + k - 1 - il);
+        root->right = build(inorder, postorder, k + 1, ir, pl + k - 1 - il + 1, pr - 1);
         return root;
     }
 };
@@ -51,10 +51,10 @@ int main() {
             nu, nu, 15, 7
     };
     MyTree myTree = MyTree(nums);
-    vector<int> preorder = myTree.preorder();
     vector<int> inorder = myTree.inorder();
+    vector<int> postorder = myTree.postorder();
 
-    TreeNode *root = Solution().buildTree(preorder, inorder);
+    TreeNode *root = Solution().buildTree(inorder, postorder);
     vector<vector<int>> res = MyTree().levelOrder(root);
     OutputBasicArray2D1(res);
 
