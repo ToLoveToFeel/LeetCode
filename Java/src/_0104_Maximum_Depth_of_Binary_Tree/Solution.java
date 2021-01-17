0e548a2b7098c807865d9f659b8dc0ddd9ad98f9
ToLoveToFeel <1137247975@qq.com>
2021-01-17 20:01:25 +0800

    Leetcode 0104 二叉树的最大深度

--- Java/src/_0104_Maximum_Depth_of_Binary_Tree/Solution.java
+++ Java/src/_0104_Maximum_Depth_of_Binary_Tree/Solution.java
@@ -1,12 +1,24 @@
 package _0104_Maximum_Depth_of_Binary_Tree;
 
 public class Solution {
-    // 递归算法
-    // 该函数语义：返回以root为根的树的高度
-    public int maxDepth(TreeNode root) {
-        if (null == root)
-            return 0;
 
+    // 返回以root为根的树的高度
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
+        int[] nums = new int[]{
+                3,
+                9, 20,
+                nu, nu, 15, 7
+        };
+
+        TreeNode root = (new MyTree(nums)).getRoot();
+        System.out.println((new Solution()).maxDepth(root));
     }
 }
--- Java/src/_0104_Maximum_Depth_of_Binary_Tree/Solution2.java
+++ Java/src/_0104_Maximum_Depth_of_Binary_Tree/Solution2.java
@@ -4,28 +4,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution2 {
-    // 队列
+
     // BFS
-    public int maxDepth(TreeNode root){
-        if (null == root)
-            return 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         int levelNum = 1;  // 记录当前层的数量
         int maxDepth = 0;  // 记录深度
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int newLevelNum = 0;
             maxDepth++;
-            for (int i = 0; i < levelNum; i++){
+            for (int i = 0; i < levelNum; i++) {
                 TreeNode node = queue.remove();
 
-                if (node.left != null){
+                if (node.left != null) {
                     queue.add(node.left);
                     newLevelNum++;
                 }
-                if (node.right != null){
+                if (node.right != null) {
                     queue.add(node.right);
                     newLevelNum++;
                 }
@@ -33,5 +32,18 @@ public class Solution2 {
             levelNum = newLevelNum;
         }
         return maxDepth;
+    }
+
+    public static void main(String[] args) {
+
+        int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
+        int[] nums = new int[]{
+                3,
+                9, 20,
+                nu, nu, 15, 7
+        };
+
+        TreeNode root = (new MyTree(nums)).getRoot();
+        System.out.println((new Solution2()).maxDepth(root));
     }
 }
