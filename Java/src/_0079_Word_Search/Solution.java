c2ad5e0a338fb5ae6066fbb27d97112df5fdcea2
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 16:57:52 +0800

    Leetcode 0079 单词搜索

--- Java/src/_0079_Word_Search/Solution.java
+++ Java/src/_0079_Word_Search/Solution.java
@@ -6,6 +6,7 @@ package _0079_Word_Search;
  * 典型的二维平面回溯问题
  */
 public class Solution {
+
     int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};  // 四个方向：上右下左
     int m, n;  // board的行数和列数
     boolean[][] visited;  // 记录某个位置是否访问过，访问过为true
@@ -14,30 +15,29 @@ public class Solution {
         return x >= 0 && x < m && y >= 0 && y < n;
     }
 
-    // 从board[startx][starty]开始，向四个方向寻找
-    private boolean searchBoard(char[][] board, String word, int index, int startx, int starty) {
-        if (index == word.length() - 1)
-            return board[startx][starty] == word.charAt(index);
+    // 从board[x][y]开始，向四个方向寻找
+    private boolean searchBoard(char[][] board, String word, int index, int x, int y) {
 
-        if (board[startx][starty] == word.charAt(index)) {
-            visited[startx][starty] = true;
+        if (index == word.length() - 1) return board[x][y] == word.charAt(index);
+
+        if (board[x][y] == word.charAt(index)) {
+            visited[x][y] = true;
             // 从startx,starty出发，向四个方向寻找
             for (int i = 0; i < 4; i++) {
-                int newx = startx + d[i][0];
-                int newy = starty + d[i][1];
-                if (inArea(newx, newy) && !visited[newx][newy] &&
-                        searchBoard(board, word, index + 1, newx, newy))
+                int a = x + d[i][0], b = y + d[i][1];
+                if (inArea(a, b) && !visited[a][b] &&
+                        searchBoard(board, word, index + 1, a, b))
                     return true;
             }
-            visited[startx][starty] = false;
+            visited[x][y] = false;
         }
         return false;
     }
 
     public boolean exist(char[][] board, String word) {
+
         m = board.length;
-        if (board.length == 0)
-            throw new IllegalArgumentException("列数为0!");
+        if (board.length == 0) throw new IllegalArgumentException("列数为0!");
         n = board[0].length;
         visited = new boolean[m][n];  // 默认为false
 
@@ -49,14 +49,15 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         char[][] board = {
                 {'A', 'B', 'C', 'E'},
                 {'S', 'F', 'C', 'S'},
                 {'A', 'D', 'E', 'E'},
         };
         Solution solution = new Solution();
-        System.out.println(solution.exist(board, "ABCCED"));
-        System.out.println(solution.exist(board, "SEE"));
-        System.out.println(solution.exist(board, "ABCB"));
+        System.out.println(solution.exist(board, "ABCCED"));// true
+        System.out.println(solution.exist(board, "SEE"));// true
+        System.out.println(solution.exist(board, "ABCB"));// false
     }
 }
