4c7d75f5eda42abbf30fd71f3146c13229a2de39
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 11:55:53 +0800

    Leetcode 0024 两两交换链表中的节点

--- Java/src/_0024_Swap_Nodes_in_Pairs/Solution.java
+++ Java/src/_0024_Swap_Nodes_in_Pairs/Solution.java
@@ -1,15 +1,21 @@
 package _0024_Swap_Nodes_in_Pairs;
 
+/**
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：36 MB, 在所有 Java 提交中击败了60.91%的用户
+ */
 public class Solution {
+
     public ListNode swapPairs(ListNode head) {
+
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
 
         ListNode p = dummyHead;
         while (p.next != null && p.next.next != null) {
             ListNode node1 = p.next;
-            ListNode node2 = node1.next;
-            ListNode next = node2.next;
+            ListNode node2 = p.next.next;
+            ListNode next = p.next.next.next;
 
             node2.next = node1;
             node1.next = next;
@@ -22,10 +28,10 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         // 创建链表，获取链表头结点
         ListNode head = (new MyLinkedList(new int[]{1, 2, 3, 4})).getHead();
         MyLinkedList.printLinkedList(head);
-
         head = (new Solution()).swapPairs(head);
         MyLinkedList.printLinkedList(head);
     }
