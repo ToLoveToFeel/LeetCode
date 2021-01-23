90b184e5ea7a06090c3e3127253c29d6b03f0627
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 23:17:53 +0800

    Leetcode 0118 杨辉三角

--- Java/src/_0118_Pascal_s_Triangle/Solution.java
+++ Java/src/_0118_Pascal_s_Triangle/Solution.java
@@ -12,20 +12,18 @@ import java.util.List;
  */
 public class Solution {
 
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int n) {
 
         List<List<Integer>> res = new ArrayList<>();
-
-        if (numRows <= 0)
-            return res;
-        if (numRows == 1) {
+        if (n <= 0) return res;
+        if (n == 1) {
             res.add(new ArrayList<Integer>() {{
                 add(1);
             }});
             return res;
         }
 
-        // numRows一定大于等于2
+        // n 一定大于等于2
         res.add(new ArrayList<Integer>() {{
             add(1);
         }});
@@ -34,7 +32,7 @@ public class Solution {
             add(1);
         }});
 
-        for (int i = 2; i < numRows; i++) {  // 从第三行开始填写
+        for (int i = 2; i < n; i++) {  // 从第三行开始填写
             List<Integer> row = new ArrayList<>();
             row.add(1);
             for (int j = 0; j < i - 1; j++)
