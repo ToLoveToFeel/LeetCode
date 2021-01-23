bcae128be1bb37f497c56b0cdf74aa448729cc64
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 10:33:33 +0800

    Leetcode 0011 盛最多水的容器

--- Java/src/_0011_Container_With_Most_Water/Solution.java
+++ Java/src/_0011_Container_With_Most_Water/Solution.java
@@ -1,23 +1,25 @@
 package _0011_Container_With_Most_Water;
 
+/**
+ * 暴力解法
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(1)
+ */
 public class Solution {
-    // 暴力解法
-    // 时间复杂度：O(n^2)
-    // 空间复杂度：O(1)
-    public int maxArea(int[] height) {
-        assert height.length >= 2;
 
-        int area = Math.min(height[0], height[1]);
+    public int maxArea(int[] height) {
+
+        int res = Math.min(height[0], height[1]);
         for (int i = 0; i < height.length; i++)
             for (int j = i + 1; j < height.length; j++)
-                area = Math.max(area, Math.min(height[i], height[j])*(j - i));
+                res = Math.max(res, Math.min(height[i], height[j]) * (j - i));
 
-        return area;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] height = {1,8,6,2,5,4,8,3,7};
 
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println((new Solution()).maxArea(height));
     }
 }
--- Java/src/_0011_Container_With_Most_Water/Solution2.java
+++ Java/src/_0011_Container_With_Most_Water/Solution2.java
@@ -1,31 +1,29 @@
 package _0011_Container_With_Most_Water;
 
+/**
+ * 对撞指针
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ * 执行用时：4 ms, 在所有 Java 提交中击败了66.63%的用户
+ * 内存消耗：40.1 MB, 在所有 Java 提交中击败了25.95%的用户
+ */
 public class Solution2 {
-    // 对撞指针
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(1)
-    public int maxArea(int[] height) {
-        assert height.length >= 2;
 
-        int l = 0;
-        int r = height.length - 1;
-        int maxArea = 0;
-        while (l < r){
-//            maxArea = Math.max(maxArea, Math.min(height[l], height[r])* (r - l));
-            int area = Math.min(height[l], height[r])* (r - l);
-            if (maxArea < area)
-                maxArea = area;
-            if (height[l] < height[r])
-                l++;
-            else
-                r--;
+    public int maxArea(int[] height) {
+
+        int l = 0, r = height.length - 1;
+        int res = 0;
+        while (l < r) {
+            res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) l++;
+            else r--;
         }
-        return maxArea;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] height = {1,8,6,2,5,4,8,3,7};
 
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};  // 49
         System.out.println((new Solution2()).maxArea(height));
     }
 }
