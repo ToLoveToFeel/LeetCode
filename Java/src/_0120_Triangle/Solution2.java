12a6e2cb8430b5396b97ec2ff0f212618d7eb1a9
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 23:34:14 +0800

    Leetcode 0120 三角形最小路径和

--- Java/src/_0120_Triangle/Solution2.java
+++ Java/src/_0120_Triangle/Solution2.java
@@ -13,39 +13,34 @@ import java.util.List;
  * 空间复杂度：O(n^2)
  */
 class Solution2 {
+
     Integer[][] memo;
 
     private int go(List<List<Integer>> triangle, int i, int j) {
-        if (memo[i][j] != -1)
-            return memo[i][j];
-        if (i == 0)
-            return memo[i][j] = triangle.get(i).get(j);
-        if (j == 0)
-            return memo[i][j] = triangle.get(i).get(j) + go(triangle, i - 1, 0);
-        if (j == i)
-            return memo[i][j] = triangle.get(i).get(j) + go(triangle, i - 1, j - 1);
+
+        if (memo[i][j] != -1) return memo[i][j];
+        if (i == 0) return memo[i][j] = triangle.get(i).get(j);
+        if (j == 0) return memo[i][j] = triangle.get(i).get(j) + go(triangle, i - 1, 0);
+        if (j == i) return memo[i][j] = triangle.get(i).get(j) + go(triangle, i - 1, j - 1);
         return memo[i][j] = triangle.get(i).get(j) + Math.min(go(triangle, i - 1, j - 1),
                 go(triangle, i - 1, j));
     }
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return triangle.get(0).get(0);
-        memo = new Integer[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
-        }
 
-        for (int i = 0; i < n; i++) {
-            memo[n - 1][i] = go(triangle, n - 1, i);
-        }
+        int n = triangle.size();
+        if (n == 0) return 0;
+        if (n == 1) return triangle.get(0).get(0);
+
+        memo = new Integer[n][n];
+        for (int i = 0; i < n; i++) Arrays.fill(memo[i], -1);
+
+        for (int i = 0; i < n; i++) memo[n - 1][i] = go(triangle, n - 1, i);
         return Collections.min(Arrays.asList(memo[n - 1]));
     }
 
     public static void main(String[] args) {
+
         List<List<Integer>> triangle = new ArrayList<>();
         triangle.add(new ArrayList<Integer>() {{
             add(2);
--- Java/src/_0120_Triangle/Solution4.java
+++ Java/src/_0120_Triangle/Solution4.java
@@ -7,33 +7,26 @@ import java.util.List;
 
 /**
  * Date: 2020/8/31 15:23
- * Content:
- * 动态规划
+ * Content:动态规划
  * 时间复杂度：O(n^2)
  * 空间复杂度：O(n^2)
+ * 执行用时：3 ms, 在所有 Java 提交中击败了71.73%的用户
+ * 内存消耗：38.4 MB, 在所有 Java 提交中击败了77.51%的用户
  */
-class Solution3 {
+class Solution4 {
 
-    Integer[][] dp;
-
-    // 状态dp[i][j]：从上到下 到达三角形位置(i, j)的最小路径和
+    // 状态dp[i][j]：从下向上 到达三角形位置(i, j)的最小路径和
     public int minimumTotal(List<List<Integer>> t) {
 
         int n = t.size();
-        if (n == 0) return 0;
-        if (n == 1) return t.get(0).get(0);
-        dp = new Integer[n][n];
-        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
 
-        dp[0][0] = t.get(0).get(0);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < t.get(i).size(); j++) {
-                if (j == 0) dp[i][j] = t.get(i).get(j) + dp[i - 1][j];
-                else if (j == i) dp[i][j] = t.get(i).get(j) + dp[i - 1][j - 1];
-                else dp[i][j] = t.get(i).get(j) + Math.min(dp[i - 1][j - 1], dp[i - 1][j]);
-            }
+        int[][] f = new int[n + 5][n + 5];
+        for (int i = 1; i <= n; i++) f[n][i] = t.get(n - 1).get(i - 1);
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++)
+                f[i][j] = Math.min(f[i + 1][j], f[i + 1][j + 1]) + t.get(i - 1).get(j - 1);
         }
-        return Collections.min(Arrays.asList(dp[n - 1]));
+        return f[1][1];
     }
 
     public static void main(String[] args) {
@@ -57,6 +50,6 @@ class Solution3 {
             add(8);
             add(3);
         }});
-        System.out.println((new Solution3()).minimumTotal(triangle));
+        System.out.println((new Solution4()).minimumTotal(triangle));
     }
 }
