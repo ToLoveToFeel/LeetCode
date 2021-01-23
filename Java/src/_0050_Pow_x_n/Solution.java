35e6487f49a69ea99df8dff460f5d67e93a0b0e3
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 14:39:33 +0800

    Leetcode 0050 Pow(x, n)

--- Java/src/_0050_Pow_x_n/Solution.java
+++ Java/src/_0050_Pow_x_n/Solution.java
@@ -1,29 +1,27 @@
 package _0050_Pow_x_n;
 
 public class Solution {
-    public double myPow(double x, int n){
+
+    public double myPow(double x, int n) {
         return pow(x, n);
     }
 
     private double pow(double x, long n) {
-        if (n < 0){
+
+        if (n < 0) {
             x = 1.0 / x;
             n *= -1;
-        }
-        else if (n == 0)
-            return 1;
-        else if (n == 1)
-            return x;
+        } else if (n == 0) return 1;
+        else if (n == 1) return x;
 
-        if (n % 2 == 0){
+        if (n % 2 == 0) {
             double half = pow(x, n / 2);
             return half * half;
-        }
-        else // n % 2 == 1
-            return x * pow(x, n - 1);
+        } else return x * pow(x, n - 1);  // n % 2 == 1
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).myPow(2.0, 10));
         System.out.println((new Solution()).myPow(2.1, 3));
         System.out.println((new Solution()).myPow(2.0, -2));
