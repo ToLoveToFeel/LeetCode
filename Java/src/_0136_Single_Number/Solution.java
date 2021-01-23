2864bbd57f3427516b60bb01b0f50b738d7e0c4f
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 00:03:33 +0800

    Leetcode 0136 只出现一次的数字

--- Java/src/_0136_Single_Number/Solution.java
+++ Java/src/_0136_Single_Number/Solution.java
@@ -9,9 +9,7 @@ public class Solution {
     public int singleNumber(int[] nums) {
 
         int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            res ^= nums[i];
-        }
+        for (int num : nums) res ^= num;
         return res;
     }
 
