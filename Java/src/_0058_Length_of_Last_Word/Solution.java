67a27ebaaf69515fec451e3600d8fbe4d8bd9173
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 15:20:04 +0800

    Leetcode 0058 最后一个单词的长度

--- Java/src/_0058_Length_of_Last_Word/Solution.java
+++ Java/src/_0058_Length_of_Last_Word/Solution.java
@@ -1,25 +1,23 @@
 package _0058_Length_of_Last_Word;
 
+/**
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：36.8 MB, 在所有 Java 提交中击败了53.95%的用户
+ */
 public class Solution {
+
     public int lengthOfLastWord(String s) {
-        s = trimBothEndsChars(s, " ");
-        if (s.length() == 0)
-            return 0;
 
-        int size = s.length();
-        int count = 0;
-        while (size-1-count >= 0 && s.charAt(size-1-count) != ' ')
-            count++;
-        return count;
-    }
+        s = s.trim();
+        if (s.length() == 0) return 0;
 
-    private static String trimBothEndsChars(String srcStr, String splitter) {
-//        String regex = "^" + splitter + "*|" + splitter + "*$";  // 前后均匹配
-        String regex = splitter + "*$";  // 只匹配后面
-        return srcStr.replaceAll(regex, "");
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) != ' ') i--;
+        return s.length() - 1 - i;
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).lengthOfLastWord(" Hello World "));
     }
 }
--- Java/src/_0058_Length_of_Last_Word/Solution2.java
+++ Java/src/_0058_Length_of_Last_Word/Solution2.java
@@ -1,16 +1,17 @@
 package _0058_Length_of_Last_Word;
 
 public class Solution2 {
+
     public int lengthOfLastWord(String s) {
-        if (s.length() == 0)
-            return 0;
+
+        if (s.length() == 0) return 0;
 
         int size = s.length();
         int count = 0;
-        while (size-1-count >= 0 && s.charAt(size-1-count) == ' ')
+        while (size - 1 - count >= 0 && s.charAt(size - 1 - count) == ' ')
             count++;
         int blank = count;
-        while (size-1-count >= 0 && s.charAt(size-1-count) != ' ')
+        while (size - 1 - count >= 0 && s.charAt(size - 1 - count) != ' ')
             count++;
 
         return count - blank;
