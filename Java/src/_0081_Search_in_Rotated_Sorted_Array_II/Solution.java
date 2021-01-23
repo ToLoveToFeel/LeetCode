c8924fe40d807104f448c4b56d9ce34e316e6cd2
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 17:04:27 +0800

    Leetcode 0081 搜索旋转排序数组II

--- Java/src/_0081_Search_in_Rotated_Sorted_Array_II/Solution.java
+++ Java/src/_0081_Search_in_Rotated_Sorted_Array_II/Solution.java
@@ -8,7 +8,9 @@ package _0081_Search_in_Rotated_Sorted_Array_II;
  * 内存消耗：37.9 MB, 在所有 Java 提交中击败了99.86%的用户
  */
 public class Solution {
+
     public boolean search(int[] nums, int target) {
+
         for (int num : nums)
             if (num == target)
                 return true;
@@ -16,6 +18,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         int[] nums = {2, 5, 6, 0, 0, 1, 2};
         int target = 2;
         System.out.println((new Solution()).search(nums, target));
