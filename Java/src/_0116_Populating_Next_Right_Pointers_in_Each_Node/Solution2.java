9c30550ffd5c1592defef330a0fe8767846685ef
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 23:12:07 +0800

    Leetcode 0116 填充每个节点的下一个右侧节点指针

--- Java/src/_0116_Populating_Next_Right_Pointers_in_Each_Node/Solution2.java
+++ Java/src/_0116_Populating_Next_Right_Pointers_in_Each_Node/Solution2.java
@@ -8,27 +8,24 @@ import java.util.Queue;
  * Content:
  */
 public class Solution2 {
+
     public Node connect(Node root) {
-		if (root == null)
-            return root;
+
+        if (root == null) return root;
 
         Node leftMost = root;
         while (leftMost.left != null) {  // 条件成立的话，说明完全二叉树还有下一层
             Node head = leftMost;  // 当前层的头结点
-
             while (head != null) {
                 // 1: 左子树节点 指向 右子树节点
                 head.left.next = head.right;
                 // 2: 右子树节点 指向 head.next的左子树节点
                 if (head.next != null)
                     head.right.next = head.next.left;
-
                 head = head.next;
             }
-
             leftMost = leftMost.left;
         }
-
         return root;
     }
 }
