da6998a0414ecc8ee60e701677142e4ec06cd842
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 15:02:35 +0800

    Leetcode 0054 螺旋矩阵

--- Java/src/_0054_Spiral_Matrix/Solution.java
+++ Java/src/_0054_Spiral_Matrix/Solution.java
@@ -1,51 +1,47 @@
 package _0054_Spiral_Matrix;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 时间复杂度：O(N*M)
+ * 空间复杂度：O(N*M)
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：36.8 MB, 在所有 Java 提交中击败了8.33%的用户
+ */
 class Solution {
-    // 时间复杂度：O(N*M)
-    // 空间复杂度：O(N*M)
+
     public int[][] d = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public int N, M;  // 行数，列数
 
     public List<Integer> spiralOrder(int[][] matrix) {
+
         N = matrix.length;
-        if (N == 0)
-            return new ArrayList<Integer>();
+        if (N == 0) return new ArrayList<>();
         M = matrix[0].length;
-        if (M == 0)
-            return new ArrayList<Integer>();
+        if (M == 0) return new ArrayList<>();
 
-        boolean[][] visited = new boolean[N][M];
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                visited[i][j] = false;
+        boolean[][] st = new boolean[N][M];
+        for (int i = 0; i < N; i++) Arrays.fill(st[i], false);
 
-        int curd = 0, curx = 0, cury = 0;
         ArrayList<Integer> res = new ArrayList<>();
-        while (res.size() < N * M){
-            if (!visited[curx][cury]){
-                res.add(matrix[curx][cury]);
-                visited[curx][cury] = true;
+        int dir = 0, x = 0, y = 0;
+        while (res.size() < N * M) {
+            if (!st[x][y]) {
+                res.add(matrix[x][y]);
+                st[x][y] = true;
             }
-            int nextx = curx + d[curd][0];
-            int nexty = cury + d[curd][1];
-            if(inArea(nextx, nexty) && !visited[nextx][nexty]){
-                curx = nextx;
-                cury = nexty;
-            }
-            else
-                curd = (curd + 1) % 4;
+            int a = x + d[dir][0], b = y + d[dir][1];
+            if (a >= 0 && a < N && b >= 0 && b < M && !st[a][b]) {
+                x = a;
+                y = b;
+            } else dir = (dir + 1) % 4;
         }
         return res;
     }
 
-    private boolean inArea(int x, int y){
-        return x >= 0 && x < N && y >= 0 && y < M;
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
