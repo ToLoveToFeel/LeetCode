9fa93716846f3289ce8f3a9238e20c1a4afbf97c
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-23 11:36:11 +0800

    https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/

--- Java/src/_0019_Remove_Nth_Node_From_End_of_List/Solution.java
+++ Java/src/_0019_Remove_Nth_Node_From_End_of_List/Solution.java
@@ -1,10 +1,14 @@
 package _0019_Remove_Nth_Node_From_End_of_List;
 
+/**
+ * 链表遍历了两次
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 public class Solution {
-    // 时间复杂度：O(n)
-    // 链表遍历了两次
-    // 空间复杂度：O(1)
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
         int count = 0;  // 存储链表元素个数
         int delNum = 0;  // 存储正数要删除的元素位置，head位置为1
 
@@ -19,8 +23,7 @@ public class Solution {
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
         ListNode p = dummyHead;
-        for (int i = 0; i < delNum - 1; i++)
-            p = p.next;
+        for (int i = 0; i < delNum - 1; i++) p = p.next;
 
         // p后面的元素需要删除
         p.next = p.next.next;
@@ -29,10 +32,10 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         int n = 2;
         // 创建链表，获取链表头结点
         ListNode head = (new MyLinkedList(new int[]{1, 2, 3, 4, 5})).getHead();
-
         head = (new Solution()).removeNthFromEnd(head, n);
         MyLinkedList.printLinkedList(head);
     }
--- Java/src/_0019_Remove_Nth_Node_From_End_of_List/Solution2.java
+++ Java/src/_0019_Remove_Nth_Node_From_End_of_List/Solution2.java
@@ -1,19 +1,19 @@
 package _0019_Remove_Nth_Node_From_End_of_List;
 
+/**
+ * 链表遍历了一次
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 public class Solution2 {
-    // 时间复杂度：O(n)
-    // 链表遍历了一次
-    // 空间复杂度：O(1)
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (n <= 0)
-            throw new IllegalArgumentException("n must grater then 0!");
+
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
 
-        ListNode p = dummyHead;
-        ListNode q = dummyHead;
-        for (int i = 0; i < n + 1; i++)
-            q = q.next;
+        ListNode p = dummyHead, q = dummyHead;
+        for (int i = 0; i < n + 1; i++) q = q.next;
 
         while (q != null) {
             p = p.next;
@@ -21,15 +21,14 @@ public class Solution2 {
         }
 
         p.next = p.next.next;
-
         return dummyHead.next;
     }
 
     public static void main(String[] args) {
+
         int n = 2;
         // 创建链表，获取链表头结点
         ListNode head = (new MyLinkedList(new int[]{1, 2, 3, 4, 5})).getHead();
-
         head = (new Solution2()).removeNthFromEnd(head, n);
         MyLinkedList.printLinkedList(head);
     }
