2404050896686de42564bf93d9b691efd9108a77
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 14:44:38 +0800

    Leetcode 0206 反转链表

--- Java/src/_0206_Reverse_Linked_List/Solution.java
+++ Java/src/_0206_Reverse_Linked_List/Solution.java
@@ -1,28 +1,26 @@
 package _0206_Reverse_Linked_List;
 
-class Solution {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(1)
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+public class Solution {
 
     public ListNode reverseList(ListNode head) {
-        ListNode pre = null;
-        ListNode cur = head;
 
+        ListNode pre = null, cur = head;
         while (cur != null) {
             ListNode next = cur.next;
-
             cur.next = pre;
             pre = cur;
             cur = next;
         }
-
         return pre;
     }
 
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 4, 5};
-
         // 创建链表
         MyLinkedList myLinkedList = new MyLinkedList(nums);
         ListNode head = myLinkedList.getHead();
--- Java/src/_0206_Reverse_Linked_List/Solution2.java
+++ Java/src/_0206_Reverse_Linked_List/Solution2.java
@@ -1,25 +1,24 @@
 package _0206_Reverse_Linked_List;
 
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ */
 public class Solution2 {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(n)
 
     // 返回以head为头结点的链表反转后的链表头指针
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
 
+        if (head == null || head.next == null) return head;
         ListNode rhead = reverseList(head.next);
         head.next.next = head;
         head.next = null;
-
         return rhead;
     }
 
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 4, 5};
-
         // 创建链表
         MyLinkedList myLinkedList = new MyLinkedList(nums);
         ListNode head = myLinkedList.getHead();
