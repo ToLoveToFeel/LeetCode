f8d1ca2e1c98c3d0e42f3452fcc8846d96e351e2
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 20:45:51 +0800

    Leetcode 0257 二叉树的所有路径

--- Java/src/_0257_Binary_Tree_Paths/Solution.java
+++ Java/src/_0257_Binary_Tree_Paths/Solution.java
@@ -3,17 +3,18 @@ package _0257_Binary_Tree_Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 时间复杂度：O(n)，n为结点个数
+ * 空间复杂度：O(h)，h为树高
+ */
 public class Solution {
-    // 时间复杂度：O(n)，n为结点个数
-    // 空间复杂度：O(h)，h为树高
+
     // 函数语义：返回以root为根的所有的路径字符串
     public List<String> binaryTreePaths(TreeNode root) {
+
         ArrayList<String> res = new ArrayList<>();
-
-        if (null == root)
-            return res;
-
-        if (root.left == null && root.right == null) {  // 递归终止情况
+        if (root == null) return res;
+        if (root.left == null && root.right == null) {
             res.add(Integer.toString(root.val));
             return res;
         }
@@ -21,19 +22,26 @@ public class Solution {
         List<String> leftPaths = binaryTreePaths(root.left);  // 左子树所有路径字符串
         for (String s : leftPaths) {  // 拼接上左子树的父结点
             StringBuilder sb = new StringBuilder(Integer.toString(root.val));
-            sb.append("->");
-            sb.append(s);
-            res.add(sb.toString());
+            res.add(sb.append("->").append(s).toString());
         }
-
         List<String> rightPaths = binaryTreePaths(root.right);
         for (String s : rightPaths) {
             StringBuilder sb = new StringBuilder(Integer.toString(root.val));
-            sb.append("->");
-            sb.append(s);
-            res.add(sb.toString());
+            res.add(sb.append("->").append(s).toString());
         }
 
         return res;
+    }
+
+    public static void main(String[] args) {
+
+        int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
+        int[] nums = new int[]{
+                1,
+                2, 3,
+                nu, 5
+        };
+        TreeNode root = (new MyTree(nums)).getRoot();
+        System.out.println((new Solution()).binaryTreePaths(root));
     }
 }
