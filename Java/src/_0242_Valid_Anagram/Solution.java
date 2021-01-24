d3125ae54af0d1fec25ee0fa19cae97e8a3e12a2
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 20:37:28 +0800

    Leetcode 0242 有效的字母异位词

--- Java/src/_0242_Valid_Anagram/Solution.java
+++ Java/src/_0242_Valid_Anagram/Solution.java
@@ -9,12 +9,10 @@ public class Solution {
 
     public boolean isAnagram(String s, String t) {
 
-        if (s.length() != t.length())
-            return false;
+        if (s.length() != t.length()) return false;
 
         int[] sFreq = new int[26];
         int[] tFreq = new int[26];
-
         for (int i = 0; i < s.length(); i++) {
             sFreq[s.charAt(i) - 'a']++;
             tFreq[t.charAt(i) - 'a']++;
@@ -23,7 +21,6 @@ public class Solution {
         for (int i = 0; i < 26; i++)
             if (sFreq[i] != tFreq[i])
                 return false;
-
         return true;
     }
 
