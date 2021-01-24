f9ccbe30f4835ae98fb3db3aca32a114f3b96023
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 09:36:50 +0800

    Leetcode 0674 最长连续递增序列

--- Java/src/_0674_Longest_Continuous_Increasing_Subsequence/Solution.java
+++ Java/src/_0674_Longest_Continuous_Increasing_Subsequence/Solution.java
@@ -6,30 +6,27 @@ package _0674_Longest_Continuous_Increasing_Subsequence;
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)
  * Leetcode 674 --> Leetcode 300    Leetcode 128
+ * 执行用时：1 ms, 在所有 Java 提交中击败了99.83%的用户
+ * 内存消耗：39.4 MB, 在所有 Java 提交中击败了13.41%的用户
  */
 public class Solution {
 
     public int findLengthOfLCIS(int[] nums) {
-        int res = 0;
-        int index = 0;
 
-        while (index < nums.length) {
-            int length = 1;
-            int curIndex = index;
-            while (curIndex + 1 < nums.length && nums[curIndex] < nums[curIndex + 1]) {
-                curIndex++;
-                length++;
-            }
-            res = Math.max(res, length);
-            index = curIndex + 1;
+        int res = 0, i = 0;
+        while (i < nums.length) {
+            int j = i;
+            while (j + 1 < nums.length && nums[j] < nums[j + 1]) j++;
+            res = Math.max(res, j - i + 1);
+            i = j + 1;
         }
         return res;
     }
 
     public static void main(String[] args) {
 
-//        int[] nums = {1, 3, 5, 4, 7};
-        int[] nums = {2, 2, 2, 2, 2};
+//        int[] nums = {1, 3, 5, 4, 7};  // 3
+        int[] nums = {2, 2, 2, 2, 2};  // 1
         System.out.println((new Solution()).findLengthOfLCIS(nums));
     }
 }
