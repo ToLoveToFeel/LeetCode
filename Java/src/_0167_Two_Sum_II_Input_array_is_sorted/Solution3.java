5b925a2f715ea6b56c16b326753812762a3b840f
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 11:09:55 +0800

    Leetcode 0167 两数之和 II - 输入有序数组

--- Java/src/_0167_Two_Sum_II_Input_array_is_sorted/Solution3.java
+++ Java/src/_0167_Two_Sum_II_Input_array_is_sorted/Solution3.java
@@ -1,38 +1,32 @@
 package _0167_Two_Sum_II_Input_array_is_sorted;
 
+import _0000_study._common.Output;
+
 /**
  * 双指针
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)
+ * 执行用时：1 ms, 在所有 Java 提交中击败了92.72%的用户
+ * 内存消耗：39 MB, 在所有 Java 提交中击败了5.15%的用户
  */
 public class Solution3 {
 
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSum(int[] nums, int target) {
 
-        int[] res = new int[2];
-
-        int l = 0, r = numbers.length - 1;  // 对撞指针
+        int l = 0, r = nums.length - 1;  // 对撞指针
         while (l < r) {
-            if (numbers[l] + numbers[r] == target) {
-                res[0] = l + 1;
-                res[1] = r + 1;
-                break;
-            } else if (numbers[l] + numbers[r] < target)
-                l++;
-            else
-                r--;
+            if (nums[l] + nums[r] == target) return new int[]{l + 1, r + 1};
+            else if (nums[l] + nums[r] < target) l++;
+            else r--;
         }
-
-        return res;
+        return new int[]{-1, -1};
     }
 
     public static void main(String[] args) {
 
         int[] numbers = {-1, 0};
         int target = -1;
-
         int[] res = (new Solution3()).twoSum(numbers, target);
-        for (int num : res)
-            System.out.println(num);
+        Output.OutputBasicArray1D(res);
     }
 }
