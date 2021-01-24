6fb5d667249091656704b1502dbfe8ff817ab2ce
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 20:23:38 +0800

    Leetcode 0237 删除链表中的节点

--- Java/src/_0237_Delete_Node_in_a_Linked_List/ListNode.java
+++ Java/src/_0237_Delete_Node_in_a_Linked_List/ListNode.java
@@ -1,42 +1,14 @@
 package _0237_Delete_Node_in_a_Linked_List;
 
+/**
+ * Date: 2020/9/4 18:05
+ * Content:
+ */
 public class ListNode {
     int val;
     ListNode next;
-    public ListNode(int x) { val = x; }
-    public ListNode(int[] arr){
-        if (null == arr || 0 == arr.length)
-            throw new IllegalArgumentException("array can not be empty!");
 
-        this.val = arr[0];
-        ListNode cur = this;
-        for (int i = 1; i < arr.length; i++) {
-            cur.next = new ListNode(arr[i]);
-            cur = cur.next;
-        }
-    }
-
-    ListNode findNode(int x){
-
-        ListNode curNode = this;
-        while(curNode != null){
-            if(curNode.val == x)
-                return curNode;
-            curNode = curNode.next;
-        }
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer ret = new StringBuffer();
-        ListNode cur = this;
-        while(null != cur) {
-            ret.append(cur.val + "->");
-            cur = cur.next;
-        }
-        ret.append("null");
-
-        return ret.toString();
+    ListNode(int x) {
+        val = x;
     }
 }
--- Java/src/_0237_Delete_Node_in_a_Linked_List/Solution.java
+++ Java/src/_0237_Delete_Node_in_a_Linked_List/Solution.java
@@ -1,22 +1,23 @@
 package _0237_Delete_Node_in_a_Linked_List;
 
+/**
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：37.9 MB, 在所有 Java 提交中击败了41.40%的用户
+ */
 public class Solution {
-    public void deleteNode(ListNode node) {
-        if (null == node || null == node.next)
-            throw new IllegalArgumentException("node should be valid and can not be the tail node.");
 
+    public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 1, 9};
-        ListNode head = new ListNode(arr);
-        System.out.println(head);
 
-        ListNode node = head.findNode(5);
-
-        (new Solution()).deleteNode(node);
-        System.out.println(head);
+        // 创建链表，获取链表头结点
+        ListNode head = (new MyLinkedList(new int[]{4, 5, 1, 9})).getHead();
+        MyLinkedList.printLinkedList(head);
+        ListNode d = head.next.next;  // 1
+        (new Solution()).deleteNode(d);
+        MyLinkedList.printLinkedList(head);
     }
 }
