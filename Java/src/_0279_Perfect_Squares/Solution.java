58fb762e91afe66806330d6c202dcec573c4f6d1
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 20:58:37 +0800

    Leetcode 0279 完全平方数

--- Java/src/_0279_Perfect_Squares/Solution.java
+++ Java/src/_0279_Perfect_Squares/Solution.java
@@ -1,42 +1,53 @@
 package _0279_Perfect_Squares;
 
-import javafx.util.Pair;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-import java.util.LinkedList;
-
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * 执行用时：11 ms, 在所有 Java 提交中击败了94.53%的用户
+ * 内存消耗：38 MB, 在所有 Java 提交中击败了27.87%的用户
+ */
 public class Solution {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(n)
+
+    private static class MyPair {
+        int x, y;
+
+        public MyPair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public int numSquares(int n) {
-        if (0 == n)
-            return 0;
-        LinkedList<Pair<Integer, Integer>> queue = new LinkedList<>();  // (数字，n经过的 步数 到达该数字)
-        queue.addLast(new Pair<>(n, 0));
-        boolean[] visited = new boolean[n+1];
+
+        if (n == 0) return 0;
+
+        Deque<MyPair> queue = new ArrayDeque<>();  // (数字，n经过的 步数 到达该数字)
+        queue.add(new MyPair(n, 0));
+        boolean[] visited = new boolean[n + 1];
         visited[n] = true;
+        while (!queue.isEmpty()) {
+            MyPair front = queue.remove();
+            int num = front.x, step = front.y;
 
-        while (!queue.isEmpty()){
-            Pair<Integer, Integer> front = queue.removeFirst();
-            int num = front.getKey();
-            int step = front.getValue();
-
-            for (int i = 1; ; i++){
-                int a = num - i*i;
-                if (a < 0)
-                    break;
-                if (0 == a)
-                    return step + 1;
-                if (!visited[a]){
-                    queue.addLast(new Pair<>(a, step+1));
+            for (int i = 1; ; i++) {
+                int a = num - i * i;
+                if (a < 0) break;
+                if (a == 0) return step + 1;
+                if (!visited[a]) {
+                    queue.addLast(new MyPair(a, step + 1));
                     visited[a] = true;
                 }
             }
         }
 
-        throw new IllegalArgumentException("No Trie!");
+        return -1;
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).numSquares(12));
         System.out.println((new Solution()).numSquares(13));
     }
--- Java/src/_0279_Perfect_Squares/Solution3.java
+++ Java/src/_0279_Perfect_Squares/Solution3.java
@@ -2,26 +2,29 @@ package _0279_Perfect_Squares;
 
 import java.util.Arrays;
 
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * 完全背包问题
+ */
 public class Solution3 {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(n)
-    // 动态编程，掌握
+
     public int numSquares(int n) {
 
-        int[] mem = new int[n+1];  // mem[i] 代表 i可以由几个完全平方数表示
-        Arrays.fill(mem, Integer.MAX_VALUE);
-        mem[0] = 0;
-        for(int i = 1; i <= n ; i ++)
-            for(int j = 1 ; i - j * j >= 0 ; j ++)
+        int[] f = new int[n + 1];  // f[i] 代表 i可以由几个完全平方数表示
+        Arrays.fill(f, Integer.MAX_VALUE);
+        f[0] = 0;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j * j <= i; j++)
                 // i本次次由完全平方数表示的个数 = min（i上次由完全平方数表示的个数，i - j * j由完全平方数表示的最少个数+1）
-                mem[i] = Math.min(mem[i], 1 + mem[i - j * j]);
+                f[i] = Math.min(f[i], 1 + f[i - j * j]);
 
-        return mem[n];
+        return f[n];
     }
 
     public static void main(String[] args) {
 
-        System.out.println((new Solution3()).numSquares(12));
-        System.out.println((new Solution3()).numSquares(13));
+        System.out.println((new Solution3()).numSquares(12));  // 2
+        System.out.println((new Solution3()).numSquares(13));  // 3
     }
 }
