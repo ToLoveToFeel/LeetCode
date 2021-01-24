6e7671b235a6505b63444475b8450c4f9c3f0cd4
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-24 15:26:06 +0800

    Leetcode 0226 翻转二叉树

--- Java/src/_0226_Invert_Binary_Tree/Solution.java
+++ Java/src/_0226_Invert_Binary_Tree/Solution.java
@@ -1,12 +1,15 @@
 package _0226_Invert_Binary_Tree;
 
+/**
+ * 时间复杂度：O(n)，n为节点个数
+ * 时间复杂度：O(h)，h为二叉树高度
+ */
 public class Solution {
+
     // 该函数语义：反转以root为根的二叉树
-    // 时间复杂度：O(n)，n为节点个数
-    // 时间复杂度：O(h)，h为二叉树高度
     public TreeNode invertTree(TreeNode root) {
-        if (null == root)
-            return root;
+
+        if (root == null) return root;
 
         invertTree(root.left);
         invertTree(root.right);
@@ -19,13 +22,13 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
         int[] nums = new int[]{
                 4,
                 2, 7,
                 1, 3, 6, 9
         };
-
         TreeNode root = (new MyTree(nums)).getRoot();
         System.out.println(MyTree.levelOrder((new Solution()).invertTree(root)));
     }
