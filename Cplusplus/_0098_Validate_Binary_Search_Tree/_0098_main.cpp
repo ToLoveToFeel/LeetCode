6630e2d7bd513eace2b64a43b6c3048a0900ec21
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-25 16:55:47 +0800

    Leetcode 0098 验证二叉搜索树

---
+++ Cplusplus/_0098_Validate_Binary_Search_Tree/_0098_main.cpp
@@ -0,0 +1,41 @@
+// Created by WXX on 2021/1/25 16:48
+#include "MyTree.h"
+
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        if (!root) return true;
+        return dfs(root)[0];
+    }
+
+    // 返回的三元组：(是否为BST, 以root为根的数的最大值, 以root为根的数的最小值)
+    vector<int> dfs(TreeNode *root) {
+        vector<int> res({1, root->val, root->val});
+        if (root->left) {
+            auto t = dfs(root->left);
+            if (!t[0] || t[2] >= root->val) res[0] = 0;
+            res[1] = min(res[1], t[1]);
+            res[2] = max(res[2], t[2]);
+        }
+        if (root->right) {
+            auto t = dfs(root->right);
+            if (!t[0] || t[1] <= root->val) res[0] = 0;
+            res[1] = min(res[1], t[1]);
+            res[2] = max(res[2], t[2]);
+        }
+        return res;
+    }
+};
+
+int main() {
+
+    int nu = INT_MAX;
+    vector<int> nums = {
+            2,
+            1, 3
+    };
+    TreeNode *root = MyTree(nums).getRoot();
+    cout << Solution().isValidBST(root) << endl;  // 1
+
+    return 0;
+}
