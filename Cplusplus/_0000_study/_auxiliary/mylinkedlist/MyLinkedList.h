70ba5a27a0918d9c861922a250046f0f8db1eb00
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-25 15:17:56 +0800

    Leetcode 0095 不同的二叉搜索树 II

--- Cplusplus/_0000_study/_auxiliary/mylinkedlist/MyLinkedList.h
+++ Cplusplus/_0000_study/_auxiliary/mylinkedlist/MyLinkedList.h
@@ -66,5 +66,46 @@ public:
 
 };
 
+// 输出函数
+void OutputBasicArray1D(vector<int> &nums) {
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+void OutputBasicArray2D1(vector<vector<int>> &nums) {
+
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && i < nums.size()) cout << ' ';
+        cout << "[";
+        for (int j = 0; j < nums[i].size(); j++) {
+            cout << nums[i][j];
+            if (j != nums[i].size() - 1) cout << ", ";
+        }
+        cout << "]";
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+void OutputBasicArray2D(vector<vector<int>> &nums) {
+
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && i < nums.size()) cout << ' ';
+        cout << "[";
+        for (int j = 0; j < nums[i].size(); j++) {
+            cout << nums[i][j];
+            if (j != nums[i].size() - 1) cout << ", ";
+        }
+        cout << "]";
+        if (i != nums.size() - 1) cout << ", " << endl;
+    }
+    cout << "]" << endl;
+}
 
 #endif //CPLUSPLUS_MYLINKEDLIST_H
---
+++ Cplusplus/_0095_Unique_Binary_Search_Trees_II/_0095_main.cpp
@@ -0,0 +1,43 @@
+// Created by WXX on 2021/1/25 15:02
+#include "MyTree.h"
+
+/**
+ * 执行用时：8 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：15.8 MB, 在所有 C++ 提交中击败了78.28%的用户
+ */
+class Solution {
+public:
+    vector<TreeNode *> generateTrees(int n) {
+        if (!n) return {};
+        return dfs(1, n);
+    }
+
+    // 返回由[l...r]形成的树的树根的集合
+    vector<TreeNode *> dfs(int l, int r) {
+
+        if (l > r) return {NULL};
+        vector<TreeNode *> res;
+
+        for (int i = l; i <= r; i++) {
+            auto left = dfs(l, i - 1), right = dfs(i + 1, r);
+            for (auto l : left)
+                for (auto r : right) {
+                    auto root = new TreeNode(i);
+                    root->left = l, root->right = r;
+                    res.push_back(root);
+                }
+        }
+        return res;
+    }
+};
+
+int main() {
+
+    vector<TreeNode *> res = Solution().generateTrees(3);
+    for (auto root : res) {
+        vector<vector<int>> ans = MyTree().levelOrder(root);
+        OutputBasicArray2D1(ans);
+    }
+
+    return 0;
+}
