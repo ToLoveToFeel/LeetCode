91fa15906543a4aa8878cfe5a702dc3f67f7a763
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-25 22:32:48 +0800

    Leetcode 0132 分割回文串 II

---
+++ Cplusplus/_0132_Palindrome_Partitioning_II/_0132_main.cpp
@@ -0,0 +1,38 @@
+// Created by WXX on 2021/1/25 22:22
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCut(string s) {
+
+        int n = s.size();
+        s = ' ' + s;
+        vector<vector<bool>> g(n + 1, vector<bool>(n + 1));
+        vector<int> f(n + 1, 1e8);  // f[i]：代表s[1...i]最少可以被分为多少个回文串
+        // 预处理：g[i][j]代表s[i...j]是否为回文串
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= j; i++)
+                if (i == j) g[i][j] = true;
+                else if (s[i] == s[j]) {
+                    if (i + 1 > j - 1 || g[i + 1][j - 1]) g[i][j] = true;
+                }
+
+        f[0] = 0;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= i; j++)
+                if (g[j][i])
+                    f[i] = min(f[i], f[j - 1] + 1);
+        return f[n] - 1;
+    }
+};
+
+int main() {
+
+    string s = "aab";  // 1
+    cout << Solution().minCut(s) << endl;
+
+    return 0;
+}
