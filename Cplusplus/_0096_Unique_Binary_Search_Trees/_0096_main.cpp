0e8c962510c0fdc3cf88d77f58f86916bd2cbdf8
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-26 10:44:36 +0800

    Leetcode 0096 不同的二叉搜索树

--- Cplusplus/_0096_Unique_Binary_Search_Trees/_0096_main.cpp
+++ Cplusplus/_0096_Unique_Binary_Search_Trees/_0096_main.cpp
@@ -4,10 +4,15 @@
 
 using namespace std;
 
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：5.8 MB, 在所有 C++ 提交中击败了96.93%的用户
+ */
 class Solution {
 public:
     int numTrees(int n) {
 
+        // f[i]: 表示一共i个不同的节点可以组成的数的个数
         vector<int> f(n + 1);
         f[0] = 1;
         for (int i = 1; i <= n; i++)
