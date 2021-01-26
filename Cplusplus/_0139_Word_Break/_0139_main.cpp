458e32454451538bd5ae9408ed0a8d552b4f8c9d
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-26 15:27:09 +0800

    Leetcode 0139 单词拆分

---
+++ Cplusplus/_0139_Word_Break/_0139_main.cpp
@@ -0,0 +1,46 @@
+// Created by WXX on 2021/1/26 15:10
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+// 字符串哈希，动态规划
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+
+        typedef unsigned long long ULL;
+        const int P = 131;
+        unordered_set<ULL> hash;
+        for (auto &word : wordDict) {
+            ULL h = 0;
+            for (auto c : word) h = h * P + c;
+            hash.insert(h);
+        }
+
+        int n = s.size();
+        vector<bool> f(n + 1);
+        f[0] = true;
+        s = ' ' + s;
+        for (int i = 0; i < n; i++) {
+            if (f[i]) {
+                ULL h = 0;
+                for (int j = i + 1; j <= n; j++) {
+                    h = h * P + s[j];
+                    if (hash.count(h)) f[j] = true;
+                }
+            }
+        }
+        return f[n];
+    }
+};
+
+int main() {
+
+    string s = "applepenapple";
+    vector<string> wordDict = {"apple", "pen"};
+    cout << Solution().wordBreak(s, wordDict) << endl;  // true
+
+    return 0;
+}
