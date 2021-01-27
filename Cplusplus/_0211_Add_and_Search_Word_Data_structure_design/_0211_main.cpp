9dc95c06fe333d0ccfc53a0950f4fb057cae61fa
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-27 16:05:28 +0800

    Leetcode 0211 添加与搜索单词

---
+++ Cplusplus/_0211_Add_and_Search_Word_Data_structure_design/_0211_main.cpp
@@ -0,0 +1,67 @@
+// Created by WXX on 2021/1/27 15:53
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class WordDictionary {
+public:
+    struct Node {
+        bool is_end;
+        Node *son[26];
+
+        Node() {
+            is_end = false;
+            for (int i = 0; i < 26; i++)
+                son[i] = NULL;
+        }
+    } *root;
+
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        root = new Node();
+    }
+
+    void addWord(string word) {
+        auto p = root;
+        for (auto c : word) {
+            int u = c - 'a';
+            if (!p->son[u]) p->son[u] = new Node();
+            p = p->son[u];
+        }
+        p->is_end = true;
+    }
+
+    bool search(string word) {
+        return dfs(root, word, 0);
+    }
+
+    // 返回以p为根的trie树中是否存在字符串word[i...)
+    bool dfs(Node *p, string word, int i) {
+        if (i == word.size()) return true;
+        if (word[i] != '.') {
+            int u = word[i] - 'a';
+            if (!p->son[u]) return false;
+            return dfs(p->son[u], word, i + 1);
+        } else {
+            for (int j = 0; j < 26; j++)
+                if (p->son[j] && dfs(p->son[j], word, i + 1))
+                    return true;
+            return false;
+        }
+    }
+};
+
+int main() {
+
+    WordDictionary *w = new WordDictionary();
+    w->addWord("bad");
+    w->addWord("dad");
+    w->addWord("mad");
+    cout << w->search("pad") << endl;  // return False
+    cout << w->search("bad") << endl;  // return True
+    cout << w->search(".ad") << endl;  // return True
+    cout << w->search("b..") << endl;  // return True
+
+    return 0;
+}
