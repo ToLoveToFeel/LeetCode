070597b93cb2bbd656dc35210ce329d3bf1541aa
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-30 15:05:56 +0800

    Leetcode 0201  数字范围按位与

--- Cplusplus/_0201_Bitwise_AND_of_Numbers_Range/_0201_main.cpp
+++ Cplusplus/_0201_Bitwise_AND_of_Numbers_Range/_0201_main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 // 从高位到低位找到m和n第一个不同的位，结果从该位开始都是0
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：5.8 MB, 在所有 C++ 提交中击败了96.09%的用户
+ */
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
