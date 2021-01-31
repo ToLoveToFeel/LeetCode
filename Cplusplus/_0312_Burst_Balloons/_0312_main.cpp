711fda2deb0644527caad20d44f1071fee035e2a
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-31 22:47:31 +0800

    Leetcode 0312 戳气球

---
+++ Cplusplus/_0312_Burst_Balloons/_0312_main.cpp
@@ -0,0 +1,32 @@
+// Created by WXX on 2021/1/31 22:36
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> a(n + 2, 1);
+        for (int i = 1; i <= n; i++) a[i] = nums[i - 1];
+
+        vector<vector<int>> f(n + 2, vector<int>(n + 2));
+        for (int len = 3; len <= n + 2; len++)
+            for (int i = 0; i + len - 1 <= n + 1; i++) {
+                int j = i + len - 1;
+                for (int k = i + 1; k < j; k++)
+                    f[i][j] = max(f[i][j], f[i][k] + f[k][j] + a[i] * a[k] * a[j]);
+            }
+        return f[0][n + 1];
+    }
+};
+
+int main() {
+
+    vector<int> nums = {3, 1, 5, 8};
+    cout << Solution().maxCoins(nums) << endl;  // 167
+
+    return 0;
+}
