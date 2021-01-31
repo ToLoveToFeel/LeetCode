722643a17fda3a645407a094051b437b21864a5a
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-31 20:08:56 +0800

    Leetcode 0303 区域和检索 - 数组不可变

--- Java/src/_0303_Range_Sum_Query_Immutable/Solution.java
+++ Java/src/_0303_Range_Sum_Query_Immutable/Solution.java
@@ -7,17 +7,15 @@ public class Solution {
         private SegmentTree<Integer> segmentTree;
 
         public NumArray(int[] nums) {
+
             if (nums.length > 0) {
                 Integer[] data = new Integer[nums.length];
-                for (int i = 0; i < nums.length; i++)
-                    data[i] = nums[i];
+                for (int i = 0; i < nums.length; i++) data[i] = nums[i];
                 segmentTree = new SegmentTree<>(data, (a, b) -> a + b);
             }
         }
 
         public int sumRange(int i, int j) {
-            if (segmentTree == null)
-                throw new IllegalArgumentException("Segment Tree is null");
 
             return segmentTree.query(i, j);
         }
@@ -26,7 +24,6 @@ public class Solution {
     public static void main(String[] args) {
 
         int[] nums = {-2, 0, 3, -5, 2, -1};
-
         NumArray array = new NumArray(nums);
         System.out.println(array.sumRange(2, 3));  // -2
     }
--- Java/src/_0303_Range_Sum_Query_Immutable/Solution2.java
+++ Java/src/_0303_Range_Sum_Query_Immutable/Solution2.java
@@ -1,5 +1,8 @@
 package _0303_Range_Sum_Query_Immutable;
 
+/**
+ * 前缀和
+ */
 public class Solution2 {
 
     static class NumArray {
@@ -7,22 +10,23 @@ public class Solution2 {
         private int[] sum;  // sum[i]存储前i个元素和， sum[0] = 0
 
         public NumArray(int[] nums) {
+
             sum = new int[nums.length + 1];
             sum[0] = 0;
-            for (int i = 1; i < sum.length; i++) {
+            for (int i = 1; i < sum.length; i++)
                 sum[i] = sum[i - 1] + nums[i - 1];
-            }
         }
 
         public int sumRange(int i, int j) {
-            return sum[j + 1] - sum[i];
+            i++;
+            j++;
+            return sum[j] - sum[i - 1];
         }
     }
 
     public static void main(String[] args) {
 
         int[] nums = {-2, 0, 3, -5, 2, -1};
-
         NumArray array = new NumArray(nums);
         System.out.println(array.sumRange(2, 3));  // -2
     }
