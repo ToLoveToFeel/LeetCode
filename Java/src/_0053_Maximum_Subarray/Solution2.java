f5106101f140cfcb0b3b6a2392df71c0c2699884
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-31 23:55:39 +0800

    Leetcode 0053 最大子序和

--- Java/src/_0053_Maximum_Subarray/Solution2.java
+++ Java/src/_0053_Maximum_Subarray/Solution2.java
@@ -6,11 +6,11 @@ public class Solution2 {
     //      ----------------------->
     //  -2  1   -3  4   -1  2   1   -5  4
     public int maxSubArray(int[] nums) {
-        int preMax = nums[0];
-        int max = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            preMax = preMax > 0 ? preMax + nums[i] : nums[i];
-            max = Math.max(max, preMax);
+
+        int f = 0, max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            f = Math.max(f, 0) + nums[i];
+            max = Math.max(max, f);
         }
         return max;
     }
@@ -18,7 +18,7 @@ public class Solution2 {
     public static void main(String[] args) {
 
         int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println((new Solution2()).maxSubArray(nums));
+        System.out.println((new Solution2()).maxSubArray(nums));  // 6
     }
 }
 
