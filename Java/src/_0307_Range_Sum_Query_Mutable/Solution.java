1481a0d725f3bfdd85606d63a4a33e5a9a9729c0
‘ToLoveToFeel’ <1137247975@qq.com>
2021-01-31 21:12:57 +0800

    Leetcode 0307 区域和检索 - 数组可变

--- Java/src/_0307_Range_Sum_Query_Mutable/Solution.java
+++ Java/src/_0307_Range_Sum_Query_Mutable/Solution.java
@@ -4,32 +4,30 @@ public class Solution {
 
     static class NumArray {
 
-        private SegmentTree<Integer> segmentTree;
+        private SegmentTree<Integer> seg;
 
         public NumArray(int[] nums) {
             if (nums.length > 0) {
                 Integer[] data = new Integer[nums.length];
-                for (int i = 0; i < nums.length; i++)
-                    data[i] = nums[i];
-                segmentTree = new SegmentTree<>(data, (a, b) -> a + b);
+                for (int i = 0; i < nums.length; i++) data[i] = nums[i];
+                seg = new SegmentTree<>(data, (a, b) -> a + b);
             }
         }
 
         public void update(int i, int val) {
-            segmentTree.set(i, val);
+
+            seg.set(i, val);
         }
 
         public int sumRange(int i, int j) {
-            if (segmentTree == null)
-                throw new IllegalArgumentException("Segment Tree is null");
-            return segmentTree.query(i, j);
+
+            return seg.query(i, j);
         }
     }
 
     public static void main(String[] args) {
 
         int[] nums = {-2, 0, 3, -5, 2, -1};
-
         NumArray array = new NumArray(nums);
         array.update(1, 2);
         System.out.println(array.sumRange(0, 2));  // 3
