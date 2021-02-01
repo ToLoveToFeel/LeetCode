6944637f3184c52d325c0e282961e20014084085
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-01 15:27:47 +0800

    Leetcode 0321 拼接最大数

--- Java/src/_0321_Create_Maximum_Number/Solution.java
+++ Java/src/_0321_Create_Maximum_Number/Solution.java
@@ -1,9 +1,9 @@
 package _0321_Create_Maximum_Number;
 
+import _0000_study._common.Output;
+
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.stream.Collectors;
 
 /**
  * Date: 2020/12/2 16:39
@@ -17,10 +17,9 @@ public class Solution {
 
     // 类似于Leetcode 0402，此时保证nums.length >= k
     // 返回保留 len 位数据的最大值
-    private int[] pickKMaxNumber(int[] nums, int len) {
+    private int[] maxArray(int[] nums, int len) {
 
         int k = nums.length - len;  // 删除k位数据
-
         // 利用单调递减栈解决
         Deque<Integer> stack = new ArrayDeque<>();
         for (int num : nums) {
@@ -33,44 +32,28 @@ public class Solution {
 
         // 比如：nums:[4,3,2,1], len=2,则stack:[4,3,2,1] <- 栈顶，此时需要先出栈两个数据
         int[] res = new int[len];
-        int length = stack.size() - len;  // 多余的数据个数
-        for (int i = 0; i < length; i++) {  // 出栈多余的数据
-            stack.pop();
-        }
-        for (int i = res.length - 1; i >= 0; i--) {  // 得到结果
-            res[i] = stack.pop();
-        }
-
+        for (int i = 0; i < stack.size() - len; i++) stack.pop(); // 出栈多余的数据
+        for (int i = res.length - 1; i >= 0; i--) res[i] = stack.pop();  // 得到结果
         return res;
     }
 
     // 将两个数据合成一个最大的数据
     private int[] merge(int[] nums1, int[] nums2) {
 
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int n1 = nums1.length, n2 = nums2.length;
 
         int[] res = new int[n1 + n2];
 
         int i = 0, j = 0;
         for (int k = 0; k < n1 + n2; k++) {
-            if (i >= n1) {
-                res[k] = nums2[j];
-                j++;
-            } else if (j >= n2) {
-                res[k] = nums1[i];
-                i++;
-            } else if (compare(nums1, i, nums2, j)) {
+            if (i >= n1) res[k] = nums2[j++];
+            else if (j >= n2) res[k] = nums1[i++];
+            else if (compare(nums1, i, nums2, j)) {
                 // 不能直接比较 nums1[i] > nums2[j]，在相等的情况下会导致错误，应该继续比较，需要替换为compare
                 // 例子 int[] nums1 = {6, 7}; int[] nums2 = {6, 0, 4}; int k = 5;
-                res[k] = nums1[i];
-                i++;
-            } else {
-                res[k] = nums2[j];
-                j++;
-            }
+                res[k] = nums1[i++];
+            } else res[k] = nums2[j++];
         }
-
         return res;
     }
 
@@ -78,51 +61,33 @@ public class Solution {
     // 如果 nums1[p1]比较大，但会true
     private boolean compare(int[] nums1, int p1, int[] nums2, int p2) {
 
-        if (p1 >= nums1.length)
-            return false;
-        else if (p2 >= nums2.length)
-            return true;
-        else if (nums1[p1] > nums2[p2])
-            return true;
-        else if (nums1[p1] < nums2[p2])
-            return false;
-        else // nums1[p1] == nums2[p2]
-            return compare(nums1, p1 + 1, nums2, p2 + 1);
+        if (p1 >= nums1.length) return false;
+        else if (p2 >= nums2.length) return true;
+        else if (nums1[p1] > nums2[p2]) return true;
+        else if (nums1[p1] < nums2[p2]) return false;
+        else return compare(nums1, p1 + 1, nums2, p2 + 1);  // nums1[p1] == nums2[p2]
     }
 
     // 判断两个长度相等的数据的大小，如果 nums1 > nums2 返回true
     private boolean judge(int[] nums1, int[] nums2) {
 
         for (int i = 0; i < nums1.length; i++) {
-            if (nums1[i] > nums2[i])
-                return true;
-            else if (nums1[i] < nums2[i])
-                return false;
+            if (nums1[i] > nums2[i]) return true;
+            else if (nums1[i] < nums2[i]) return false;
         }
-        // 相等返回 false
-        return false;
+        return false;  // 相等返回 false
     }
 
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
 
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int n1 = nums1.length, n2 = nums2.length;
 
         int[] res = new int[k];
-
         for (int i = Math.max(0, k - n2); i <= Math.min(k, n1); i++) {
-            // 0 <= i <= Math.min(k, n1)
-            // 0 <= k - i <= Math.min(k, n2)
-            // 所以  k - Math.min(k, n2) <= i  所以 k +  Math.max(-k, -n2) <= i
-            // 所以  Math.max(k-k, k-n2) <= i
-            int[] pick1 = pickKMaxNumber(nums1, i);
-            int[] pick2 = pickKMaxNumber(nums2, k - i);
-
-            int[] curMax = merge(pick1, pick2);
-            if (judge(curMax, res))
-                res = curMax;
+            int[] a = maxArray(nums1, i), b = maxArray(nums2, k - i);
+            int[] c = merge(a, b);
+            if (judge(c, res)) res = c;
         }
-
         return res;
     }
 
@@ -145,7 +110,12 @@ public class Solution {
         int[] nums2 = {8, 9};
         int k = 3;
         int[] res = (new Solution()).maxNumber(nums1, nums2, k);
-
-        System.out.println(Arrays.stream(res).boxed().collect(Collectors.toList()));
+        Output.OutputBasicArray1D(res);
     }
 }
+/*
+for (int i = Math.max(0, k - n2); i <= Math.min(k, n1); i++) {
+// 0 <= i <= Math.min(k, n1), 0 <= k - i <= Math.min(k, n2)
+// 所以  k - Math.min(k, n2) <= i  所以 k +  Math.max(-k, -n2) <= i
+// 所以  Math.max(k-k, k-n2) <= i
+ */
