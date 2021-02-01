ee0d2196d2a71392262012f85b4fee9f06e324d2
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-01 14:52:20 +0800

    Leetcode 0263 丑数

---
+++ Java/src/_0263_Ugly_Number/Solution.java
@@ -0,0 +1,26 @@
+package _0263_Ugly_Number;
+
+/**
+ * Date: 2021/2/1 14:49
+ * Content:
+ * 执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：35.5 MB, 在所有 Java 提交中击败了55.87%的用户
+ */
+public class Solution {
+
+    public boolean isUgly(int num) {
+
+        if (num <= 0) return false;
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println((new Solution()).isUgly(6));  // true
+        System.out.println((new Solution()).isUgly(8));  // true
+        System.out.println((new Solution()).isUgly(14));  // false
+    }
+}
