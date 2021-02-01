e17d95f889e515fd18410ac4ab1962ff162af35d
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-01 14:37:31 +0800

    Leetcode 0316 去除重复字母

--- Java/src/_0316_Remove_Duplicate_Letters/Solution.java
+++ Java/src/_0316_Remove_Duplicate_Letters/Solution.java
@@ -1,9 +1,6 @@
 package _0316_Remove_Duplicate_Letters;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Date: 2020/12/2 16:05
@@ -19,39 +16,31 @@ public class Solution {
 
     public String removeDuplicateLetters(String s) {
 
-        if (s.length() <= 1)
-            return s;
+        if (s.length() <= 1) return s;
 
         // 第一步：统计各个小写字母出现的次数
-        int[] letters = new int[26];
-        for (char c : s.toCharArray()) {
-            letters[c - 'a']++;
-        }
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) cnt[c - 'a']++;
 
         // 第二步：通过单调栈解决
-        Deque<Character> stack = new ArrayDeque<>();  // 单调栈，不是完全单调的，因为要保证字符串中的字母至少出现一次
+        Deque<Character> stk = new ArrayDeque<>();  // 单调栈，不是完全单调的，因为要保证字符串中的字母至少出现一次
         Set<Character> set = new HashSet<>();  // set中存储着stack存在的小写字母
         for (char c : s.toCharArray()) {
             if (!set.contains(c)) {  // 如果stack中不存在字符c，才考虑入栈
                 // 如果当前栈非空 && 当前考察元素<栈顶元素 && 栈顶元素在后续考察的元素中还存在
                 // 只有这样才能删除栈顶元素
-                while (!stack.isEmpty() && c < stack.peek() && letters[stack.peek() - 'a'] > 0) {
-                    set.remove(stack.pop());
-                }
+                while (!stk.isEmpty() && c < stk.peek() && cnt[stk.peek() - 'a'] > 0) set.remove(stk.pop());
                 // 添加当前考察的字符
                 set.add(c);
-                stack.push(c);
+                stk.push(c);
             }
             // 对应字符次数减一
-            letters[c - 'a'] -= 1;
+            cnt[c - 'a'] -= 1;
         }
 
         // 第三步：得到结果
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-
+        while (!stk.isEmpty()) sb.append(stk.pop());
         return sb.reverse().toString();
     }
 
