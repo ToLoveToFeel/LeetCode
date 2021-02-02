8a0ce03d197e1e354307a8cb3ec5e0ceb5fb65c1
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-02 19:29:49 +0800

    Leetcode 0268 丢失的数字

--- Cplusplus/_0268_Missing_Number/_0268_main.cpp
+++ Cplusplus/_0268_Missing_Number/_0268_main.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+/**
+ * 执行用时：24 ms, 在所有 C++ 提交中击败了89.91%的用户
+ * 内存消耗：17.5 MB, 在所有 C++ 提交中击败了92.39%的用户
+ */
 class Solution {
 public:
     int missingNumber(vector<int> &nums) {
