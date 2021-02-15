3f3e2c1f7287d365c6078abeb00d59cd2a1f9eaa
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-15 22:17:25 +0800

    Leetcode 0388 文件的最长绝对路径

--- Cplusplus/_0388_Longest_Absolute_File_Path/_388_main.cpp
+++ Cplusplus/_0388_Longest_Absolute_File_Path/_388_main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 // 文件（文件夹也看成一种特殊的文件）的层次关系可以看成一棵树，假设树根位于第0层
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：6.2 MB, 在所有 C++ 提交中击败了98.91%的用户
+ */
 class Solution {
 public:
     int lengthLongestPath(string input) {
