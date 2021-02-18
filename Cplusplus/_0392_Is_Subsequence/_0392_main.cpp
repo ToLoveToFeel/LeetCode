2c18db291520ba8c6aacb3eed439585c91483859
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-18 09:56:44 +0800

    LeetCode 0392 判断子序列

--- Cplusplus/_0392_Is_Subsequence/_0392_main.cpp
+++ Cplusplus/_0392_Is_Subsequence/_0392_main.cpp
@@ -3,13 +3,18 @@
 
 using namespace std;
 
+// 考点：双指针
+/**
+ * 执行用时：4 ms, 在所有 C++ 提交中击败了42.30%的用户
+ * 内存消耗：6.2 MB, 在所有 C++ 提交中击败了88.21%的用户
+ */
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
 
         int k = 0;
         for (auto c : t)
-            if (k < t.size() && c == s[k])
+            if (k < s.size() && c == s[k])
                 k++;
         return k == s.size();
     }
@@ -18,6 +23,7 @@ public:
 int main() {
 
     cout << Solution().isSubsequence("abc", "ahbgdc") << endl;  // true
+    cout << Solution().isSubsequence("abc", "abcd") << endl;  // true
     cout << Solution().isSubsequence("axc", "ahbgdc") << endl;  // false
 
     return 0;
