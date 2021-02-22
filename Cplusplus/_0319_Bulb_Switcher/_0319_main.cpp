89cfd011d476c232bb2708c8f7043b2d33b83a47
ToLoveToFeel <1137247975@qq.com>
2021-02-22 20:45:37 +0800

    Leetcode 0319 灯泡开关

---
+++ Cplusplus/_0319_Bulb_Switcher/_0319_main.cpp
@@ -0,0 +1,21 @@
+// Created by WXX on 2021/2/22 20:36
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// 被按奇数次的灯泡最后亮着，因此就是考虑哪些数字的约数个数为奇数
+class Solution {
+public:
+    int bulbSwitch(int n) {
+
+        return sqrt(n);
+    }
+};
+
+int main() {
+
+    cout << Solution().bulbSwitch(1) << endl;  // 1
+
+    return 0;
+}
