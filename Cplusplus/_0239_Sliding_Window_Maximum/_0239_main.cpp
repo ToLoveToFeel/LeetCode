4b86844ec87155bab1ed7d3e602e6f4c53c4d7bc
ToLoveToFeel <1137247975@qq.com>
2021-02-24 17:53:49 +0800

    Leetcode 0239 滑动窗口最大值

--- Cplusplus/_0239_Sliding_Window_Maximum/_0239_main.cpp
+++ Cplusplus/_0239_Sliding_Window_Maximum/_0239_main.cpp
@@ -3,18 +3,16 @@
 
 using namespace std;
 
-const int N = 100010;
-int q[N];
-int hh = 0, tt = -1;
-
 /**
- * 执行用时：516 ms, 在所有 C++ 提交中击败了35.59%的用户
- * 内存消耗：116.3 MB, 在所有 C++ 提交中击败了22.09%的用户
+ * 执行用时：240 ms, 在所有 C++ 提交中击败了97.90%的用户
+ * 内存消耗：114.1 MB, 在所有 C++ 提交中击败了88.81%的用户
  */
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-
+    vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+        int n = nums.size();
+        int q[n];
+        int hh = 0, tt = -1;
         vector<int> res;
         for (int i = 0; i < nums.size(); i++) {
             if (hh <= tt && i - k + 1 > q[hh]) hh++;
@@ -22,19 +20,24 @@ public:
             q[++tt] = i;
             if (i >= k - 1) res.push_back(nums[q[hh]]);
         }
-
         return res;
     }
 };
 
+void OutputBasicArray1D(vector<int> nums) {
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
 int main() {
 
-//    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
-//    int k = 3;
-    vector<int> nums = {1};
-    int k = 1;
-    vector<int> res = Solution().maxSlidingWindow(nums, k);
-    for (int num : res) cout << num << " ";
+    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+    OutputBasicArray1D(Solution().maxSlidingWindow(nums, k));
 
     return 0;
 }
