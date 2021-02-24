62dd545f6b7f3cf5b96f27067119ba9eaa4cb1ef
ToLoveToFeel <1137247975@qq.com>
2021-02-25 00:23:22 +0800

    Leetcode 0322 零钱兑换

--- Java/src/_0322_Coin_Change/Solution3.java
+++ Java/src/_0322_Coin_Change/Solution3.java
@@ -7,21 +7,21 @@ import java.util.Arrays;
  * Content: 完全背包问题
  * 状态：dp[n]：用coins中的面值装满价值为 n 的总金额所需硬币的最少数目
  * 状态转移：dp[i] = min(dp[i], dp[i-coin] + 1)，其中i>=coin,且coin为coins里的元素
+ * 执行用时：12 ms, 在所有 Java 提交中击败了94.69%的用户
+ * 内存消耗：38.1 MB, 在所有 Java 提交中击败了22.43%的用户
  */
 public class Solution3 {
 
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] coins, int m) {
 
-        int C = amount;  // 总价值
-        int[] dp = new int[C + 1];
-        Arrays.fill(dp, C + 1);
-
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, m + 1);
         dp[0] = 0;
         for (int v : coins)
-            for (int j = v; j <= C; j++)
+            for (int j = v; j <= m; j++)
                 dp[j] = Math.min(dp[j], dp[j - v] + 1);
 
-        return dp[C] == C + 1 ? -1 : dp[C];
+        return dp[m] == m + 1 ? -1 : dp[m];
     }
 
     public static void main(String[] args) {
