985122b85efd878526b390ebdb17e4ff93e44963
ToLoveToFeel <1137247975@qq.com>
2021-02-24 10:00:53 +0800

    Leetcode 0319 灯泡开关

--- Cplusplus/_0319_Bulb_Switcher/_0319_main.cpp
+++ Cplusplus/_0319_Bulb_Switcher/_0319_main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 // 被按奇数次的灯泡最后亮着，因此就是考虑哪些数字的约数个数为奇数
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：5.6 MB, 在所有 C++ 提交中击败了99.54%的用户
+ */
 class Solution {
 public:
     int bulbSwitch(int n) {
