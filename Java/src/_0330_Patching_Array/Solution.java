af7f102b75439ae73b425f1ba9cd310d8ea7c5e1
ToLoveToFeel <1137247975@qq.com>
2021-02-25 10:42:15 +0800

    Leetcode 0330 按要求补齐数组

--- Java/src/_0330_Patching_Array/Solution.java
+++ Java/src/_0330_Patching_Array/Solution.java
@@ -2,28 +2,24 @@ package _0330_Patching_Array;
 
 /**
  * Date: 2020/12/29 10:13
- * Content:
+ * Content: 当前已经可以凑出[0,x)，若下一个数nums[i]<=x，加入；否则加入新数据x
  * https://leetcode-cn.com/problems/patching-array/solution/an-yao-qiu-bu-qi-shu-zu-by-leetcode-solu-klp1/
- * 执行用时：1 ms, 在所有 Java 提交中击败了10.42%的用户
- * 内存消耗：38.1 MB, 在所有 Java 提交中击败了65.26%的用户
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：38.2 MB, 在所有 Java 提交中击败了7.86%的用户
  */
 public class Solution {
 
     public int minPatches(int[] nums, int n) {
 
-        int res = 0;
         long x = 1;
-        int len = nums.length, index = 0;
+        int i = 0, res = 0;
         while (x <= n) {
-            if (index < len && nums[index] <= x) {
-                x += nums[index];
-                index++;
-            } else {
+            if (i < nums.length && nums[i] <= x) x += nums[i++];
+            else {
                 x *= 2;
                 res++;
             }
         }
-
         return res;
     }
 
