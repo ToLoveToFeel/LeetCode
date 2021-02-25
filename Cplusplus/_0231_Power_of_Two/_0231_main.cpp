008b98395ce23bdb534b666d88cd086a0abe8d04
ToLoveToFeel <1137247975@qq.com>
2021-02-25 21:46:24 +0800

    Leetcode 0231 2的幂

--- Cplusplus/_0231_Power_of_Two/_0231_main.cpp
+++ Cplusplus/_0231_Power_of_Two/_0231_main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 // lowbit
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：5.5 MB, 在所有 C++ 提交中击败了99.93%的用户
+ */
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
