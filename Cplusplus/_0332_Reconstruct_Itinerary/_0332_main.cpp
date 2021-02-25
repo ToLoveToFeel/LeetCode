e7335e7b77a6bcffc40f52901d24ff7cb23fe316
ToLoveToFeel <1137247975@qq.com>
2021-02-25 19:58:32 +0800

    Leetcode 0332 重新安排行程

---
+++ Cplusplus/_0332_Reconstruct_Itinerary/_0332_main.cpp
@@ -0,0 +1,53 @@
+// Created by WXX on 2021/2/25 19:47
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+// 欧拉路径：所有的边走一遍（只走一次）
+class Solution {
+public:
+    unordered_map<string, multiset<string>> g;
+    vector<string> ans;
+
+    vector<string> findItinerary(vector<vector<string>> &tickets) {
+
+        for (auto &e : tickets) g[e[0]].insert(e[1]);
+        dfs("JFK");
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    // 回溯的路径就是欧拉路径的逆序
+    void dfs(string u) {
+        while (g[u].size()) {
+            auto ver = *g[u].begin();
+            g[u].erase(g[u].begin());
+            dfs(ver);
+        }
+        ans.push_back(u);
+    }
+};
+
+void OutputBasicArray1D(vector<string> nums) {
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+
+    vector<vector<string>> tickets = {{"MUC", "LHR"},
+                                      {"JFK", "MUC"},
+                                      {"SFO", "SJC"},
+                                      {"LHR", "SFO"}};
+    OutputBasicArray1D(Solution().findItinerary(tickets));
+
+    return 0;
+}
