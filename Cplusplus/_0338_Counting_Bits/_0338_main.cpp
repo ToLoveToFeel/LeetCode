20a6a5a858e5d9456967c96f0cbde41fa8008319
ToLoveToFeel <1137247975@qq.com>
2021-02-25 21:11:57 +0800

    Leetcode 0338. 比特位计数

---
+++ Cplusplus/_0338_Counting_Bits/_0338_main.cpp
@@ -0,0 +1,34 @@
+// Created by WXX on 2021/2/25 21:09
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 递推
+class Solution {
+public:
+    vector<int> countBits(int num) {
+
+        vector<int> f(num + 1);
+        for (int i = 1; i <= num; i++)
+            f[i] = f[i >> 1] + (i & 1);
+        return f;
+    }
+};
+
+void OutputBasicArray1D(vector<int> nums) {
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+
+    OutputBasicArray1D(Solution().countBits(2));  // [0, 1, 1]
+    OutputBasicArray1D(Solution().countBits(5));  // [0, 1, 1, 2, 1, 2]
+
+    return 0;
+}
