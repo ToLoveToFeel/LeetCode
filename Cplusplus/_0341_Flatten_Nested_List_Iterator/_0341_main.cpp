9fd4d17e414e671d8dc59a87390ed7716666c9a9
ToLoveToFeel <1137247975@qq.com>
2021-02-25 21:23:38 +0800

    Leetcode 0341. 扁平化嵌套列表迭代器

---
+++ Cplusplus/_0341_Flatten_Nested_List_Iterator/_0341_main.cpp
@@ -0,0 +1,45 @@
+// Created by WXX on 2021/2/25 21:17
+#include <vector>
+
+using namespace std;
+
+class NestedInteger {
+public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+
+// 分为两步：(1) 将数据遍历存入vector中；(2) 从vector中获取数据
+class NestedIterator {
+public:
+    vector<int> q;
+    int k = 0;
+
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        k = 0;
+        for (auto &l : nestedList) dfs(l);
+    }
+
+    void dfs(NestedInteger &l) {
+        if (l.isInteger()) q.push_back(l.getInteger());
+        else {
+            for (auto v : l.getList()) dfs(v);
+        }
+    }
+
+    int next() {
+        return q[k++];
+    }
+
+    bool hasNext() {
+        return k < q.size();
+    }
+};
