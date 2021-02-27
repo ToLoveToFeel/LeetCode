7cbed09df0ac14087cf3ad302e5dc6f534ef895c
ToLoveToFeel <1137247975@qq.com>
2021-02-27 22:49:13 +0800

    Leetcode 0662 二叉树最大宽度

---
+++ Cplusplus/_0662_Maximum_Width_of_Binary_Tree/_0662_main.cpp
@@ -0,0 +1,46 @@
+// Created by WXX on 2021/2/27 22:32
+#include "MyTree.h"
+
+// 如果当前节点编号为p，则其左孩子编号为2*p，右儿子为2*p+1
+// 为了防止层数过多，编号报int，因此遍历每一层时，如果遍历到t
+// 将下一层需要插入的数据变为: (t-该层第一个数据+1) * 2 / (t-该层第一个数据+1) * 2 + 1
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+
+        if (!root) return 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 1});
+        int res = 1;
+        while (q.size()) {
+            int sz = q.size();
+            int l = q.front().second, r = 0;
+
+            for (int i = 0; i < sz; i++) {
+                auto t = q.front(); q.pop();
+                auto v = t.first;
+                auto p = t.second - l + 1;
+                r = t.second;
+                if (v->left) q.push({v->left, p * 2});
+                if (v->right) q.push({v->right, p * 2 + 1});
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
+
+int main() {
+
+    int nu = INT_MAX;
+    vector<int> nums = {
+            1,
+            3, 2,
+            5, nu, nu, 9,
+            6, nu, nu, nu, nu, nu, nu, 7
+    };
+    TreeNode *root = MyTree(nums).getRoot();
+    cout << Solution().widthOfBinaryTree(root) << endl;  // 8
+
+    return 0;
+}
