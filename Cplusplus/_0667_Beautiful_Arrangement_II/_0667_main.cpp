7de1b5bc466a0f90e8bd3269d2b2d7540ced8d2a
ToLoveToFeel <1137247975@qq.com>
2021-02-27 23:36:44 +0800

    LeetCode 0667 优美的排列 II

---
+++ Cplusplus/_0667_Beautiful_Arrangement_II/_0667_main.cpp
@@ -0,0 +1,39 @@
+// Created by WXX on 2021/2/27 23:28
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 这是一个构造题目
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+
+        vector<int> res(n);
+        for (int i = 0; i < n - k - 1; i++) res[i] = i + 1;
+        int u = n - k - 1;
+        int i = n - k, j = n;
+        while (u < n) {
+            res[u++] = i++;
+            if (u < n) res[u++] = j--;
+        }
+        return res;
+    }
+};
+
+void OutputBasicArray1D(vector<int> nums) {
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+
+    OutputBasicArray1D(Solution().constructArray(3, 1));  // [1, 2, 3]
+    OutputBasicArray1D(Solution().constructArray(3, 2));  // [1, 3, 2]
+
+    return 0;
+}
