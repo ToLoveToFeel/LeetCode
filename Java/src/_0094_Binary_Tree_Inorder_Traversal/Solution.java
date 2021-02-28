6d392c1770bd728c77805dea909729aa3d4b1f45
ToLoveToFeel <1137247975@qq.com>
2021-02-28 14:13:45 +0800

    Leetcode 0094 二叉树的中序遍历

--- Java/src/_0094_Binary_Tree_Inorder_Traversal/Solution.java
+++ Java/src/_0094_Binary_Tree_Inorder_Traversal/Solution.java
@@ -5,23 +5,33 @@ import java.util.List;
 
 /**
  * 递归算法
- * 时间复杂度：O(n)，n为节点数目
- * 空间复杂度：O(h)，h为二叉树高度
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：36.6 MB, 在所有 Java 提交中击败了70.81%的用户
  */
 public class Solution {
+    ArrayList<Integer> res = new ArrayList<>();
 
     public List<Integer> inorderTraversal(TreeNode root) {
-
-        ArrayList<Integer> res = new ArrayList<>();
-        inorderTraversal(root, res);
+        dfs(root);
         return res;
     }
 
-    private void inorderTraversal(TreeNode node, List<Integer> list) {
-        if (node != null) {
-            inorderTraversal(node.left, list);
-            list.add(node.val);
-            inorderTraversal(node.right, list);
-        }
+    private void dfs(TreeNode node) {
+        if (node == null) return;
+        dfs(node.left);
+        res.add(node.val);
+        dfs(node.right);
+    }
+
+    public static void main(String[] args) {
+
+        int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
+        int[] nums = new int[]{
+                1,
+                nu, 2,
+                nu, nu, 3
+        };
+        TreeNode root = (new MyTree(nums)).getRoot();
+        System.out.println((new Solution()).inorderTraversal(root));
     }
 }
--- Java/src/_0094_Binary_Tree_Inorder_Traversal/Solution4.java
+++ Java/src/_0094_Binary_Tree_Inorder_Traversal/Solution4.java
@@ -1,34 +1,39 @@
 package _0094_Binary_Tree_Inorder_Traversal;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
- * 经典非递归算法，对应前序遍历的Solution4
- * 时间复杂度：O(n)，n为节点数目
- * 空间复杂度：O(h)，h为二叉树高度
+ * 经典非递归算法
+ * 执行用时：1 ms, 在所有 Java 提交中击败了41.71%的用户
+ * 内存消耗：36.9 MB, 在所有 Java 提交中击败了12.74%的用户
  */
 public class Solution4 {
 
     public List<Integer> inorderTraversal(TreeNode root) {
 
         ArrayList<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curNode = root;
-        while (curNode != null || !stack.isEmpty()) {
-            while (curNode != null) {
-                stack.push(curNode);
-                curNode = curNode.left;
+        Deque<TreeNode> stk = new ArrayDeque<>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
             }
-
-            curNode = stack.pop();
-            res.add(curNode.val);
-            curNode = curNode.right;
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
         }
-
         return res;
+    }
+
+    public static void main(String[] args) {
+
+        int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
+        int[] nums = new int[]{
+                1,
+                nu, 2,
+                nu, nu, 3
+        };
+        TreeNode root = (new MyTree(nums)).getRoot();
+        System.out.println((new Solution4()).inorderTraversal(root));
     }
 }
