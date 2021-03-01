98cc732a204acbf063f70949c077ac33bd98b159
ToLoveToFeel <1137247975@qq.com>
2021-03-01 08:33:21 +0800

    Leetcode 0303 区域和检索 - 数组不可变

--- Java/src/_0303_Range_Sum_Query_Immutable/Solution2.java
+++ Java/src/_0303_Range_Sum_Query_Immutable/Solution2.java
@@ -2,25 +2,26 @@ package _0303_Range_Sum_Query_Immutable;
 
 /**
  * 前缀和
+ * 执行用时：10 ms, 在所有 Java 提交中击败了77.14%的用户
+ * 内存消耗：41.3 MB, 在所有 Java 提交中击败了60.70%的用户
  */
 public class Solution2 {
 
     static class NumArray {
 
-        private int[] sum;  // sum[i]存储前i个元素和， sum[0] = 0
+        private int[] s;  // sum[i]存储前i个元素和， sum[0] = 0
 
         public NumArray(int[] nums) {
 
-            sum = new int[nums.length + 1];
-            sum[0] = 0;
-            for (int i = 1; i < sum.length; i++)
-                sum[i] = sum[i - 1] + nums[i - 1];
+            s = new int[nums.length + 1];
+            s[0] = 0;
+            for (int i = 1; i < s.length; i++) s[i] = s[i - 1] + nums[i - 1];
         }
 
         public int sumRange(int i, int j) {
             i++;
             j++;
-            return sum[j] - sum[i - 1];
+            return s[j] - s[i - 1];
         }
     }
 
