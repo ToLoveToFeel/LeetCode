51cd921dbda6242336b2995e8263b5926a3ae304
ToLoveToFeel <1137247975@qq.com>
2021-03-09 09:24:45 +0800

    Leetcode 0680 验证回文字符串 Ⅱ

--- Cplusplus/_0680_Valid_Palindrome_II/_0680_main.cpp
+++ Cplusplus/_0680_Valid_Palindrome_II/_0680_main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 // 思路：双指针，如果遇到不相同的位置，可以尝试跳过左边的或者右边的
 // 如果存在这样的回文串，我们的算法一定可以找到
+/**
+ * 执行用时：52 ms, 在所有 C++ 提交中击败了71.46%的用户
+ * 内存消耗：19.1 MB, 在所有 C++ 提交中击败了91.75%的用户
+ */
 class Solution {
 public:
     bool check(string &s, int i, int j) {
