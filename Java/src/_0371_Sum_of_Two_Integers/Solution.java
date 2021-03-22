d302a905f293bcd39b4374010597165d266d2a51
ToLoveToFeel <1137247975@qq.com>
2021-03-22 09:50:34 +0800

    Leetcode 0371 两整数之和

--- Java/src/_0371_Sum_of_Two_Integers/Solution.java
+++ Java/src/_0371_Sum_of_Two_Integers/Solution.java
@@ -5,18 +5,18 @@ package _0371_Sum_of_Two_Integers;
  * Content:
  */
 public class Solution {
-
     public int getSum(int a, int b) {
+
         while (b != 0) {
             int x = a, y = b;
             a = (x ^ y);
             b = ((x & y) << 1);
         }
-
         return a;
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).getSum(13, 11));
     }
 }
