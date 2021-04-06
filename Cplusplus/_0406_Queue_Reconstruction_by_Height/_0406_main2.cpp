309d0fa927c0489f049d66704ae0a744b62098d9
ToLoveToFeel <1137247975@qq.com>
2021-04-06 16:48:52 +0800

    Leetcode 0406 根据身高重建队列

--- Cplusplus/_0406_Queue_Reconstruction_by_Height/_0406_main2.cpp
+++ Cplusplus/_0406_Queue_Reconstruction_by_Height/_0406_main2.cpp
@@ -7,24 +7,50 @@ using namespace std;
 
 // 具体分析参考：https://blog.csdn.net/weixin_42638946/article/details/114502760?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522161769890016780255260256%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fblog.%2522%257D&request_id=161769890016780255260256&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~first_rank_v2~rank_v29-1-114502760.nonecase&utm_term=%E6%A0%91%E7%8A%B6
 /**
- * 执行用时：200 ms, 在所有 C++ 提交中击败了35.38%的用户
- * 内存消耗：11.8 MB, 在所有 C++ 提交中击败了70.56%的用户
+ * 执行用时：104 ms, 在所有 C++ 提交中击败了76.69%的用户
+ * 内存消耗：23.9 MB, 在所有 C++ 提交中击败了24.85%的用户
  */
 class Solution {
 public:
-    vector<vector<int>> reconstructQueue(vector<vector<int>> &people) {
+    int n;
+    vector<int> tr;
 
-        // 按照第一维降序，第二维升序排列
-        sort(people.begin(), people.end(), [](const vector<int> &a, const vector<int> &b) {
-            if (a[0] == b[0]) return a[1] < b[1];
-            return a[0] > b[0];
+    int lowbit(int x) {
+        return x & -x;
+    }
+
+    void add(int x, int c) {
+        for (int i = x; i <= n; i += lowbit(i)) tr[i] += c;
+    }
+
+    int query(int x) {
+        int res = 0;
+        for (int i = x; i; i -= lowbit(i)) res += tr[i];
+        return res;
+    }
+
+    vector<vector<int>> reconstructQueue(vector<vector<int>> &people) {
+        n = people.size();
+        tr.resize(n + 1);  // 树状数组下标必须从1开始
+
+        sort(people.begin(), people.end(), [](vector<int> a, vector<int> b) {
+            if (a[0] != b[0]) return a[0] < b[0];  // 按照第一维升序
+            return a[1] > b[1];  // 按照第二维降序
         });
 
-        vector<vector<int>> res;
-        for (auto &p : people) {
-            res.insert(res.begin() + p[1], p);
+        vector<vector<int>> res(n);
+        for (auto p : people) {
+            int h = p[0], k = p[1];
+            int l = 1, r = n;
+            while (l < r) {
+                int mid = l + r >> 1;
+                // query(mid)返回a[1..mid]中1的个数，0表示该位置没被占用
+                if (mid - query(mid) >= k + 1) r = mid;
+                else l = mid + 1;
+            }
+            res[r - 1] = p;  // a[1]表示第0个位置的占用情况
+            add(r, 1);
         }
-
         return res;
     }
 };
