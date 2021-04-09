de534160793a5669abfab15f317e95cdb2984363
ToLoveToFeel <1137247975@qq.com>
2021-04-09 16:16:56 +0800

    Leetcode 0211 添加与搜索单词

--- Java/src/_0211_Add_and_Search_Word_Data_structure_design/WordDictionary.java
+++ Java/src/_0211_Add_and_Search_Word_Data_structure_design/WordDictionary.java
@@ -4,32 +4,36 @@ import java.util.TreeMap;
 
 public class WordDictionary {
 
-    private class Node{
+    private class Node {
 
         public boolean isWord;
         public TreeMap<Character, Node> next;
 
-        public Node(boolean isWord){
+        public Node(boolean isWord) {
             this.isWord = isWord;
             next = new TreeMap<>();
         }
 
-        public Node(){
+        public Node() {
             this(false);
         }
     }
 
     private Node root;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public WordDictionary() {
         root = new Node();
     }
 
-    /** Adds a word into the data structure. */
+    /**
+     * Adds a word into the data structure.
+     */
     public void addWord(String word) {
         Node cur = root;
-        for (int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             if (cur.next.get(c) == null)
                 cur.next.put(c, new Node());
@@ -38,28 +42,28 @@ public class WordDictionary {
         cur.isWord = true;
     }
 
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
     public boolean search(String word) {
         return match(root, word, 0);
     }
 
     // 在以node为根节点的字典树中查找是否存在单词word，index表示匹配到第几个字符
-    private boolean match(Node node, String word, int index){
+    private boolean match(Node node, String word, int index) {
         if (index == word.length())
             return node.isWord;
 
         char c = word.charAt(index);
-        if (c != '.'){
+        if (c != '.') {
             if (node.next.get(c) == null)
                 return false;
             return match(node.next.get(c), word, index + 1);
-        }
-        else{  // c == '.'
+        } else {  // c == '.'
             for (char nextChar : node.next.keySet())
                 if (match(node.next.get(nextChar), word, index + 1))
                     return true;
             return false;
         }
-
     }
 }
