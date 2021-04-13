7f44a09cde4ee9d414c84adaedc9bc0b4b2907c2
ToLoveToFeel <1137247975@qq.com>
2021-04-13 09:10:08 +0800

    Leetcode 0785 判断二分图

--- Java/src/_0785_Is_Graph_Bipartite/Solution.java
+++ Java/src/_0785_Is_Graph_Bipartite/Solution.java
@@ -1,36 +1,42 @@
 package _0785_Is_Graph_Bipartite;
 
+/**
+ * Date: 2021/4/13 8:59
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：38.9 MB, 在所有 Java 提交中击败了65.81%的用户
+ */
 public class Solution {
 
-    private boolean[] visited;  // 记录图是否访问过
-    private int[] colors;  // 记录顶点染的颜色
-    private int[][] graph;
+    int[] color;  // 0代表当前节点还未染色, 1、2分别表示两种颜色
+    int[][] g;
 
     public boolean isBipartite(int[][] graph) {
 
-        int V = graph.length;  // 节点数目
-        visited = new boolean[V];  // 默认false
-        colors = new int[V];  // 默认为0
-        this.graph = graph;
-
-        for (int v = 0; v < V; v++)
-            if (!visited[v])
-                if (!dfs(v, 0))
+        g = graph;
+        int n = g.length;  // 图中顶点数
+        color = new int[n];
+        for (int i = 0; i < n; i++)
+            if (color[i] == 0)
+                if (!dfs(i, 1))
                     return false;
-
         return true;
     }
 
-    private boolean dfs(int v, int color){
-        visited[v] = true;
-        colors[v] = color;
-        for (int w : graph[v])
-            if (!visited[w]) {
-                if (!dfs(w, 1 - color))
-                    return false;
-            }
-            else if (colors[v] == colors[w])  // w已经染过色，判断w颜色和当前节点v染色是否相同，相同不是二分图
-                return false;
+    private boolean dfs(int u, int c) {
+        color[u] = c;
+        for (int w : g[u]) {
+            if (color[w] == 0) {
+                if (!dfs(w, 3 - c)) return false;
+            } else if (color[w] == c) return false;
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+
+//        int[][] graph = {{1, 2, 3}, {0, 2}, {0, 1, 3}, {0, 2}};
+//        System.out.println((new Solution()).isBipartite(graph));  // false
+        int[][] graph = {{1, 3}, {0, 2}, {1, 3}, {0, 2}};
+        System.out.println((new Solution()).isBipartite(graph));  // true
     }
 }
