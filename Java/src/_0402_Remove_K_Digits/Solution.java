363c7e651e5768fd8720fe9f7ce9076ae4fba664
ToLoveToFeel <1137247975@qq.com>
2021-04-21 18:39:34 +0800

    Leetcode 0402 移除K位数字

--- Java/src/_0402_Remove_K_Digits/Solution.java
+++ Java/src/_0402_Remove_K_Digits/Solution.java
@@ -1,52 +1,45 @@
 package _0402_Remove_K_Digits;
 
+import com.alibaba.druid.sql.visitor.functions.Char;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
  * Date: 2020/11/15 11:27
- * Content: 栈(单调递增栈)
- * https://leetcode-cn.com/problems/remove-k-digits/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-5/
- *
- * 执行用时：12 ms, 在所有 Java 提交中击败了39.25%的用户
- * 内存消耗：39.1 MB, 在所有 Java 提交中击败了33.83%的用户
+ * Content: 栈
+ * 执行用时：10 ms, 在所有 Java 提交中击败了57.78%的用户
+ * 内存消耗：39.1 MB, 在所有 Java 提交中击败了21.50%的用户
  */
 public class Solution {
 
     public String removeKdigits(String num, int k) {
-        if (num.length() == k)
-            return "0";
 
-        // 利用单调栈解决
-        Deque<Character> stack = new ArrayDeque<>();
-        int len = num.length() - k;  // 最终数据的长度
+        Deque<Character> stk = new ArrayDeque<>();
+        stk.push('0');
         for (char c : num.toCharArray()) {
-            // 还要删除字符 && 栈非空 && 当前考察的元素<栈顶元素
-            while (k > 0 && !stack.isEmpty() && c < stack.peek()){
-                stack.pop();
+            while (k > 0 && c < stk.peek()) {
+                stk.pop();
                 k--;
             }
-            stack.push(c);
+            stk.push(c);
         }
+        while (k-- > 0)  stk.pop();
 
+        // 栈转为String
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        String res = sb.reverse().substring(0, len).replaceAll("^(0+)", "");
-        return res.equals("") ? "0" : res;
+        while (!stk.isEmpty()) sb.append(stk.pop());
+        String res = sb.reverse().toString();
+        // 去掉前导0
+        k = 0;
+        while (k + 1 < res.length() && res.charAt(k) == '0') k++;
+        return res.substring(k);
     }
 
     public static void main(String[] args) {
 
-//        String num = "1432219";  // 1219
-//        int k = 3;
-//        System.out.println((new Solution()).removeKdigits(num, k));
-//        String num = "100200";  // 200
-//        int k = 1;
-//        System.out.println((new Solution()).removeKdigits(num, k));
-        String num = "10";  // 0
-        int k = 1;
-        System.out.println((new Solution()).removeKdigits(num, k));
+        System.out.println((new Solution()).removeKdigits("1432219", 3));
+        System.out.println((new Solution()).removeKdigits("100200", 1));
+        System.out.println((new Solution()).removeKdigits("10", 1));
     }
 }
