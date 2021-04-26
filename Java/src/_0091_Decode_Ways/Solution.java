8e1632d0268bebc2ca3e815fcf9a794fdb021144
ToLoveToFeel <1137247975@qq.com>
2021-04-26 17:04:24 +0800

    Leetcode 0091 解码方法

--- Java/src/_0091_Decode_Ways/Solution.java
+++ Java/src/_0091_Decode_Ways/Solution.java
@@ -11,13 +11,13 @@ public class Solution {
     public int numDecodings(String s) {
 
         int n = s.length();
-        s = ' ' + s;
+        char[] cs = (' ' + s).toCharArray();
         int[] f = new int[n + 1];
         f[0] = 1;
-        for (int i = 0; i <= n; i++) {
-            if (s.charAt(i) >= '1' && s.charAt(i) <= '9') f[i] += f[i - 1];
+        for (int i = 1; i <= n; i++) {
+            if (cs[i] >= '1' && cs[i] <= '9') f[i] += f[i - 1];
             if (i > 1) {
-                int t = (s.charAt(i - 1) - '0') * 10 + s.charAt(i) - '0';
+                int t = (cs[i - 1] - '0') * 10 + cs[i] - '0';
                 if (t >= 10 && t <= 26) f[i] += f[i - 2];
             }
         }
@@ -27,8 +27,6 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        String s = "226";  // 3
-        System.out.println((new Solution()).numDecodings(s));
-
+        System.out.println((new Solution()).numDecodings("226"));  // 3
     }
 }
