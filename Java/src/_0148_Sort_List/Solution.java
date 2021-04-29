825e1fd23df4d994ef12ec5c20c9490b6c58e515
ToLoveToFeel <1137247975@qq.com>
2021-04-29 22:16:14 +0800

    Leetcode 0148 排序链表

--- Java/src/_0148_Sort_List/Solution.java
+++ Java/src/_0148_Sort_List/Solution.java
@@ -11,10 +11,9 @@ public class Solution {
     // Leetcode 0021 合并两个有序链表
     // 返回l1和l2合并后链表的头结点
     public ListNode merge(ListNode l1, ListNode l2) {
-
-        if (l1 == null || l2 == null)
-            return l1 == null ? l2 : l1;
-        else if (l1.val < l2.val) { // 递归
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) { // 递归
             l1.next = merge(l1.next, l2);
             return l1;
         } else {
@@ -25,8 +24,7 @@ public class Solution {
 
     public ListNode sortList(ListNode head) {
         // 递归到底情况处理，链表为空，或者链表只有一个节点
-        if (head == null || head.next == null)
-            return head;
+        if (head == null || head.next == null) return head;
 
         // 快慢指针，寻找中间点
         ListNode s = head, f = head;
@@ -37,9 +35,7 @@ public class Solution {
         ListNode newHead = s.next;
         s.next = null;  // 断开链表，分成前后两部分
 
-        ListNode left = sortList(head);
-        ListNode right = sortList(newHead);
-
+        ListNode left = sortList(head), right = sortList(newHead);
         return merge(left, right);  // 返回合并后的链表头指针
     }
 
