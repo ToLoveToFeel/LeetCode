4122024fe30d3037778c11eebfb25350850ba116
ToLoveToFeel <1137247975@qq.com>
2021-04-30 16:07:36 +0800

    Leetcode 0165 比较版本号

--- Java/src/_0165_Compare_Version_Numbers/Solution.java
+++ Java/src/_0165_Compare_Version_Numbers/Solution.java
@@ -10,25 +10,20 @@ public class Solution {
 
     public int compareVersion(String v1, String v2) {
 
-        for (int i = 0, j = 0; i < v1.length() || j < v2.length(); ) {
-            int a = i, b = j;
-            while (a < v1.length() && v1.charAt(a) != '.') a++;
-            while (b < v2.length() && v2.charAt(b) != '.') b++;
-            int x = (a == i ? 0 : Integer.parseInt(v1.substring(i, a)));
-            int y = (b == j ? 0 : Integer.parseInt(v2.substring(j, b)));
+        String[] t1 = v1.split("\\."), t2 = v2.split("\\.");
+        for (int i = 0; i < t1.length || i < t2.length; i++) {
+            int x = i < t1.length ? Integer.parseInt(t1[i]) : 0;
+            int y = i < t2.length ? Integer.parseInt(t2[i]) : 0;
             if (x > y) return 1;
             if (x < y) return -1;
-            i = a + 1;
-            j = b + 1;
         }
         return 0;
     }
 
     public static void main(String[] args) {
 
-        String v1 = "1.01";
-        String v2 = "1.001";
-        System.out.println((new Solution()).compareVersion(v1, v2));  // 0
+//        System.out.println((new Solution()).compareVersion("1.01", "1.001"));  // 0
+        System.out.println((new Solution()).compareVersion("0.1", "1.1"));  // -1
 
     }
 }
