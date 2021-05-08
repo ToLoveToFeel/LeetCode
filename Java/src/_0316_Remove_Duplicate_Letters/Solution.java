2a68d89249f036c729812c6e1273aada3045c3b9
ToLoveToFeel <1137247975@qq.com>
2021-05-08 22:59:46 +0800

    Leetcode 0316 去除重复字母

--- Java/src/_0316_Remove_Duplicate_Letters/Solution.java
+++ Java/src/_0316_Remove_Duplicate_Letters/Solution.java
@@ -44,7 +44,8 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println((new Solution()).removeDuplicateLetters("a"));  // a
+//        System.out.println((new Solution()).removeDuplicateLetters("a"));  // a
+        System.out.println((new Solution()).removeDuplicateLetters("bcabc"));  // abc
         System.out.println((new Solution()).removeDuplicateLetters("cbacdcbc"));  // acdb
     }
 }
