5a88d61e48f1ef20fdfe50579941ea4b064dd7fd
ToLoveToFeel <1137247975@qq.com>
2021-05-10 22:24:18 +0800

    Leetcode 0344 翻转字符串

--- Java/src/_0344_Reverse_String/Solution.java
+++ Java/src/_0344_Reverse_String/Solution.java
@@ -1,14 +1,14 @@
 package _0344_Reverse_String;
 
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 public class Solution {
-    // 时间复杂度：O(n)
-    // 空间复杂度：O(1)
-    public void reverseString(char[] s) {  // 对撞指针
-        int l = 0;
-        int r = s.length - 1;
 
-        while (l < r)
-            swap(s, l++, r--);
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) swap(s, l++, r--);
     }
 
     private void swap(char[] s, int l, int r) {
@@ -18,11 +18,10 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+
         char[] s = {'h', 'e', 'l', 'l', 'o'};
         System.out.println(s);
-
         (new Solution()).reverseString(s);
-
         System.out.println(s);
     }
 }
