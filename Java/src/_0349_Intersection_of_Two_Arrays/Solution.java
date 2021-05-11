dd56c2a59cfe005833cf9b74aee5a35222f9d70c
ToLoveToFeel <1137247975@qq.com>
2021-05-11 11:00:49 +0800

    Leetcode 0349 两个数组的交集

--- Java/src/_0349_Intersection_of_Two_Arrays/Solution.java
+++ Java/src/_0349_Intersection_of_Two_Arrays/Solution.java
@@ -1,35 +1,30 @@
 package _0349_Intersection_of_Two_Arrays;
 
+import _0000_study._common.Output;
+
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 class Solution {
 
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> comSet = new HashSet<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int val : nums1) {
-            comSet.add(val);
-        }
-        for (int val : nums2) {
-            if (comSet.contains(val)) {
-                arrayList.add(val);
-                comSet.remove(val);
+
+        HashSet<Integer> hash = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
+        for (int x : nums1) hash.add(x);
+        for (int x : nums2) {
+            if (hash.contains(x)) {
+                res.add(x);
+                hash.remove(x);
             }
         }
-        int[] ret = new int[arrayList.size()];
-        for (int i = 0; i < arrayList.size(); i++) {
-            ret[i] = arrayList.get(i);
-        }
-
-        return ret;
+        return res.stream().mapToInt(Integer::valueOf).toArray();
     }
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 2, 2, 1, 3, 3};
-        int[] nums2 = new int[]{2, 3, 2};
-        int[] ret = (new Solution()).intersection(nums1, nums2);
-        for (int val : ret)
-            System.out.println(val);
+
+        int[] nums1 = new int[]{1, 2, 2, 1, 3, 3}, nums2 = new int[]{2, 3, 2};
+        Output.OutputBasicArray1D((new Solution()).intersection(nums1, nums2));
     }
 }
