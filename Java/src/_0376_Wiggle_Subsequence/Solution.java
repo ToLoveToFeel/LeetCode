cd66976ecb7ca63668956a55b55399f06da4faf3
ToLoveToFeel <1137247975@qq.com>
2021-05-12 23:24:07 +0800

    Leetcode 0376 摆动子序列

--- Java/src/_0376_Wiggle_Subsequence/Solution.java
+++ Java/src/_0376_Wiggle_Subsequence/Solution.java
@@ -11,19 +11,11 @@ public class Solution {
 
     public int wiggleMaxLength(int[] nums) {
 
-        int n = nums.length;
-        int down = 1;  // 代表 摆动子序列中最后两个元素是下降的 子序列长度
-        int up = 1;  // 代表 摆动子序列中最后两个元素是上升的 子序列长度
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i]) {
-                up = down + 1;
-            } else if (nums[i - 1] > nums[i]) {
-                down = up + 1;
-            }
-        }
-
-        return n == 0 ? 0 : Math.max(down, up);
+        int up = 0, down = 0;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i - 1] < nums[i]) up = down + 1;
+            else if (nums[i - 1] > nums[i]) down = up + 1;
+        return Math.max(up, down) + 1;
     }
 
     public static void main(String[] args) {
@@ -31,7 +23,7 @@ public class Solution {
 //        int[] nums = {1, 7, 4, 9, 2, 5};  // 6
 //        int[] nums = {1, 17, 5, 10, 13, 15, 10, 5, 16, 8};  // 7
 //        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};  // 2
-        int[] nums = {0, 0};  // 0
+        int[] nums = {0, 0};  // 1
         System.out.println((new Solution()).wiggleMaxLength(nums));
     }
 }
