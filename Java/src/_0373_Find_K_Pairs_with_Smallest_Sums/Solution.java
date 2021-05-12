317be9eb81769bfd305a64503dda741e8e33ac0d
ToLoveToFeel <1137247975@qq.com>
2021-05-12 22:17:38 +0800

    Leetcode 0373 查找和最小的K对数字

---
+++ Java/src/_0373_Find_K_Pairs_with_Smallest_Sums/Solution.java
@@ -0,0 +1,35 @@
+package _0373_Find_K_Pairs_with_Smallest_Sums;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Date: 2021/5/12 22:16
+ */
+public class Solution {
+
+    public List<List<Integer>> kSmallestPairs(int[] a, int[] b, int k) {
+
+        int n = a.length, m = b.length;
+        if (n == 0 || m == 0) return new ArrayList<>();
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((o1, o2) -> { return o1[0] - o2[0]; });
+        for (int i = 0; i < m; i++) heap.add(new int[]{a[0] + b[i], 0, i});
+        List<List<Integer>> res = new ArrayList<>();
+        while (k-- != 0 && !heap.isEmpty()) {
+            int[] t = heap.remove();
+            res.add(Arrays.asList(a[t[1]], b[t[2]]));
+            if (t[1] + 1 < n)
+                heap.add(new int[]{a[t[1] + 1] + b[t[2]], t[1] + 1, t[2]});
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 7, 11}, b = {2, 4, 6};
+        System.out.println((new Solution()).kSmallestPairs(a, b, 3));  // [[1, 2], [1, 4], [1, 6]]
+    }
+}
