bc6d53b74575bae9e683527dfcadc92540602552
ToLoveToFeel <1137247975@qq.com>
2021-05-17 22:20:47 +0800

    python Leetcode 0003 无重复字符的最长子串

--- Python/_0003_Longest_Substring_Without_Repeating_Characters/Solution.py
+++ Python/_0003_Longest_Substring_Without_Repeating_Characters/Solution.py
@@ -1,28 +1,22 @@
 # coding=utf-8
 
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        freq = [0 for _ in range(256)]
-        l = 0
-        r = -1
-        res = 0
 
-        while l < len(s):
-            if r + 1 < len(s) and freq[ord(s[r + 1])] == 0:
-                r += 1
-                freq[ord(s[r])] += 1
-            else:
-                freq[ord(s[l])] -= 1
-                l += 1
-            res = max(res, r - l + 1)
+# 执行用时：80 ms, 在所有 Python3 提交中击败了56.19%的用户
+# 内存消耗：14.8 MB, 在所有 Python3 提交中击败了94.00%的用户
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
 
+        f = [0 for _ in range(256)]
+        j = res = 0
+        for i in range(len(s)):
+            f[ord(s[i])] += 1
+            while f[ord(s[i])] > 1:
+                f[ord(s[j])] -= 1
+                j += 1
+            res = max(res, i - j + 1)
         return res
 
 
 if __name__ == "__main__":
-    So = Solution()
-    print(So.lengthOfLongestSubstring("abcabcbb"))
+    print(Solution().lengthOfLongestSubstring("abcabcbb"))
+
