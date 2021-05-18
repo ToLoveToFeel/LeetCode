0e5b13c583735bbe77dbbabe5bf2005f2547e2fd
ToLoveToFeel <1137247975@qq.com>
2021-05-18 09:51:57 +0800

    python Leetcode 0054 螺旋矩阵

--- Python/_0054_Spiral_Matrix/Solution.py
+++ Python/_0054_Spiral_Matrix/Solution.py
@@ -1,23 +1,26 @@
 # coding=utf-8
+from typing import List
 
-class Solution(object):
-    """
-        1、对于这个列表矩阵，先输出第一行并将其pop除去，然后将矩阵逆时针旋转90度，
-           继续输出第一行并将其pop出去，递归的执行上述操作直至矩阵为空。
-    """
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        ret = []
 
-        while matrix:
-            ret.extend(matrix.pop(0))
-            matrix = list(map(list, zip(*matrix)))[::-1]
-            if not matrix or not matrix[0]:
-                break
-        return ret
+# 执行用时：40 ms, 在所有 Python3 提交中击败了60.05%的用户
+# 内存消耗：14.8 MB, 在所有 Python3 提交中击败了80.38%的用户
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        n = len(matrix); m = len(matrix[0])
+        dx = [0, 1, 0, -1]; dy = [1, 0, -1, 0]
+        st = [[False for _ in range(m)] for _ in range(n)]
+
+        res = []
+        x = y = d = 0
+        for i in range(n * m):
+            res.append(matrix[x][y])
+            st[x][y] = True
+            a = x + dx[d]; b = y + dy[d]
+            if a < 0 or a >= n or b < 0 or b >= m or st[a][b]:
+                d = (d + 1) % 4
+                a = x + dx[d]; b = y + dy[d]
+            x = a; y = b
+        return res
 
 
 if __name__ == "__main__":
@@ -26,7 +29,21 @@ if __name__ == "__main__":
         [4, 5, 6],
         [7, 8, 9]
     ]
-    So = Solution()
-    print(So.spiralOrder(matrix))
+    print(Solution().spiralOrder(matrix))
 
 
+# class Solution(object):
+#     def spiralOrder(self, matrix):
+#         """
+#         :type matrix: List[List[int]]
+#         :rtype: List[int]
+#         """
+#         res = []
+#
+#         while matrix:
+#             res.extend(matrix.pop(0))
+#             matrix = list(map(list, zip(*matrix)))[::-1]
+#             print(matrix)
+#             if not matrix or not matrix[0]:
+#                 break
+#         return res
