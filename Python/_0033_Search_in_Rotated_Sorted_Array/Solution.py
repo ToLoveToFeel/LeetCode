2d43959bca17ef77157d1f13374ed5a1b64dc120
ToLoveToFeel <1137247975@qq.com>
2021-05-21 21:37:47 +0800

    Leetcode 0033 搜索旋转排序数组

---
+++ Python/_0033_Search_in_Rotated_Sorted_Array/Solution.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# Date: 2021/5/21 21:36
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0; r = len(nums) - 1
+        while l < r:
+            mid = l + r + 1 >> 1
+            if nums[mid] >= nums[0]:
+                l = mid
+            else:
+                r = mid - 1
+
+        if target >= nums[0]:
+            l = 0
+        else:
+            l = r + 1
+            r = len(nums) - 1
+        while l < r:
+            mid = l + r >> 1
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid + 1
+
+        if nums[r] == target:
+            return r
+        else:
+            return -1
+
+
+if __name__ == "__main__":
+    nums = [4, 5, 6, 7, 0, 1, 2]
+    print(Solution().search(nums, 0))  # 4
