78302cb81ab3d5cdb673318f89b6e42eeaa79013
ToLoveToFeel <1137247975@qq.com>
2021-05-21 21:41:06 +0800

    Leetcode 0033 搜索旋转排序数组

--- Python/_0033_Search_in_Rotated_Sorted_Array/Solution.py
+++ Python/_0033_Search_in_Rotated_Sorted_Array/Solution.py
@@ -3,6 +3,8 @@
 from typing import List
 
 
+# 执行用时：44 ms, 在所有 Python3 提交中击败了38.71%的用户
+# 内存消耗：15.3 MB, 在所有 Python3 提交中击败了5.03%的用户
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0; r = len(nums) - 1
