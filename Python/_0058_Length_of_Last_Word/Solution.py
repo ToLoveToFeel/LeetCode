79fcb2d176592414808d424197f60ce924deda1c
ToLoveToFeel <1137247975@qq.com>
2021-05-24 17:04:01 +0800

    Leetcode 0058 最后一个单词的长度

--- Python/_0058_Length_of_Last_Word/Solution.py
+++ Python/_0058_Length_of_Last_Word/Solution.py
@@ -1,31 +1,11 @@
 # coding=utf-8
-# @Time       : 2020/3/4
-# @Author     : Wang Xiaoxiao
-# @University : Dalian University of Technology
-# @FileName   : Solution.py
-# @Software   : PyCharm
-# @github     : https://github.com/i-love-linux/LeetCode
-# 最后一个单词长度
 
 
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        s = s.strip()  # 删除前后空格
-
-        size = len(s)
-        count = 0
-        while size-1-count >= 0 and s[size-1-count] != " ":
-            count += 1
-
-        return count
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.strip(" ").split(' ')[-1])
 
 
 if __name__ == "__main__":
-
-    So = Solution()
-    print(So.lengthOfLastWord("Hello World"))
-
+    print(Solution().lengthOfLastWord(" "))
+    print(Solution().lengthOfLastWord("Hello World"))
