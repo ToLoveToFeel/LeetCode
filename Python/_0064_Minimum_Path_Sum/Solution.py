0918d16b595471230e2772168e3dfd11d0289a69
ToLoveToFeel <1137247975@qq.com>
2021-05-26 16:42:29 +0800

    // 示例 2：

--- Python/_0064_Minimum_Path_Sum/Solution.py
+++ Python/_0064_Minimum_Path_Sum/Solution.py
@@ -1,46 +1,22 @@
 # coding=utf-8
-# @Time       : 2020/3/7
-# @Author     : Wang Xiaoxiao
-# @University : Dalian University of Technology
-# @FileName   : Solution.py
-# @Software   : PyCharm
-# @github     : https://github.com/i-love-linux/LeetCode
-# 最小路径和
+from typing import List
 
 
-class Solution(object):
-    def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        if len(grid) == 0 or len(grid[0]) == 0:
-            return 0
-
-        r = len(grid) - 1
-        c = len(grid[0]) - 1
-        dp = [[-1 for _ in range(c + 1)] for _ in range(r+1)]
-
-        dp[r][c] = grid[r][c]
-        for i in range(r - 1, -1, -1):
-            dp[i][c] = dp[i+1][c] + grid[i][c]  # 填充最后一列的
-        for j in range(c - 1, -1, -1):
-            dp[r][j] = dp[r][j+1] + grid[r][j]  # 填充最后一行的
-
-        for i in range(r - 1, -1, -1):  # 填充其他的
-            for j in range(c - 1, -1, -1):
-                dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
-
-        return dp[0][0]
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid); m = len(grid[0])
+        f = [[int(1e8) for _ in range(m)] for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if i == 0 and j == 0:
+                    f[i][j] = grid[i][j]
+                else:
+                    if i > 0:
+                        f[i][j] = min(f[i][j], f[i - 1][j] + grid[i][j])
+                    if j > 0:
+                        f[i][j] = min(f[i][j], f[i][j - 1] + grid[i][j])
+        return f[n - 1][m - 1]
 
 
 if __name__ == "__main__":
-    grid = [
-              [1,3],
-              [1,1]
-            ]
-    So = Solution()
-    print(So.minPathSum(grid))
-
-
-
+    print(Solution().minPathSum([[1, 3], [1, 1]]))  # 3
