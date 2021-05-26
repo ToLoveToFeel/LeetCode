9b56ead61a6caa9822dfa5035bbdcdcc3bddbde1
ToLoveToFeel <1137247975@qq.com>
2021-05-26 16:53:31 +0800

    Leetcode 0071 简化路径

--- Python/_0071_Simplify_Path/Solution.py
+++ Python/_0071_Simplify_Path/Solution.py
@@ -1,27 +1,17 @@
 # coding=utf-8
-# @Time : 2020/3/11
-# 简化路径
 
-class Solution(object):
-    """
-        1、将原始路径按照"/" split划分，然后将每个元素依次放入栈中，如果当前元素是 "..",则对栈执行pop操作；
-        2、将栈中的元素按照规范的格式进行输出
-    """
-    def simplifyPath(self, path):
-        """
-        :type path: str
-        :rtype: str
-        """
-        stack = []
-        pathList = path.strip('/').split('/')
-        for item in pathList:
-            if item not in ('.', '..', ''):
-                stack.append(item)
-            elif item == '..' and stack:
-                stack.pop()
-        return '/' + '/'.join(stack)
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        sl = path.split('/')
+        stk = []
+        for s in sl:
+            if s != '.' and s != ".." and s != "":
+                stk.append(s)
+            elif s == ".." and len(stk) != 0:
+                stk.pop(len(stk) - 1)
+        return "/" + "/".join(stk)
 
 
 if __name__ == "__main__":
-    print(Solution().simplifyPath("/home/////foo/"))
-
+    print(Solution().simplifyPath("/home/////foo/"))  # /home/foo
