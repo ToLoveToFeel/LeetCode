a534936d8e0b3d7868b9fcc6fe9e65a324ef7f13
ToLoveToFeel <1137247975@qq.com>
2021-05-26 16:52:23 +0800

    Leetcode 0070 爬楼梯

--- Python/_0070_Climbing_Stairs/Solution.py
+++ Python/_0070_Climbing_Stairs/Solution.py
@@ -1,32 +1,15 @@
 # coding=utf-8
-# @Time       : 2020/3/10
-# @Author     : Wang Xiaoxiao
-# @University : Dalian University of Technology
-# @FileName   : Solution.py
-# @Software   : PyCharm
-# @github     : https://github.com/i-love-linux/LeetCode
-# 爬楼梯
 
 
-class Solution(object):
-    # 动态规划
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n == 1:
-            return 1
-
-        memo = [-1 for _ in range(n + 1)]
-        memo[0] = 1
-        memo[1] = 1
-        for i in range(2, n+1):
-            memo[i] = memo[i-1] + memo[i-2]
-
-        return memo[n]
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1; b = 1
+        for i in range(n - 1):
+            c = a + b
+            a = b
+            b = c
+        return b
 
 
 if __name__ == "__main__":
-    print(Solution().climbStairs(3))
-
+    print(Solution().climbStairs(3))  # 3
