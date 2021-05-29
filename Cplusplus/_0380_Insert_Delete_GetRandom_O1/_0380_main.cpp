73cf36ac2ed0122b7025528aeb9e9939db1a3125
ToLoveToFeel <1137247975@qq.com>
2021-05-29 16:21:52 +0800

    Leetcode 0380 O(1) 时间插入、删除和获取随机元素

---
+++ Cplusplus/_0380_Insert_Delete_GetRandom_O1/_0380_main.cpp
@@ -0,0 +1,57 @@
+// Created by WXX on 2021/5/29 16:13
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * 执行用时：48 ms, 在所有 C++ 提交中击败了59.19%的用户
+ * 内存消耗：22 MB, 在所有 C++ 提交中击败了77.50%的用户
+ */
+class RandomizedSet {
+public:
+
+    unordered_map<int, int> hash;  // (数据，数据在数组中的下标)
+    vector<int> nums;
+
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int x) {
+        if (hash.count(x) == 0) {
+            nums.push_back(x);
+            hash[x] = nums.size() - 1;
+            return true;
+        }
+        return false;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int x) {
+        if (hash.count(x)) {
+            int y = nums.back();
+            int px = hash[x], py = hash[y];  // x, y在nums对应的下标为px, py
+            swap(nums[px], nums[py]);
+            swap(hash[x], hash[y]);
+            nums.pop_back();
+            hash.erase(x);
+            return true;
+        }
+        return false;
+    }
+
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
