eb63bc4b12ed5e9f847c5c08916e2402c708721a
ToLoveToFeel <1137247975@qq.com>
2021-05-31 15:57:36 +0800

    Leetcode 0405 数字转换为十六进制

--- Java/src/_0405_Convert_a_Number_to_Hexadecimal/Solution.java
+++ Java/src/_0405_Convert_a_Number_to_Hexadecimal/Solution.java
@@ -5,41 +5,20 @@ package _0405_Convert_a_Number_to_Hexadecimal;
  * Content:
  */
 public class Solution {
-    private static final int MASK = 0b1111;
-
-    private char toSimpleHex(int num) {
-        char c = '0';
-        if (num < 10)
-            c = (char) ('0' + num);
-        else if (num < 16) {
-            c = (char) ('a' + num - 10);
-        }
-        return c;
-    }
 
     public String toHex(int num) {
-        if (num == 0)
-            return "0";
+        if (num == 0) return "0";
+        char[] nums = "0123456789abcdef".toCharArray();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) {  // int 为 32 位，每次处理 4 位，需要处理 8 次
-            sb.append(toSimpleHex(num & MASK));
-            num >>= 4;
+        while (num != 0) {
+            sb.append(nums[num & 0xf]);
+            num >>>= 4;  // >>
         }
-
-        sb = sb.reverse();
-        // 处理多余的 0
-        int start = 0;
-        for (int i = 0; i < 8; i++) {
-            if (sb.charAt(i) != '0') {
-                start = i;
-                break;
-            }
-        }
-
-        return sb.substring(start);
+        return sb.toString();
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).toHex(26));
         System.out.println((new Solution()).toHex(0));
         System.out.println((new Solution()).toHex(-1));
