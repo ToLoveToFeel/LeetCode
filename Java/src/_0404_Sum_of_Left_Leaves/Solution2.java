7d5a1c3fca17e938cf4a762c4e421102c5a6d25c
ToLoveToFeel <1137247975@qq.com>
2021-05-31 15:43:13 +0800

    Leetcode 0404 左叶子之和

--- Java/src/_0404_Sum_of_Left_Leaves/Solution2.java
+++ Java/src/_0404_Sum_of_Left_Leaves/Solution2.java
@@ -7,38 +7,34 @@ package _0404_Sum_of_Left_Leaves;
 public class Solution2 {
 
     int res = 0;
-    // 计算以 node 为根节点的二叉树的所有左叶子之和
-    private void dfs(TreeNode node, boolean isLeft) {
+    // 计算以 root 为根节点的二叉树的所有左叶子之和
+    private void dfs(TreeNode root) {
 
-        if (node == null)
-            return;
-        if (node.left == null && node.right == null && isLeft)
-            res += node.val;
+        if (root == null) return;
+        if (root.left != null)
+            if (root.left.left == null && root.left.right == null)
+                res += root.left.val;
 
-        dfs(node.left, true);
-        dfs(node.right, false);
+        dfs(root.left);
+        dfs(root.right);
     }
 
     // 递归前序遍历
     public int sumOfLeftLeaves(TreeNode root) {
-        dfs(root, false);
+        dfs(root);
         return res;
     }
 
     public static void main(String[] args) {
+
         int nu = Integer.MIN_VALUE;  // 用Integer.MIN_VALUE表示二叉树 null
-//        int[] nums = new int[]{
-//                3,
-//                9, 20,
-//                nu, nu, 15, 7
-//        };
         int[] nums = new int[]{
-                1,
-                2, 3,
-                4, 5, nu, nu
+                3,
+                9, 20,
+                nu, nu, 15, 7
         };
 
         TreeNode root = (new MyTree(nums)).getRoot();
-        System.out.println((new Solution2()).sumOfLeftLeaves(root));
+        System.out.println((new Solution2()).sumOfLeftLeaves(root));  // 24
     }
 }
