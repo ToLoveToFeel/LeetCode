a86c9a2172b442851957fd15e283b12edde66a56
ToLoveToFeel <1137247975@qq.com>
2021-05-31 20:12:37 +0800

    Leetcode 0416 分割等和子集

--- Java/src/_0416_Partition_Equal_Subset_Sum/Solution.java
+++ Java/src/_0416_Partition_Equal_Subset_Sum/Solution.java
@@ -29,11 +29,8 @@ class Solution {
     public boolean canPartition(int[] nums) {
 
         int sum = 0;
-        for (int i = 0; i < nums.length; i++)
-            sum += nums[i];
-
-        if (sum % 2 != 0)
-            return false;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
 
         return tryPartition(nums, nums.length - 1, sum / 2);
     }
--- Java/src/_0416_Partition_Equal_Subset_Sum/Solution3.java
+++ Java/src/_0416_Partition_Equal_Subset_Sum/Solution3.java
@@ -4,42 +4,26 @@ import java.util.Arrays;
 
 /**
  * Date: 2020/9/1 9:26
- * Content:
- * 0-1背包问题：
- * 使用动态规划
+ * Content: 0-1背包问题：
  */
 class Solution3 {
-    /*
-    典型的背包问题，在n个物品中选出一定物品，填满sum/2的背包
-    状态： F(n, C)考虑将n个物品填满容量为C的背包
-    状态转移：F(i, c) = F(i-1, c) || F(i-1, c-w(i))
-    时间复杂度：O(n * sum/2) = O(n * sum)
-     */
+
     public boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
 
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++)
-            sum += nums[i];
-
-        if (sum % 2 != 0)
-            return false;
-
-        int n = nums.length;
-        int C = sum / 2;
-        boolean[] dp = new boolean[C + 1];
-
-        for (int j = 0; j <= C; j++)
-            dp[j] = (nums[0] == j);
-        for (int i = 1; i < n; i++)
-            for (int j = C; j >= nums[i]; j--)
-                dp[j] = dp[j] || dp[j - nums[i]];
-
-        return dp[C];
+        int s = sum / 2;
+        boolean[] f = new boolean[s + 1];
+        f[0] = true;
+        for (int x : nums)
+            for (int j = sum / 2; j >= x; j--)
+                f[j] = f[j] || f[j - x];
+        return f[s];
     }
 
     public static void main(String[] args) {
 
         int[] nums = {1, 5, 11, 5};
-        System.out.println((new Solution3()).canPartition(nums));
+        System.out.println((new Solution3()).canPartition(nums));  // true
     }
 }
