84ee709aac4fc7bda1d2b92d2af796df22e0b144
ToLoveToFeel <1137247975@qq.com>
2021-06-03 19:18:13 +0800

    Leetcode 0447 回旋镖的数量

--- Java/src/_0447_Number_of_Boomerangs/Solution.java
+++ Java/src/_0447_Number_of_Boomerangs/Solution.java
@@ -3,15 +3,15 @@ package _0447_Number_of_Boomerangs;
 class Solution {
     // 时间复杂度：O(n^3)
     // Time Limit Exceeded
-    public int numberOfBoomerangs(int[][] points) {
+    public int numberOfBoomerangs(int[][] p) {
 
         int res = 0;
-        for (int i = 0; i < points.length; i++)
-            for (int j = 0; j < points.length; j++)
-                for (int k = 0; k < points.length; k++){
+        for (int i = 0; i < p.length; i++)
+            for (int j = 0; j < p.length; j++)
+                for (int k = 0; k < p.length; k++){
                     if (i != j && i != k && j != k){
-                        if (Math.pow(points[i][0] - points[j][0], 2) + Math.pow(points[i][1] - points[j][1], 2) ==
-                                Math.pow(points[i][0] - points[k][0], 2) + Math.pow(points[i][1] - points[k][1], 2)) {
+                        if (Math.pow(p[i][0] - p[j][0], 2) + Math.pow(p[i][1] - p[j][1], 2) ==
+                                Math.pow(p[i][0] - p[k][0], 2) + Math.pow(p[i][1] - p[k][1], 2)) {
                             res++;
                         }
                     }
@@ -20,8 +20,8 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        int[][] points = {{0, 0}, {1, 0}, {2, 0}};
 
+        int[][] points = {{0, 0}, {1, 0}, {2, 0}};
         System.out.println((new Solution()).numberOfBoomerangs(points));
     }
 }
--- Java/src/_0447_Number_of_Boomerangs/Solution2.java
+++ Java/src/_0447_Number_of_Boomerangs/Solution2.java
@@ -3,38 +3,26 @@ package _0447_Number_of_Boomerangs;
 import java.util.HashMap;
 
 public class Solution2 {
-    // 时间复杂度：O(n^2)
-    // 空间复杂度：O(n)
-    public int numberOfBoomerangs(int[][] points) {
+
+    public int numberOfBoomerangs(int[][] p) {
 
         int res = 0;
-
-        for (int i = 0; i < points.length; i++) {
-            HashMap<Integer, Integer> record = new HashMap<>();
-
-            for (int j = 0; j < points.length; j++)
+        for (int i = 0; i < p.length; i++) {
+            HashMap<Integer, Integer> hash = new HashMap<>();
+            for (int j = 0; j < p.length; j++)
                 if (i != j) {
-                    int distance = dis(points[i], points[j]);
-                    if (!record.containsKey(distance))
-                        record.put(distance, 1);
-                    else
-                        record.put(distance, record.get(distance) + 1);
+                    int dx = p[i][0] - p[j][0], dy = p[i][1] - p[j][1];
+                    int d = dx * dx + dy * dy;
+                    hash.put(d, hash.getOrDefault(d, 0) + 1);
                 }
-
-            for (Integer dis : record.keySet())
-                res += record.get(dis) * (record.get(dis) - 1);
+            for (int dis : hash.keySet()) res += hash.get(dis) * (hash.get(dis) - 1);
         }
-
         return res;
     }
 
-    private int dis(int[] pa, int[] pb){
-        return (pa[0] - pb[0]) * (pa[0] - pb[0]) + (pa[1] - pb[1]) * (pa[1] - pb[1]);
-    }
-
     public static void main(String[] args) {
-        int[][] points = {{0, 0}, {1, 0}, {2, 0}};
 
+        int[][] points = {{0, 0}, {1, 0}, {2, 0}};
         System.out.println((new Solution2()).numberOfBoomerangs(points));
     }
 }
