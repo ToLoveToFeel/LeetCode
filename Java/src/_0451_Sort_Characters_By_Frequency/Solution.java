0e68e30c5c4d6d3ae5077196d21519f96fe1d54a
ToLoveToFeel <1137247975@qq.com>
2021-06-04 09:34:05 +0800

    Leetcode 0451 根据字符出现的频率排序

--- Java/src/_0451_Sort_Characters_By_Frequency/Solution.java
+++ Java/src/_0451_Sort_Characters_By_Frequency/Solution.java
@@ -5,40 +5,24 @@ package _0451_Sort_Characters_By_Frequency;
 import java.util.*;
 
 public class Solution {
-    // Time Limit Exceeded
-    // 时间复杂度：O()
-    // 空间复杂度：O()
+
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c))
-                map.put(c, 1);
-            else
-                map.put(c, map.get(c) + 1);
+        HashMap<Character, Integer> cnt = new HashMap<>();
+        for (char x : s.toCharArray()) cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+
+        List<Character> list = new ArrayList<>(cnt.keySet());  // 转换为list
+        list.sort((a, b) -> cnt.get(b).compareTo(cnt.get(a)));
+        StringBuilder sb = new StringBuilder();
+        for (char c : list) {
+            for (int j = 0; j < cnt.get(c); j++)
+                sb.append(c);
         }
-
-        String res = "";
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet()); //转换为list
-//        list.sort(new Comparator<Map.Entry<Character, Integer>>() {
-//            @Override
-//            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-//                return o2.getValue().compareTo(o1.getValue());
-//            }
-//        });
-        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-
-        for (int i = 0; i < list.size(); i++){
-            char c = list.get(i).getKey();
-            for (int j = 0; j < list.get(i).getValue(); j++)
-                res += c;
-        }
-
-        return res;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution()).frequencySort("tree"));
         System.out.println((new Solution()).frequencySort("cccaaa"));
         System.out.println((new Solution()).frequencySort("Aabb"));
--- Java/src/_0451_Sort_Characters_By_Frequency/Solution2.java
+++ Java/src/_0451_Sort_Characters_By_Frequency/Solution2.java
@@ -5,32 +5,29 @@ package _0451_Sort_Characters_By_Frequency;
 import java.util.PriorityQueue;  // 默认小顶堆
 
 public class Solution2 {
+
     public String frequencySort(String s) {
         // 第一步：统计各个字母出现的次数
-        int[] letters = new int[128];
-        for (char c : s.toCharArray()) {
-            letters[c]++;
-        }
-
+        int[] cnt = new int[128];
+        for (char c : s.toCharArray()) cnt[c]++;
         // 第二步：创建优先队列
-        PriorityQueue<Character> queue = new PriorityQueue<>(128, (a, b) -> Integer.compare(letters[b], letters[a]));
+        PriorityQueue<Character> pq = new PriorityQueue<>(128,
+                (a, b) -> Integer.compare(cnt[b], cnt[a]));
         for (int i = 0; i < 128; i++) {
-            if (letters[i] != 0)
-                queue.add((char) i);
+            if (cnt[i] != 0)
+                pq.add((char) i);
         }
-
         // 第三步：从优先队列中取出元素，第一个元素出现的次数最多，得到答案
         StringBuilder sb = new StringBuilder();
-        while (!queue.isEmpty()) {
-            char c = queue.remove();
-            while (letters[c]-- > 0)
-                sb.append(c);
+        while (!pq.isEmpty()) {
+            char c = pq.remove();
+            while (cnt[c]-- > 0) sb.append(c);
         }
-
         return sb.toString();
     }
 
     public static void main(String[] args) {
+
         System.out.println((new Solution2()).frequencySort("tree"));
         System.out.println((new Solution2()).frequencySort("cccaaa"));
         System.out.println((new Solution2()).frequencySort("Aabb"));
