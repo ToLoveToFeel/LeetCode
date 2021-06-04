129983baeb660b6d5c853910ab0da9f6b4919661
ToLoveToFeel <1137247975@qq.com>
2021-06-04 10:30:30 +0800

    Leetcode 0454 四数相加 II

--- Java/src/_0454_4Sum_II/Solution.java
+++ Java/src/_0454_4Sum_II/Solution.java
@@ -7,30 +7,19 @@ package _0454_4Sum_II;
 class Solution {
 
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-
         int res = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                for (int k = 0; k < C.length; k++) {
-                    for (int l = 0; l < D.length; l++) {
-                        if (A[i] + B[j] + C[k] + D[l] == 0)
+        for (int a : A)
+            for (int b : B)
+                for (int c : C)
+                    for (int d : D)
+                        if (a + b + c + d == 0)
                             res++;
-                    }
-                }
-            }
-        }
-
         return res;
     }
 
     public static void main(String[] args) {
 
-        int[] A = {1, 2};
-        int[] B = {-2, -1};
-        int[] C = {-1, 2};
-        int[] D = {0, 2};
-
+        int[] A = {1, 2}, B = {-2, -1}, C = {-1, 2}, D = {0, 2};
         System.out.println((new Solution()).fourSumCount(A, B, C, D));
     }
 }
--- Java/src/_0454_4Sum_II/Solution2.java
+++ Java/src/_0454_4Sum_II/Solution2.java
@@ -13,35 +13,21 @@ public class Solution2 {
 
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
 
-        HashMap<Integer, Integer> record = new HashMap<>();
-
-        for (int i = 0; i < C.length; i++) {
-            for (int j = 0; j < D.length; j++) {
-                if (!record.containsKey(C[i] + D[j]))
-                    record.put(C[i] + D[j], 1);
-                else
-                    record.put(C[i] + D[j], record.get(C[i] + D[j]) + 1);
-            }
-        }
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int c : C)
+            for (int d : D)
+                hash.put(c + d, hash.getOrDefault(c + d, 0) + 1);
 
         int res = 0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                if (record.containsKey(0 - A[i] - B[j]))
-                    res += record.get(0 - A[i] - B[j]);
-            }
-        }
-
+        for (int a : A)
+            for (int b : B)
+                res += hash.getOrDefault(-(a + b), 0);
         return res;
     }
 
     public static void main(String[] args) {
 
-        int[] A = {1, 2};
-        int[] B = {-2, -1};
-        int[] C = {-1, 2};
-        int[] D = {0, 2};
-
-        System.out.println((new Solution2()).fourSumCount(A, B, C, D));
+        int[] A = {1, 2}, B = {-2, -1}, C = {-1, 2}, D = {0, 2};
+        System.out.println((new Solution2()).fourSumCount(A, B, C, D));  // 2
     }
 }
