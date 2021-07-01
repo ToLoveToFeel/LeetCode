d2aa4d935ddadfab78d95f337f0d04dc0f52e13b
ToLoveToFeel <1137247975@qq.com>
2021-07-01 14:23:53 +0800

    Leetcode 0980 不同路径 III

--- Java/src/_0980_Unique_Paths_III/Solution.java
+++ Java/src/_0980_Unique_Paths_III/Solution.java
@@ -1,63 +1,62 @@
 package _0980_Unique_Paths_III;
 
+/**
+ * 执行用时：2 ms, 在所有 Java 提交中击败了21.70%的用户
+ * 内存消耗：35.7 MB, 在所有 Java 提交中击败了61.60%的用户
+ */
 public class Solution {
-    // 图论问题，哈密尔顿路径问题
+
     private int[][] grid;
-    private int R, C;
-    private boolean[][] visited;
+    private int n, m;
+    private boolean[][] st;
     private int start, end;
 
-    private int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};  // 某个点的四个方向，上右下左
+    private final static int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};  // 某个点的四个方向，上右下左
 
     public int uniquePathsIII(int[][] grid) {
 
         this.grid = grid;
-        this.R = grid.length;
-        this.C = grid[0].length;
-        visited = new boolean[R][C];
-        int left = R * C;  // 记录走过的节点数目
+        n = grid.length;
+        m = grid[0].length;
+        st = new boolean[n][m];
 
-        for (int i = 0; i < R; i++)
-            for (int j = 0; j < C; j++)
-                if (grid[i][j] == 1){
-                    start = i * C + j;  // 二维数据压缩成一维数据
-                    grid[i][j] = 0;
-                }
-                else if (grid[i][j] == 2){
-                    end = i * C + j;
-                    grid[i][j] = 0;
-                }
-                else if (grid[i][j] == -1)
-                    left --;
-        return dfs(start, left);
+        int cnt = n * m;  // 初始表示多少个可以走的格子数目
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == 1) start = i * m + j;
+                else if (grid[i][j] == 2) end = i * m + j;
+                else if (grid[i][j] == -1) cnt--;
+
+        return dfs(start, cnt);
     }
 
-    private int dfs(int v, int left){
-        int x = v / C, y = v % C;
-        visited[x][y] = true;
-        left--;
-        if (left == 0 && v == end){
-            visited[x][y] = false;
+    private int dfs(int u, int cnt) {
+
+        int x = u / m, y = u % m;
+        if (--cnt == 0 && u == end) {
+            st[x][y] = false;
             return 1;
         }
 
         int res = 0;
-        for (int d = 0; d < 4; d++){
-            int newX = x + dirs[d][0], newY = y + dirs[d][1];
-            if (inArea(newX, newY) && grid[newX][newY] == 0 && !visited[newX][newY])
-                res += dfs(newX * C + newY, left);
+        st[x][y] = true;
+        for (int d = 0; d < 4; d++) {
+            int a = x + dirs[d][0], b = y + dirs[d][1];
+            if (a >= 0 && a < n && b >= 0 && b < m && grid[a][b] != -1 && !st[a][b])
+                res += dfs(a * m + b, cnt);
         }
+        st[x][y] = false;
 
-        visited[x][y] = false;
         return res;
     }
 
-    private boolean inArea(int x, int y){
-        return x >= 0 && x < R && y >= 0 && y < C;
-    }
-
     public static void main(String[] args) {
-        int[][] grid = {{1,0,0,0}, {0,0,0,0}, {0,0,0,2}};
-        System.out.println((new Solution()).uniquePathsIII(grid));
+
+        int[][] grid = {
+                {1, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 2}
+        };
+        System.out.println((new Solution()).uniquePathsIII(grid));  // 4
     }
 }
