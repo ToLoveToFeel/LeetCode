c5c655e3fd23f3b2cd96c738f7b345739c3b7488
ToLoveToFeel <1137247975@qq.com>
2021-07-08 09:26:27 +0800

    Leetcode 0930 和相同的二元子数组

---
+++ Cplusplus/_0930_Binary_Subarrays_With_Sum/_0930_main.cpp
@@ -0,0 +1,33 @@
+// Created by WXX on 2021/7/8 9:24
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * 执行用时：60 ms, 在所有 C++ 提交中击败了67.12%的用户
+ * 内存消耗：36.7 MB, 在所有 C++ 提交中击败了5.13%的用户
+ */
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal) {
+        int n = nums.size();
+        vector<int> s(n + 1);
+        for (int i = 1; i <= n; i++) s[i] = s[i - 1] + nums[i - 1];
+
+        int res = 0;
+        unordered_map<int, int> cnt;
+        for (int i = 1; i <= n; i++) {
+            cnt[s[i - 1]]++;
+            res += cnt[s[i] - goal];
+        }
+        return res;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
