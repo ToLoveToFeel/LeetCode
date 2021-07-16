2f260cbf0e8eb8e2b32517e667ae058ca91b5abf
ToLoveToFeel <1137247975@qq.com>
2021-07-16 17:05:42 +0800

    Leetcode 0538 把二叉搜索树转换为累加树

---
+++ Python/_0538_Convert_BST_to_Greater_Tree/Solution.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# Date: 2021/7/16 16:57
+from MyTree import *
+
+
+# 执行用时：84 ms, 在所有 Python3 提交中击败了24.67%的用户
+# 内存消耗：17.1 MB, 在所有 Python3 提交中击败了51.92%的用户
+class Solution:
+    def __init__(self):
+        self.sum = 0
+
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        self.dfs(root)
+        return root
+
+    def dfs(self, root):
+        if root is not None:
+            self.dfs(root.right)
+
+            self.sum += root.val
+            root.val = self.sum
+
+            self.dfs(root.left)
+
+
+if __name__ == "__main__":
+    nu = None
+    nums = [
+        5,
+        3, 13,
+        nu, 4, 7, 16
+    ]
+    myTree = MyTree(nums)
+    root = myTree.getRoot()
+    Solution().convertBST(root)
+    print(myTree)  # [[41], [48, 29], [45, 36, 16]]
