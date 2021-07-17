2bbff2242931f9488d67dfc1a5c6fa8e2c7fbece
ToLoveToFeel <1137247975@qq.com>
2021-07-17 15:47:47 +0800

    Leetcode 0647 回文子串

--- Java/src/_0647_Palindromic_Substrings/Solution.java
+++ Java/src/_0647_Palindromic_Substrings/Solution.java
@@ -4,7 +4,6 @@ package _0647_Palindromic_Substrings;
  * Date: 2020/12/15 13:56
  * Content: 暴力解法即可，根据回文串的奇偶中心扩散
  * 由数据范围反推算法复杂度以及算法内容 : https://www.acwing.com/blog/content/32/
- * <p>
  * 执行用时：4 ms, 在所有 Java 提交中击败了80.50%的用户
  * 内存消耗：36.6 MB, 在所有 Java 提交中击败了81.53%的用户
  */
@@ -12,17 +11,18 @@ public class Solution {
 
     public int countSubstrings(String s) {
 
+        char[] cs = s.toCharArray();
         int res = 0;
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < cs.length; i++) {
             // 枚举长度为奇数的情况
-            for (int j = i, k = i; j >= 0 && k < s.length(); j--, k++) {
-                if (s.charAt(j) != s.charAt(k))
+            for (int j = i, k = i; j >= 0 && k < cs.length; j--, k++) {
+                if (cs[j] != cs[k])
                     break;
                 res++;
             }
             // 枚举长度为偶数的情况
-            for (int j = i, k = i + 1; j >= 0 && k < s.length(); j--, k++) {
-                if (s.charAt(j) != s.charAt(k))
+            for (int j = i, k = i + 1; j >= 0 && k < cs.length; j--, k++) {
+                if (cs[j] != cs[k])
                     break;
                 res++;
             }
