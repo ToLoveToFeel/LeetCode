33752a82a99ceb775dabc2add9a63b132c075b1d
ToLoveToFeel <1137247975@qq.com>
2021-07-17 15:23:21 +0800

    Leetcode 0621 任务调度器

--- Java/src/_0621_Task_Scheduler/Solution.java
+++ Java/src/_0621_Task_Scheduler/Solution.java
@@ -2,10 +2,8 @@ package _0621_Task_Scheduler;
 
 /**
  * Date: 2020/12/5 14:26
- * Content:
- * https://leetcode-cn.com/problems/task-scheduler/solution/jian-ming-yi-dong-de-javajie-da-by-lan-s-jfl9/
+ * Content: https://leetcode-cn.com/problems/task-scheduler/solution/jian-ming-yi-dong-de-javajie-da-by-lan-s-jfl9/
  * 时间复杂度：O(tasks.length)
- * <p>
  * 执行用时：2 ms, 在所有 Java 提交中击败了97.81%的用户
  * 内存消耗：40.1 MB, 在所有 Java 提交中击败了29.36%的用户
  */
@@ -13,28 +11,18 @@ public class Solution {
 
     public int leastInterval(char[] tasks, int n) {
 
-        int length = tasks.length;
-        if (length < 2 || n == 0)
-            return length;
-
         // 第一步：计数排序(都是大写字母)
-        int[] letters = new int[26];
-        for (char c : tasks) {
-            letters[c - 'A']++;
-        }
-
+        int[] cnt = new int[26];
+        for (char c : tasks) cnt[c - 'A']++;
         // 第二步：计算出现次数最大值，以及有多少个任务和出现最多的那个任务出现次数一样
         int maxTimes = 0;  // 记录出现次数最多的那个任务
         int maxCount = 0;  // 一共有多少个任务和出现最多的那个任务出现次数一样
-        for (int i = 0; i < 26; i++) {
-            maxTimes = Math.max(maxTimes, letters[i]);
-        }
-        for (int i = 0; i < 26; i++) {
-            if (letters[i] == maxTimes)
+        for (int i = 0; i < 26; i++) maxTimes = Math.max(maxTimes, cnt[i]);
+        for (int i = 0; i < 26; i++)
+            if (cnt[i] == maxTimes)
                 maxCount++;
-        }
 
-        return Math.max(length, (maxTimes - 1) * (n + 1) + maxCount);
+        return Math.max(tasks.length, (maxTimes - 1) * (n + 1) + maxCount);
     }
 
     public static void main(String[] args) {
