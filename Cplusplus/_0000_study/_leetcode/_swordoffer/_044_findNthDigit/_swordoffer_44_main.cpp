8930a239fc0a7507899af3656619d010c7bf81a8
ToLoveToFeel <1137247975@qq.com>
2021-07-19 16:53:23 +0800

    剑指 Offer 44 数字序列中某一位的数字

---
+++ Cplusplus/_0000_study/_leetcode/_swordoffer/_044_findNthDigit/_swordoffer_44_main.cpp
@@ -0,0 +1,29 @@
+// Created by WXX on 2021/7/19 16:51
+#include <iostream>
+
+using namespace std;
+
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：5.8 MB, 在所有 C++ 提交中击败了83.36%的用户
+ */
+class Solution {
+public:
+    int findNthDigit(int n) {
+        long long k = 1, t = 9, s = 1;  // k位数有t个，我们考察的字符在第s个数据中
+        while (n > k * t) {
+            n -= k * t;
+            k++, t *= 10, s *= 10;
+        }
+        s += (n + k - 1) / k - 1;  // n/k上取整=(n+k-1)/k下取整
+        n = n % k ? n % k : k;
+        return to_string(s)[n - 1] - '0';
+    }
+};
+
+int main() {
+
+    cout << Solution().findNthDigit(3) << endl;  // 3
+
+    return 0;
+}
