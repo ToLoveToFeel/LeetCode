e0c5a7b7b8707c6fbd24ff985845931ee737d479
ToLoveToFeel <1137247975@qq.com>
2021-07-29 09:40:40 +0800

    Leetcode 1104 二叉树寻路

---
+++ Cplusplus/_1104_Path_In_Zigzag_Labelled_Binary_Tree/_1104_main.cpp
@@ -0,0 +1,41 @@
+// Created by WXX on 2021/7/29 9:36
+#include "MyTree.h"
+#include <cmath>
+
+/**
+ * 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+ * 内存消耗：6.4 MB, 在所有 C++ 提交中击败了9.01%的用户
+ */
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int t) {
+        int p[25];  // 存储2^i
+        p[0] = 1;
+        for (int i = 1; i < 25; i++) p[i] = p[i - 1] * 2;
+
+        int n = log(t) / log(2) + 1;  // 当前t位于第n层
+        vector<int> res(1, t);
+        while (n > 1) {
+            t = p[n - 2] + p[n - 1] - 1 - t / 2;
+            res.push_back(t);
+            n--;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+int main() {
+
+    int nu = INT_MAX;
+    vector<int> nums = {
+            1,
+            3, 2,
+            4, 5, 6, 7,
+            15, 14, 13, 12, 11, 10, 9, 8,
+    };
+    TreeNode *root = MyTree(nums).getRoot();
+    OutputBasicArray1D(Solution().pathInZigZagTree(14));  // [1, 3, 4, 14]
+
+    return 0;
+}
