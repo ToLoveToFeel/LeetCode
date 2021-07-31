7e89864885adc411ce063aa6d25a594d6069f83b
ToLoveToFeel <1137247975@qq.com>
2021-07-31 15:28:54 +0800

    Leetcode 0987 二叉树的垂序遍历

---
+++ Python/_0987_Vertical_Order_Traversal_of_a_Binary_Tree/MyTree.py
@@ -0,0 +1,138 @@
+# coding=utf-8
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+'''
+    nu = None
+    nums = [
+        1,
+        2, 2,
+        3, nu, 4, 3
+    ]
+    root = MyTree(nums).getRoot()
+'''
+
+
+class MyTree:
+    def __init__(self, nums=[]):
+        self.nums = nums
+        self.size = 0
+
+        if len(self.nums) == 0 or self.nums[0] == None:
+            return
+        self.root = TreeNode(self.nums[0])
+        self.size += 1
+        self._createByLevelOrder(self.root, self.nums, 0)
+
+    def size(self):
+        return self.size
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def getRoot(self):
+        return self.root
+
+    # 层次遍历
+    def levelOrder(self, root):
+        res = []
+        q = []
+        if root is not None:
+            q.append(root)
+
+        while len(q) > 0:
+            level = []
+            length = len(q)
+            while length > 0:
+                t = q[0]
+                q.pop(0)
+                level.append(t.val)
+                if t.left is not None:
+                    q.append(t.left)
+                if t.right is not None:
+                    q.append(t.right)
+                length -= 1
+            res.append(level)
+        return res
+
+    # 前序遍历
+    def preorder(self, root):
+        res = []
+        self._preorder(root, res)
+        return res
+
+    # 中序遍历
+    def inorder(self, root):
+        res = []
+        self._inorder(root, res)
+        return res
+
+    # 后序遍历
+    def postorder(self, root):
+        res = []
+        self._postorder(root, res)
+        return res
+
+    def _createByLevelOrder(self, node, nums, index):
+        n = len(nums)
+        if index >= n:
+            return
+        leftIndex = 2 * index + 1
+        if leftIndex < n:
+            if nums[leftIndex] is not None:
+                node.left = TreeNode(nums[leftIndex])
+                self.size += 1
+                self._createByLevelOrder(node.left, nums, leftIndex)
+        rightIndex = leftIndex + 1
+        if rightIndex < n:
+            if nums[rightIndex] is not None:
+                node.right = TreeNode(nums[rightIndex])
+                self.size += 1
+                self._createByLevelOrder(node.right, nums, rightIndex)
+
+    def _preorder(self, root, res):
+        if root is not None:
+            res.append(root.val)
+            self._preorder(root.left, res)
+            self._preorder(root.right, res)
+
+    def _inorder(self, root, res):
+        if root is not None:
+            self._inorder(root.left, res)
+            res.append(root.val)
+            self._inorder(root.right, res)
+
+    def _postorder(self, root, res):
+        if root is not None:
+            self._postorder(root.left, res)
+            self._postorder(root.right, res)
+            res.append(root.val)
+
+    def __len__(self):
+        """返回树中节点数"""
+        return self.size
+
+    def __str__(self):  # print该对象使用
+        return str(self.levelOrder(self.root))
+
+
+if __name__ == "__main__":
+    nu = None
+    nums = [
+        1,
+        2, 2,
+        3, nu, 4, 3
+    ]
+    root = MyTree(nums).getRoot()
+    print(MyTree().levelOrder(root))  # 层次遍历
+    print(MyTree().preorder(root))  # 前序遍历
+    print(MyTree().inorder(root))  # 中序遍历
+    print(MyTree().postorder(root))  # 后序遍历
---
+++ Python/_0987_Vertical_Order_Traversal_of_a_Binary_Tree/Solution.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# Date: 2021/7/31 15:21
+from MyTree import *
+from sortedcontainers import SortedDict
+
+# 执行用时：32 ms, 在所有 Python3 提交中击败了96.10%的用户
+# 内存消耗：15.5 MB, 在所有 Python3 提交中击败了5.32%的用户
+class Solution:
+    def __init__(self):
+        self.S = SortedDict()
+
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        self.dfs(root, 0, 0)
+
+        res = []
+        for v in self.S.values():
+            v.sort()
+            col = []
+            for p in v:
+                col.append(p[1])
+            res.append(col)
+        return res
+
+    def dfs(self, root, x, y):
+        if root is None:
+            return
+
+        if y not in self.S:
+            self.S[y] = []
+        self.S[y].append([x, root.val])
+
+        self.dfs(root.left, x + 1, y - 1)
+        self.dfs(root.right, x + 1, y + 1)
+
+
+if __name__ == "__main__":
+    nu = None
+    nums = [
+        3,
+        9, 20,
+        nu, nu, 15, 7
+    ]
+    root = MyTree(nums).getRoot()
+    print(Solution().verticalTraversal(root))  # [[9], [3, 15], [20], [7]]
