c868c4869df2c735d59c3c45d88b39d5e94f6de1
ToLoveToFeel <1137247975@qq.com>
2021-08-01 11:21:33 +0800

    Leetcode 0946 验证栈序列

---
+++ Python/_0946_Validate_Stack_Sequences/Solution.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# Date: 2021/8/1 11:20
+from typing import List
+
+
+# 执行用时：44 ms, 在所有 Python3 提交中击败了61.98%的用户
+# 内存消耗：15.1 MB, 在所有 Python3 提交中击败了37.23%的用户
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stk = []
+        j = 0
+        for i in range(len(pushed)):
+            stk.append(pushed[i])
+            while len(stk) != 0 and stk[len(stk) - 1] == popped[j]:
+                stk.pop(len(stk) - 1)
+                j += 1
+        return len(stk) == 0
+
+
+if __name__ == "__main__":
+    pass
